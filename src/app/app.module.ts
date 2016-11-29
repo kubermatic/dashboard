b9049d1b92822da4b6b39eec2bc172a8e6228de2
Mahmoud Rahbar Azad <mahmoud.azad@acando.de>
2016-11-29 22:00:05 +0100

    initial project setup
    added insipina css resources

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
