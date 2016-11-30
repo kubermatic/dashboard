5bff78a4ba64357fd82522e95f25b7132a10175e
Mahmoud Rahbar Azad <mahmoud.azad@acando.de>
2016-11-30 21:47:07 +0100

    added static resources of bootstrap and images

---
+++ src/app/frontpage/frontpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kubermatic-frontpage',
+  templateUrl: './frontpage.component.html',
+  styleUrls: ['./frontpage.component.scss']
+})
+export class FrontpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
