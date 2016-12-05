82b25bbdf0244c45918d6733b221198bea6dc961
Mahmoud Rahbar Azad <mahmoud.azad@acando.de>
2016-12-05 17:52:34 +0100

    changed some minor configs

--- src/app/kubermatic.component.spec.ts
+++ src/app/kubermatic.component.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
-import { KubermaticComponent } from './app.component';
+import { KubermaticComponent } from './kubermatic.component';
 
 describe('KubermaticComponent', () => {
   beforeEach(() => {
