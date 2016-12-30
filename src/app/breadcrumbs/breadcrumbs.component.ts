e3b6801cb5a8d1ead94da3d0d7423f720f117753
Mahmoud Reza Rahbar Azad <mahmoud.azad@acando.de>
2016-12-30 22:00:50 +0100

    changed font of navigation title to titilium fixed routerLinks and breadcrumbs

--- src/app/breadcrumbs/breadcrumbs.component.ts
+++ src/app/breadcrumbs/breadcrumbs.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from "@angular/core";
 })
 export class BreadcrumbsComponent implements OnInit {
 
+  public activePageTitle: string = "";
+
   constructor() { }
 
   ngOnInit() {
--- src/app/wizard/wizard.component.spec.ts
+++ src/app/wizard/wizard.component.spec.ts
@@ -1,17 +1,25 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { WizardComponent } from './wizard.component';
+import { WizardComponent } from "./wizard.component";
+import {FormBuilder} from "@angular/forms";
+import {ClusterNameGenerator} from "../util/name-generator.service";
+import {ApiService} from "../api/api.service";
 
-describe('WizardComponent', () => {
+describe("WizardComponent", () => {
   let component: WizardComponent;
   let fixture: ComponentFixture<WizardComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WizardComponent ]
+      declarations: [
+        WizardComponent
+      ],
+      providers: [
+        ApiService,
+        ClusterNameGenerator,
+        FormBuilder
+      ],
     })
     .compileComponents();
   }));
@@ -22,7 +30,7 @@ describe('WizardComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
