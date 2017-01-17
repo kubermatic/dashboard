7ec42db06ea856ecb27d0241ec12b3b4bf307c1a
Mahmoud Reza Rahbar Azad <mahmoud.azad@acando.de>
2017-01-17 22:08:43 +0100

    added missing slashes on api endpoints

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -30,84 +30,84 @@ export class ApiService {
   }
 
   getDataCenter(dc: string): Observable<DataCenterEntity> {
-    let url = `${this.restRoot}/dc${dc}`;
+    let url = `${this.restRoot}/dc/${dc}`;
 
     return this._http.get(url, { headers: this.headers })
       .map(res => res.json());
   }
 
   getClusters(dc: string): Observable<ClusterEntity[]> {
-    let url = `${this.restRoot}/dc${dc}/cluster`;
+    let url = `${this.restRoot}/dc/${dc}/cluster`;
 
     return this._http.get(url, { headers: this.headers })
       .map(res => res.json());
   }
 
   getCluster(clusterModel: ClusterModel): Observable<ClusterEntity> {
-    let url = `${this.restRoot}/dc${clusterModel.dc}/cluster/${clusterModel.cluster}`;
+    let url = `${this.restRoot}/dc/${clusterModel.dc}/cluster/${clusterModel.cluster}`;
 
     return this._http.get(url, { headers: this.headers })
       .map(res => res.json());
   }
 
   createCluster(dc: string): Observable<ClusterEntity> {
-    let url = `${this.restRoot}/dc${dc}/cluster`;
+    let url = `${this.restRoot}/dc/${dc}/cluster`;
 
     return this._http.post(url, "", { headers: this.headers })
       .map(res => res.json());
   }
 
   setCloudProvider(clusterModel: ClusterModel, node_dc: string): Observable<ClusterEntity> {
-    let url = `${this.restRoot}/dc${clusterModel.dc}/cluster/${clusterModel.cluster}/cloud`;
+    let url = `${this.restRoot}/dc/${clusterModel.dc}/cluster/${clusterModel.cluster}/cloud`;
 
     return this._http.put(url, {dc: node_dc}, { headers: this.headers })
       .map(res => res.json());
   }
 
   getKubeconfig(clusterModel: ClusterModel, authorization_token: string): Observable<string> {
-    let url = `${this.restRoot}/dc${clusterModel.dc}/cluster/${clusterModel.cluster}/kubeconfig?token=${authorization_token}`;
+    let url = `${this.restRoot}/dc/${clusterModel.dc}/cluster/${clusterModel.cluster}/kubeconfig?token=${authorization_token}`;
 
     return this._http.get(url, { headers: this.headers })
       .map(res => res.json());
   }
 
   deleteCluster(clusterModel: ClusterModel) {
-    let url = `${this.restRoot}/dc${clusterModel.dc}/cluster/${clusterModel.cluster}`;
+    let url = `${this.restRoot}/dc/${clusterModel.dc}/cluster/${clusterModel.cluster}`;
 
     return this._http.delete(url, { headers: this.headers })
       .map(res => res.json());
   }
 
   getClusterNodes(clusterModel: ClusterModel): Observable<NodeEntity[]> {
-    let url = `${this.restRoot}/dc${clusterModel.dc}/cluster/${clusterModel.cluster}/node`;
+    let url = `${this.restRoot}/dc/${clusterModel.dc}/cluster/${clusterModel.cluster}/node`;
 
     return this._http.get(url, { headers: this.headers })
       .map(res => res.json());
   }
 
   getClusterNode(clusterModel: ClusterModel, node_name: string): Observable<NodeEntity> {
-    let url = `${this.restRoot}/dc${clusterModel.dc}/cluster/${clusterModel.cluster}/node/${node_name}`;
+    let url = `${this.restRoot}/dc/${clusterModel.dc}/cluster/${clusterModel.cluster}/node/${node_name}`;
 
     return this._http.get(url, { headers: this.headers })
       .map(res => res.json());
   }
 
   createClusterNode(clusterModel: ClusterModel, nodeModel: CreateNodeModel): Observable<NodeEntity> {
-    let url = `${this.restRoot}/dc${clusterModel.dc}/cluster/${clusterModel.cluster}/node`;
+    let url = `${this.restRoot}/dc/${clusterModel.dc}/cluster/${clusterModel.cluster}/node`;
 
     return this._http.post(url, nodeModel, { headers: this.headers })
       .map(res => res.json());
   }
 
   deleteClusterNode(clusterModel: ClusterModel, node_name: string) {
-    let url = `${this.restRoot}/dc${clusterModel.dc}/cluster/${clusterModel.cluster}/node/${node_name}`;
+    let url = `${this.restRoot}/dc/${clusterModel.dc}/cluster/${clusterModel.cluster}/node/${node_name}`;
 
     return this._http.delete(url, { headers: this.headers })
       .map(res => res.json());
   }
 
   getKubernetesNode(clusterModel: ClusterModel, node_name: string) {
-    let url = `${this.restRoot}/dc${clusterModel.dc}/cluster/${clusterModel.cluster}/k8s/nodes/${node_name}`;
+    let url = `${this.restRoot}/dc/${clusterModel.dc}/cluster/${clusterModel.cluster}/k8s/nodes/${node_name}`;
 
     return this._http.get(url, { headers: this.headers })
       .map(res => res.json());
