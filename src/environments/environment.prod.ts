e9c60d9a4e2f01b5165a56a475265814c30aa0ff
j3ank <eugenia@loodse.com>
2017-02-15 14:44:57 +0100

    remove last changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  restRoot : "/api/v2",
+  restRoot : "/api/v1",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  restRoot : "/api/v2",
+  restRoot : "/api/v1",
 };
