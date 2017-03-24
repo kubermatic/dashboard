1403e33574114d85a37c88f3c89add8c5b0f3978
realfake <luk.burchard@gmail.com>
2017-03-24 11:41:39 +0100

    Add sub component mock to profile

---
+++ src/app/profile/add-ssh-key/add-ssh-key.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kubermatic-add-ssh-key',
+  templateUrl: './add-ssh-key.component.html',
+  styleUrls: ['./add-ssh-key.component.scss']
+})
+export class AddSshKeyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
