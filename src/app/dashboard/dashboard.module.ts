1dd9a4a939ae6134c3ad564c73e2831a3b6da5de
realfake <luk.burchard@gmail.com>
2017-03-24 11:20:41 +0100

    Remove not needed RouterModule

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {RouterModule} from "@angular/router";
+//import {RouterModule} from "@angular/router";
 
 import {ReactiveFormsModule, FormsModule} from "@angular/forms";
 import {HttpModule, BrowserXhr} from "@angular/http";
