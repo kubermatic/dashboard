e389dbba33f342080194bb7bc1fcc640d4767dba
j3ank <eugenia@loodse.com>
2017-05-30 17:34:16 +0200

    Delete Cluster Success / Error Notification

--- src/app/cluster/cluster-delete-confirmation/cluster-delete-confirmation.component.ts
+++ src/app/cluster/cluster-delete-confirmation/cluster-delete-confirmation.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Store } from "@ngrx/store";
+import * as fromRoot from "../../reducers/index";
 import { Router } from "@angular/router";
 import { ApiService } from "../../api/api.service";
 import { ClusterModel } from "../../api/model/ClusterModel";
+import { NotificationComponent } from "../../notification/notification.component";
 
 @Component({
   selector: 'kubermatic-cluster-delete-confirmation',
@@ -18,7 +21,7 @@ export class ClusterDeleteConfirmationComponent implements OnInit {
   public clusterModel: ClusterModel;
   public cluster: any;
 
-  constructor(private router: Router, private api: ApiService) {}
+  constructor(private router: Router, private api: ApiService, private store: Store<fromRoot.State>) {}
 
   ngOnInit() {
 
@@ -36,7 +39,11 @@ export class ClusterDeleteConfirmationComponent implements OnInit {
     this.clusterModel = new ClusterModel(this.seedDcName, this.clusterName);
     this.api.deleteCluster(this.clusterModel).subscribe(result => {
       this.cluster = result;
+      NotificationComponent.success(this.store, "Success", `Cluster removed successfully`);
+
       this.router.navigate(['/clusters']);
+    }, error => {
+      NotificationComponent.error(this.store, "Error", `${error.status} ${error.statusText}`);
     })
   }
 }
