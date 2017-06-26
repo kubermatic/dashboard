05efcc92dfccca66d996d1b1da5f8792484bc7fe
j3ank <eugenia@loodse.com>
2017-06-26 14:45:14 +0200

    remove error api service

--- src/app/api/api.service.spec.ts
+++ src/app/api/api.service.spec.ts
@@ -3,7 +3,7 @@
 import { TestBed, async, inject } from "@angular/core/testing";
 import { ApiService } from "./api.service";
 import {BrowserModule} from "@angular/platform-browser";
-import {Http, HttpModule} from "@angular/http";
+import {Http, HttpModule, ConnectionBackend} from "@angular/http";
 import {Auth} from "../auth/auth.service";
 import {RouterTestingModule} from "@angular/router/testing";
 import {StoreModule} from "@ngrx/store";
@@ -23,7 +23,8 @@ describe("ApiService", () => {
       providers: [
         Auth,
         ApiService,
-        Http
+        Http,
+        ConnectionBackend
       ],
     }).compileComponents();
   });
