ff6131843a8d87299194aec738e14df072b9046d
j3ank <eugenia@loodse.com>
2017-07-26 17:53:40 +0200

    Aggregated Health Status: orange -> red -> green

--- src/app/cluster-health-status/cluster-health-status.component.ts
+++ src/app/cluster-health-status/cluster-health-status.component.ts
@@ -7,11 +7,29 @@ import {MdTooltip, MdButton} from '@angular/material';
   styleUrls: ['./cluster-health-status.component.scss']
 })
 export class ClusterHealthStatusComponent implements OnInit {
-  @Input() health: any;
+  @Input() health;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public getHealthStatusColor(type) {
+    if (this.health) {
+      switch (type) {
+        case 'apiserver':
+          return this.health.apiserver ? 'green' : 'red';
+        case 'controller':
+          return this.health.controller ? 'green' : 'red';
+        case 'etcd':
+          return this.health.etcd[0] ? 'green' : 'red';
+        case 'scheduler':
+            return this.health.scheduler ? 'green' : 'red';
+        default:
+          return 'orange';
+      }
+    } else {
+      return 'orange';
+    }
+  }
 }
