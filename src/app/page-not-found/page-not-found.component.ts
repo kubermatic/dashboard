6c424be1a965b0e8a20cd523397ca8b6434bb6cb
zsemenyuk <30589941+zsemenyuk@users.noreply.github.com>
2017-08-23 16:33:14 +0300

    Create a 404 Page (#112)
    * add 404 page
    
    * fix routing for 404 page
    
    * Revert: Merge branch 'master' into feature/issue-91
    
    * - add redirecting the customer to the 404 if a cluster dosen't exist anymore;
    - hotfix - fix the bug with endless progress bar after redirecting to 404 page
    
    * hotfix - add css for the progress bar
    
    * add redirect

---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Auth } from '../auth/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'kubermatic-page-not-found',
+  templateUrl: './page-not-found.component.html',
+  styleUrls: ['./page-not-found.component.scss']
+})
+export class PageNotFoundComponent {
+  constructor(
+    private auth: Auth,
+    private router: Router
+  ) { }
+
+  backToApp(): void {
+    if (this.auth.authenticated()) {
+      this.router.navigate(['clusters']);
+    } else {
+      this.router.navigate(['login']);
+    }
+  }
+}
