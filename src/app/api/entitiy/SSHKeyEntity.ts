392c0968bf3e75594ee753934b4a917a2e9e1ae8
Henrik Schmidt <mrIncompetent@users.noreply.github.com>
2017-09-29 17:13:54 +0200

    Fix ssh-keys for client-4.0 branch of the backend (#215)

--- src/app/api/entitiy/SSHKeyEntity.ts
+++ src/app/api/entitiy/SSHKeyEntity.ts
@@ -1,10 +1,19 @@
 import {MetadataEntity} from "./MetadataEntity";
 
 export class SSHKeyEntity {
+  metadata: MetadataEntity;
+  spec: SSHKeySpec;
+
+  constructor(name: string, fingerprint: string, publicKey: string) {
+    this.spec = new SSHKeySpec(name, fingerprint, publicKey);
+  }
+}
+
+export class SSHKeySpec {
   name: string;
   fingerprint: string;
   public_key: string;
-  metadata: MetadataEntity
+  clusters: string[];
 
   constructor(name: string, fingerprint: string, publicKey: string) {
     this.name = name;
