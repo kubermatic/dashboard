28a3e5c45f14c754483ce6db8def5a2257b11ea5
Henrik Schmidt <mrIncompetent@users.noreply.github.com>
2017-10-19 10:51:29 +0200

    Extend openid scope (#274)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,11 @@
 
 let redirect_uri = window.location.protocol + '//' + window.location.host;
 let oauth = 'https://develop.kubermatic.io/dex/auth';
+let scope: string[] = ["openid", "email", "profile", "groups"];
 
 export const environment = {
   production: false,
   restRoot : "api/v1",
   digitalOceanRestRoot : "https://api.digitalocean.com/v2",
-  coreOSdexAuth : oauth + '?response_type=id_token&client_id=kubermatic&redirect_uri=' + redirect_uri + '&scope=openid&nonce=random',
+  coreOSdexAuth : oauth + '?response_type=id_token&client_id=kubermatic&redirect_uri=' + redirect_uri + '&scope='+ scope.join(" ") +'&nonce=random',
 };
