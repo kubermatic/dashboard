97356d2c42d1513d31ae27b845ba2aa1dccb2874
Eugene Dombrovsky <eugene.dombrowsky@gmail.com>
2017-11-27 16:55:44 +0300

    change dynamic-form module

--- src/app/shared/dynamic-form/dynamic-form.module.ts
+++ src/app/shared/dynamic-form/dynamic-form.module.ts
@@ -1,9 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { DynamicFormComponent } from 'app/shared/dynamic-form/dynamic-form.component';
 import { DynamicFormFieldComponent } from 'app/shared/dynamic-form/dynamic-form-field/dynamic-form-field.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
     declarations: [DynamicFormComponent, DynamicFormFieldComponent],
     exports: [],
     providers: [],
