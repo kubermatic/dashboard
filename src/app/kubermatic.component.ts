4fe9ef87781d409d0c522cca437d1599e3d0b8bb
Eugene Dombrovsky <eugene.dombrowsky@gmail.com>
2017-11-28 15:32:13 +0300

    remove needless code

--- src/app/kubermatic.component.ts
+++ src/app/kubermatic.component.ts
@@ -14,57 +14,10 @@ import { TextboxField } from './shared/model/dynamic-forms/field-textbox';
 })
 export class KubermaticComponent implements OnInit {
   @ViewChild('sidenav') public sidenav: MdSidenav;
-  public fields: any[];
 
   public constructor(
     private sidenavService: SidenavService
-  ) {
-    this.fields = [
-      new DropdownField({
-        key: 'brave',
-        placeholder: 'Bravery Rating',
-        options: [
-          { key: 'solid', value: 'Solid' },
-          { key: 'great', value: 'Great' },
-          { key: 'good', value: 'Good' },
-          { key: 'unproven', value: 'Unproven' }
-        ],
-        order: 3
-      }),
-
-      new TextboxField({
-        key: 'firstName',
-        placeholder: 'First name',
-        value: 'Bombasto',
-        required: true,
-        maxLength: 10,
-        minLength: 5,
-        order: 1
-      }),
-
-      new CheckboxField({
-        key: 'awsCas',
-        placeholder: 'Extend Provider settings',
-        value: false,
-        order: 4
-      }),
-
-      new TextboxField({
-        key: 'emailAddress',
-        placeholder: 'Email',
-        type: 'email',
-        order: 2
-      }),
-
-      new TextboxField({
-        key: 'number',
-        placeholder: 'Number',
-        type: 'number',
-        minNumber: 1,
-        order: 5
-      })
-    ];
-  }
+  ) {}
 
   public ngOnInit(): void {
     this.sidenavService
