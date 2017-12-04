288d7b934caf0dc5df0895a625f0e98043c360b6
Eugene Dombrovski <eugene.dombrowsky@gmail.com>
2017-12-04 19:44:32 +0300

    fix validation after adding a ssh key by the modal

--- src/app/wizard/ssh-key-form-field/ssh-key-form-field.component.ts
+++ src/app/wizard/ssh-key-form-field/ssh-key-form-field.component.ts
@@ -52,7 +52,7 @@ export class SshKeyFormFieldComponent implements OnInit, OnDestroy {
     const dialogRef = this.dialog.open(AddSshKeyModalComponent, this.config);
 
     dialogRef.afterClosed().subscribe(result => {
-      this.selectedSshKeys.push(result.metadata.name);
+      this.sshKeyForm.patchValue({ssh_keys: [...this.selectedSshKeys, result.metadata.name]});
       this.refreshSSHKeys();
     });
   }
