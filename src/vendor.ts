2978411e7a2a4bab9454199b7526db90f83f2589
Eugene Dombrowsky <bang1234@mail.ru>
2017-12-20 16:21:38 +0300

    Bugfix: error on the sshkeys page (#342)
    * fix: mat-toolbar styles on popups
    
    * fix: bug of SSH Keys list page

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,4 +1,5 @@
 import 'rxjs/add/operator/publishReplay';
 import 'rxjs/add/operator/combineLatest';
+import 'rxjs/add/operator/retry';
 import 'rxjs/add/observable/timer';
 import 'rxjs/add/observable/of';
