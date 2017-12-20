5dcb0f033f3c1ff1c4a511de81a999f124419812
Eugene Dombrovski <eugene.dombrowsky@gmail.com>
2017-12-20 23:00:13 +0300

    add: particles to the front page

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from 'app/pages/page-not-found/page-not-found.component';
 import { FrontpageComponent } from 'app/pages/frontpage/frontpage.component';
 import { PagesRoutingModule } from './pages-routing.module';
+import { ParticlesModule } from 'angular-particle';
 
 @NgModule({
-    imports: [PagesRoutingModule],
+    imports: [
+        PagesRoutingModule,
+        ParticlesModule
+    ],
     declarations: [
         PageNotFoundComponent,
         FrontpageComponent
