c571ea7b91db1f16e032e1e226502f031b994791
Eugene Dombrovsky <eugene.dombrowsky@gmail.com>
2018-01-03 12:54:34 +0300

    fix: show region location instead of the name

--- src/app/cluster/cluster-list/cluster-item/cluster-item.component.ts
+++ src/app/cluster/cluster-list/cluster-item/cluster-item.component.ts
@@ -1,3 +1,6 @@
+import { DatacenterService } from 'app/core/services';
+import { NodeProvider } from 'app/shared/model/NodeProviderConstants';
+import { DataCenterEntity } from 'app/shared/entity/DatacenterEntity';
 import {Component, OnInit, Input} from '@angular/core';
 import {ClusterEntity} from '../../../shared/entity/ClusterEntity';
 
@@ -7,12 +10,32 @@ import {ClusterEntity} from '../../../shared/entity/ClusterEntity';
   styleUrls: ['./cluster-item.component.scss']
 })
 export class ClusterItemComponent implements OnInit {
-  @Input() clusters: ClusterEntity;
+  @Input() cluster: ClusterEntity;
   @Input() index: number;
 
-  constructor() {}
+  public seedDc: DataCenterEntity;
+  public nodeDc: DataCenterEntity;
 
-  ngOnInit() {}
+  constructor(private dcService: DatacenterService) {}
+
+  ngOnInit() {
+    if (!this.seedDc) {
+      this.loadDataCenter(this.cluster.spec.seedDatacenterName, 'seedDc');
+    }
+
+    if (!this.nodeDc && this.cluster.provider !== NodeProvider.BRINGYOUROWN) {
+      this.loadDataCenter(this.cluster.spec.cloud.dc, 'nodeDc');
+    }
+
+  }
+
+  public loadDataCenter(dcName, dcObjectName): void {
+    this.dcService.getDataCenter(dcName).subscribe(
+      res => {
+        this[dcObjectName] = new DataCenterEntity(res.metadata, res.spec, res.seed);
+      }
+    );
+  }
 
   public getShortClusterName(name: string): string {
     return name.length > 12 ?  name.slice(0, 12) + '...' : name;
