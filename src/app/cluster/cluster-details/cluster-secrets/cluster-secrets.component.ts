199bfb4df0f9bc9077b5322926000708c6c17073
kgroschoff <kgroschoff@gmail.com>
2018-01-05 14:29:36 +0100

    add view for cluster detail secrets

---
+++ src/app/cluster/cluster-details/cluster-secrets/cluster-secrets.component.ts
@@ -0,0 +1,25 @@
+import { Component,  OnInit, Input, Inject} from '@angular/core';
+import {Health, Status} from '../../../shared/entity/ClusterEntity';
+
+
+@Component({
+  selector: 'kubermatic-cluster-secrets',
+  templateUrl: './cluster-secrets.component.html',
+  styleUrls: ['./cluster-secrets.component.scss']
+})
+export class ClusterSecretsComponent implements OnInit {
+  @Input() health: Health;
+  expand = false;
+
+  constructor() { }
+
+  ngOnInit() {  }
+
+  public expandPanel(): void {
+    if (this.expand === false) {
+      this.expand = true;
+    } else {
+      this.expand = false;
+    }
+  }
+}
