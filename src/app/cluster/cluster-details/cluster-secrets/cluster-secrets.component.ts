4be3487cba6149df48348c28eed1dcd94152364d
kgroschoff <kgroschoff@gmail.com>
2018-01-08 14:23:06 +0100

    decode and download certificates and keys

--- src/app/cluster/cluster-details/cluster-secrets/cluster-secrets.component.ts
+++ src/app/cluster/cluster-details/cluster-secrets/cluster-secrets.component.ts
@@ -1,5 +1,5 @@
 import { Component,  OnInit, Input, Inject} from '@angular/core';
-import {Health, Status} from '../../../shared/entity/ClusterEntity';
+import { ClusterEntity, Health } from '../../../shared/entity/ClusterEntity';
 
 
 @Component({
@@ -8,8 +8,8 @@ import {Health, Status} from '../../../shared/entity/ClusterEntity';
   styleUrls: ['./cluster-secrets.component.scss']
 })
 export class ClusterSecretsComponent implements OnInit {
+  @Input() cluster: ClusterEntity;
   @Input() health: Health;
-  @Input() status: Status;
   expand = false;
 
   constructor() { }
@@ -21,6 +21,64 @@ export class ClusterSecretsComponent implements OnInit {
       this.expand = true;
     } else {
       this.expand = false;
+    }
+  }
+
+  public decode(type: string): void {
+    let data;
+    let name;
+
+    switch (type) {
+      case 'root-ca-certificate':
+        name = 'ca.crt';
+        data = this.cluster.status.rootCA.cert;
+        break;
+      case 'root-ca-key':
+        data = this.cluster.status.rootCA.key;
+        name = 'ca.key';
+        break;
+      case 'apiserver-cert-certificate':
+        data = this.cluster.status.apiserverCert.cert;
+        name = 'apiserver.crt';
+        break;
+      case 'apiserver-cert-key':
+        data = this.cluster.status.apiserverCert.key;
+        name = 'apiserver.key';
+        break;
+      case 'apiserver-kubelet-client-certificate':
+        data = this.cluster.status.kubeletCert.cert;
+        name = 'kubelet-client.crt';
+        break;
+      case 'apiserver-kubelet-client-key':
+        data = this.cluster.status.kubeletCert.key;
+        name = 'kubelet-client.key';
+        break;
+      case 'serviceaccount-key':
+        data = this.cluster.status.serviceAccountKey;
+        name = 'service-account.key';
+        break;
+      case 'ssh-apiserver-rsa':
+        data = this.cluster.status.apiserverSshKey.privateKey;
+        name = 'apiserver_id-rsa';
+        break;
+      case 'ssh-apiserver-rsa-public':
+        data = this.cluster.status.apiserverSshKey.publicKey;
+        name = 'apiserver_id-rsa.pub';
+        break;
+      default:
+        break;
+    }
+
+    if (data && name) {
+      const blob = new Blob([atob(data)], {type: 'text/plain'});
+      const a = window.document.createElement('a');
+      a.href = window.URL.createObjectURL(blob);
+      a.download = name;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    } else {
+      return;
     }
   }
 }
