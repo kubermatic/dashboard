77ff9539379f58067cbf7f95ff00064109eca990
Eugene Dombrowsky <bang1234@mail.ru>
2018-01-10 16:03:45 +0300

    Remove spec files and devide tconfig files (#375)
    * remove: spec files
    
    * add: devide app and spec tscongs files

--- src/app/core/services/auth/auth.service.spec.ts
+++ src/app/core/services/auth/auth.service.spec.ts
@@ -1,12 +1,12 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from "@angular/core/testing";
-import { Auth } from "./auth.service";
-import {HttpModule} from "@angular/http";
-import {BrowserModule} from "@angular/platform-browser";
-import {RouterTestingModule} from "@angular/router/testing";
+import { TestBed, async, inject } from '@angular/core/testing';
+import { Auth } from './auth.service';
+import {HttpModule} from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterTestingModule} from '@angular/router/testing';
 
-describe("Auth", () => {
+describe('Auth', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -23,7 +23,7 @@ describe("Auth", () => {
   });
 
 
-  it("should ...", inject([Auth], (service: Auth) => {
+  it('should ...', inject([Auth], (service: Auth) => {
     expect(service).toBeTruthy();
   }));
 });
