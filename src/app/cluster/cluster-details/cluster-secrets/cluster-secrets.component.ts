e328c04035f34a4755e31311c10d19910f8cff60
kgroschoff <kgroschoff@gmail.com>
2018-01-11 11:13:40 +0100

    remove some cluster secrets

--- src/app/cluster/cluster-details/cluster-secrets/cluster-secrets.component.ts
+++ src/app/cluster/cluster-details/cluster-secrets/cluster-secrets.component.ts
@@ -29,33 +29,13 @@ export class ClusterSecretsComponent implements OnInit {
         name = 'ca.crt';
         data = this.cluster.status.rootCA.cert;
         break;
-      case 'root-ca-key':
-        data = this.cluster.status.rootCA.key;
-        name = 'ca.key';
-        break;
       case 'apiserver-cert-certificate':
         data = this.cluster.status.apiserverCert.cert;
         name = 'apiserver.crt';
         break;
-      case 'apiserver-cert-key':
-        data = this.cluster.status.apiserverCert.key;
-        name = 'apiserver.key';
-        break;
       case 'apiserver-kubelet-client-certificate':
         data = this.cluster.status.kubeletCert.cert;
         name = 'kubelet-client.crt';
-        break;
-      case 'apiserver-kubelet-client-key':
-        data = this.cluster.status.kubeletCert.key;
-        name = 'kubelet-client.key';
-        break;
-      case 'serviceaccount-key':
-        data = this.cluster.status.serviceAccountKey;
-        name = 'service-account.key';
-        break;
-      case 'ssh-apiserver-rsa':
-        data = this.cluster.status.apiserverSshKey.privateKey;
-        name = 'apiserver_id-rsa.key';
         break;
       case 'ssh-apiserver-rsa-public':
         data = this.cluster.status.apiserverSshKey.publicKey;
