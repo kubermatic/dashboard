3134d83bf9ba99ba8c2efc3dca6de5f79791c1f0
Eugene Dombrovsky <eugene.dombrowsky@gmail.com>
2018-01-25 18:03:25 +0300

    add: specs to set-cluster-name component

---
+++ src/app/wizard/set-cluster-name/set-cluster-name.component.spec.ts
@@ -0,0 +1,87 @@
+import { SharedModule } from '../../shared/shared.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgReduxTestingModule, MockNgRedux } from '@angular-redux/store/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { By } from '@angular/platform-browser';
+import { TestBed, async, ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
+import { SetClusterNameComponent } from './set-cluster-name.component';
+
+import { WizardActions } from '../../redux/actions/wizard.actions';
+import { ClusterNameGenerator } from '../../core/util/name-generator.service';
+import { InputValidationService } from '../../core/services';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ClusterNameGeneratorMock } from '../../testing/services/name-generator-mock.service';
+
+const modules: any[] = [
+    BrowserModule,
+    NgReduxTestingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    SharedModule
+];
+
+function setMockNgRedux(name: string): void {
+    const stepStub = MockNgRedux.getSelectorStub(['wizard', 'clusterNameForm', 'name']);
+    stepStub.next(name);
+}
+
+function completeRedux() {
+    const stepStub = MockNgRedux.getSelectorStub(['wizard', 'clusterNameForm', 'name']);
+    stepStub.complete();
+}
+
+describe('SetClusterNameComponent', () => {
+    let fixture: ComponentFixture<SetClusterNameComponent>;
+    let component: SetClusterNameComponent;
+    let nameGenerator: ClusterNameGenerator;
+
+    beforeEach(async(() => {
+        MockNgRedux.reset();
+        TestBed.configureTestingModule({
+            imports: [
+                ...modules,
+            ],
+            declarations: [
+                SetClusterNameComponent
+            ],
+            providers: [
+                { provide: ClusterNameGenerator, useClass: ClusterNameGeneratorMock },
+                InputValidationService
+            ],
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SetClusterNameComponent);
+        component = fixture.componentInstance;
+
+        nameGenerator = fixture.debugElement.injector.get(ClusterNameGenerator);
+    });
+
+    it('should create the Breadcrumbs', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should get name from redux', () => {
+        setMockNgRedux('test-name');
+        fixture.detectChanges();
+
+        expect(component.clusterName).toBe('test-name', 'should get a cluster name');
+    });
+
+    it('should caall generateName method', () => {
+        const generatedName = 'generated-name';
+        const spyGenerateName = spyOn(nameGenerator, 'generateName').and.returnValue(generatedName);
+        fixture.detectChanges();
+
+        component.generateName();
+        fixture.detectChanges();
+
+        const nameElement = fixture.debugElement.query(By.css('#name')).nativeElement;
+
+        expect(spyGenerateName.and.callThrough()).toHaveBeenCalledTimes(1);
+        expect(component.clusterNameForm.controls['name'].value).toBe(generatedName, 'should patch value');
+        expect(nameElement.value).toBe(generatedName, 'should display value in template');
+    });
+});
