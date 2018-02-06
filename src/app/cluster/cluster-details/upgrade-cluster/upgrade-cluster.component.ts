740ae1c3cb672c70bb82f3599ca7e49197aa6c0f
j3ank <j3ank@users.noreply.github.com>
2018-02-06 15:47:28 +0100

    upgrade cluster: update styling, wording, preselect latest version an… (#434)
    * upgrade cluster: update styling, wording, preselect latest version and remove selected version text
    
    * fix testing
    
    * missing white space

--- src/app/cluster/cluster-details/upgrade-cluster/upgrade-cluster.component.ts
+++ src/app/cluster/cluster-details/upgrade-cluster/upgrade-cluster.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import { ApiService } from 'app/core/services/api/api.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import {UpgradeClusterComponentData} from 'app/shared/model/UpgradeClusterDialogData';
@@ -9,14 +9,20 @@ import {UpgradeClusterComponentData} from 'app/shared/model/UpgradeClusterDialog
   templateUrl: './upgrade-cluster.component.html',
   styleUrls: ['./upgrade-cluster.component.scss']
 })
-export class UpgradeClusterComponent {
-  selectedVersion: string = null;
+export class UpgradeClusterComponent implements OnInit {
+  selectedVersion: string;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: UpgradeClusterComponentData,
     private api: ApiService,
     private dialogRef: MatDialogRef<UpgradeClusterComponent>
   ) { }
+
+  public ngOnInit() {
+    if (this.data.upgradesList) {
+      this.selectedVersion = this.data.upgradesList[this.data.upgradesList.length - 1];
+    }
+  }
 
   upgrade(): void {
       this.api.updateClusterUpgrade(this.data.clusterName, this.selectedVersion);
