d88ba90e66c2704eb5aa2fe116c2027ad6d225b0
Matthias Loibl <mail@matthiasloibl.com>
2018-02-07 17:13:31 +0100

    Start creating the http server with stdlib only

--- serve.go
+++ serve.go
@@ -4,88 +4,33 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
-	"os"
 	"strings"
-
-	"github.com/gorilla/handlers"
-	"github.com/elazarl/go-bindata-assetfs"
+	"time"
 )
-
-type hijack404 struct {
-	http.ResponseWriter
-	R         *http.Request
-	Handle404 func(w http.ResponseWriter, r *http.Request) bool
-}
-
-func (h *hijack404) WriteHeader(code int) {
-	if 404 == code && h.Handle404(h.ResponseWriter, h.R) {
-		panic(h)
-	}
-	h.ResponseWriter.WriteHeader(code)
-}
-
-func Handle404(handler http.Handler, handle404 func(w http.ResponseWriter, r *http.Request) bool) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		hijack := &hijack404{ResponseWriter: w, R: r, Handle404: handle404}
-		defer func() {
-			if p := recover(); p != nil {
-				if p == hijack {
-					return
-				}
-				panic(p)
-			}
-		}()
-		handler.ServeHTTP(hijack, r)
-	})
-}
-
-type compliantAssetFS struct {
-	assetfs.AssetFS
-}
-
-func (fs *compliantAssetFS) Open(name string) (http.File, error) {
-	if name == "/real-index.html" {
-		name = "/index.html"
-	}
-
-	f, err := fs.AssetFS.Open(name)
-	if err != nil {
-		if strings.HasSuffix(err.Error(), "not found") {
-			return nil, os.ErrNotExist
-		}
-	}
-	return f, err
-}
-
-func noDirListing(h http.Handler) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if strings.HasSuffix(r.URL.Path, "/") {
-			http.NotFound(w, r)
-		}
-		h.ServeHTTP(w, r)
-	})
-}
 
 func main() {
 	port := flag.Int("port", 8080, "Port to listen on")
 	address := flag.String("address", "0.0.0.0", "Address to listen on")
 	flag.Parse()
 
-	afs := compliantAssetFS{*assetFS()}
-	fs := http.FileServer(&afs)
-	err := http.ListenAndServe(fmt.Sprintf("%s:%d", *address, *port),
-		handlers.CombinedLoggingHandler(os.Stdout, Handle404(
-			noDirListing(fs),
-			func(w http.ResponseWriter, r *http.Request) bool {
-				w.Header().Del("Content-Type")
-				w.Header().Del("X-Content-Type-Options")
-				r.URL.Path = "/real-index.html"
-				fs.ServeHTTP(w, r)
-				return true
-			},
-		),
-	))
-	if err != nil {
-		panic(err.Error())
+	s := http.NewServeMux()
+	s.HandleFunc("/", handler)
+
+	addr := fmt.Sprintf("%s:%d", *address, *port)
+	if err := http.ListenAndServe(addr, s); err != nil {
+		panic(err)
 	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	defer func(start time.Time) {
+		fmt.Printf("%s took %v\n", r.URL.Path, time.Since(start))
+	}(time.Now())
+
+	if strings.HasSuffix(r.URL.Path, "/") {
+		http.NotFound(w, r)
+		return
+	}
+
+	http.FileServer(http.Dir("./dist")).ServeHTTP(w, r)
 }
