d9fe91bfeb1b0ba428872f4e3abf061b36230907
kgroschoff <kgroschoff@gmail.com>
2018-03-26 15:44:21 +0200

    implement call to api to revoke admin token

--- src/app/cluster/cluster-details/cluster-secrets/revoke-admin-token/revoke-admin-token.component.ts
+++ src/app/cluster/cluster-details/cluster-secrets/revoke-admin-token/revoke-admin-token.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material';
+import { NotificationActions } from '../../../../redux/actions/notification.actions';
 import { ApiService } from '../../../../core/services/api/api.service';
 import { DataCenterEntity } from '../../../../shared/entity/DatacenterEntity';
 import { ClusterEntity } from '../../../../shared/entity/ClusterEntity';
@@ -13,6 +15,7 @@ import { ClusterEntity } from '../../../../shared/entity/ClusterEntity';
 export class RevokeAdminTokenComponent implements OnInit {
   @Input() cluster: ClusterEntity;
   @Input() datacenter: DataCenterEntity;
+  public disableRevoke: boolean = true;
   public revokeAdminTokenForm: FormGroup;
   public pattern: string = '[bcdfghjklmnpqrstvwxz2456789]{6}.[bcdfghjklmnpqrstvwxz2456789]{16}';
 
@@ -20,10 +23,24 @@ export class RevokeAdminTokenComponent implements OnInit {
 
   ngOnInit() {
     this.revokeAdminTokenForm = this.fb.group({
-      adminToken: ['', Validators.pattern('[bcdfghjklmnpqrstvwxz2456789]{6}.[bcdfghjklmnpqrstvwxz2456789]{16}')]
+      adminToken: ['', Validators.pattern(this.pattern)]
     });
   }
 
+  onChange(event: any) {
+    if ((this.revokeAdminTokenForm.controls['adminToken'].value).match(this.pattern)) {
+      this.disableRevoke = false;
+    } else {
+      this.disableRevoke = true;
+    }
+  }
+
   public revokeAdminToken() {
+    if (!this.disableRevoke) {
+      this.cluster.address.adminToken = this.revokeAdminTokenForm.controls['adminToken'].value;
+      this.api.editCluster(this.cluster, this.datacenter.spec.seed).subscribe(res => {
+        NotificationActions.success('Success', `Revoke Admin Token successfully`);
+      });
+    }
   }
 }
