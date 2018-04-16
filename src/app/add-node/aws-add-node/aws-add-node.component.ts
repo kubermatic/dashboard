e951609fb7586bb2ab3cc383f9d8c8531ce43bda
kgroschoff <kgroschoff@gmail.com>
2018-04-16 15:07:10 +0200

    adjust codebase

--- src/app/add-node/aws-add-node/aws-add-node.component.ts
+++ src/app/add-node/aws-add-node/aws-add-node.component.ts
@@ -19,37 +19,34 @@ export class AwsAddNodeComponent implements OnInit, OnDestroy {
     type: new FormControl('t2.small', Validators.required),
     disk_size: new FormControl(25, Validators.required),
     disk_type: new FormControl('standard', Validators.required),
-    ami: new FormControl('')
+    ami: new FormControl(''),
+    tags: new FormArray([
+      new FormGroup({
+        key: new FormControl(''),
+        value: new FormControl('')
+      })
+    ])
   });
-  public tagForm: FormGroup;
-  public tagList: FormArray;
-  private formOnChangeSub: Subscription[] = [];
+  public tags: FormArray;
+  private formOnChangeSub: Subscription;
 
-  constructor(private addNodeService: AddNodeService, private fb: FormBuilder) {
+  constructor(private addNodeService: AddNodeService) {
   }
 
   ngOnInit(): void {
-    this.tagForm = this.fb.group({
-      tagList: this.fb.array([this.setTagList()])
+
+    this.formOnChangeSub = this.awsNodeForm.valueChanges.subscribe(data => {
+      this.addNodeService.changeNodeProviderData(this.getNodeProviderData());
     });
-
-    this.formOnChangeSub.push(this.awsNodeForm.valueChanges.subscribe(data => {
-      this.addNodeService.changeNodeProviderData(this.getNodeProviderData());
-    }));
-
-    this.formOnChangeSub.push(this.tagForm.valueChanges.subscribe(data => {
-      this.addNodeService.changeNodeProviderData(this.getNodeProviderData());
-    }));
 
     this.addNodeService.changeNodeProviderData(this.getNodeProviderData());
   }
 
   getNodeProviderData(): NodeProviderData {
     const tagMap = {};
-    const tagListValue = this.tagForm.get('tagList').value;
-    for (const i in tagListValue) {
-      if (tagListValue[i].key !== '' && tagListValue[i].value !== '') {
-        tagMap[tagListValue[i].key] = tagListValue[i].value;
+    for (const i in this.awsNodeForm.controls.tags.value) {
+      if (this.awsNodeForm.controls.tags.value[i].key !== '' && this.awsNodeForm.controls.tags.value[i].value !== '') {
+        tagMap[this.awsNodeForm.controls.tags.value[i].key] = this.awsNodeForm.controls.tags.value[i].value;
       }
     }
 
@@ -67,32 +64,24 @@ export class AwsAddNodeComponent implements OnInit, OnDestroy {
     };
   }
 
-  getTagForm(tagForm) {
-    return tagForm.get('tagList').controls;
-  }
-
-  setTagList(): FormGroup {
-    return this.fb.group({
-      key: '',
-      value: ''
-    });
+  getTagForm(form) {
+    return form.get('tags').controls;
   }
 
   addTag() {
-    this.tagList = <FormArray>this.tagForm.get('tagList');
-    this.tagList.push(this.setTagList());
+    this.tags = <FormArray>this.awsNodeForm.get('tags');
+    this.tags.push(new FormGroup({
+      key: new FormControl(''),
+      value: new FormControl('')
+    }));
   }
 
   deleteTag(index: number): void {
-    const arrayControl = <FormArray>this.tagForm.get('tagList');
+    const arrayControl = <FormArray>this.awsNodeForm.get('tags');
     arrayControl.removeAt(index);
   }
 
   ngOnDestroy(): void {
-    for (const sub of this.formOnChangeSub) {
-      if (sub) {
-        sub.unsubscribe();
-      }
-    }
+    this.formOnChangeSub.unsubscribe();
   }
 }
