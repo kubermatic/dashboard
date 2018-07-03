a78c6b393f593ce6e7854fe6358816b34505c26a
Kristin Groschoff <kgroschoff@gmail.com>
2018-07-03 14:36:53 +0200

    image/template vom dc (#689)
    * add default template for vsphere
    
    * fix tests
    
    * add Spec

--- src/app/shared/entity/datacenter/OpenStackDatacenterSpec.ts
+++ src/app/shared/entity/datacenter/OpenStackDatacenterSpec.ts
@@ -1,4 +1,8 @@
+import { DatacenterOperatingSystemOptions } from '../DatacenterEntity';
+
 export class OpenStackDatacenterSpec {
   availability_zone: string;
   auth_url: string;
+  region: string;
+  images: DatacenterOperatingSystemOptions;
 }
