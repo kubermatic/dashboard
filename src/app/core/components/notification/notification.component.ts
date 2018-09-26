2b6e90c003342e376dd96a46d0774a6adf37dd63
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2018-09-26 14:26:41 +0200

    Copy notifications to clipboard on click

--- src/app/core/components/notification/notification.component.ts
+++ src/app/core/components/notification/notification.component.ts
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs';
 import { Component } from '@angular/core';
-import { NotificationsService } from 'angular2-notifications';
+import { NotificationsService, Notification } from 'angular2-notifications';
 import { NotificationToast, NotificationToastType } from '../../../shared/interfaces/notification-toast.interface';
 import { select } from '@angular-redux/store';
 
@@ -35,20 +35,51 @@ export class NotificationComponent {
   }
 
   createToast(toast: NotificationToast) {
+    let notification: Notification;
     switch (toast.type) {
       case NotificationToastType.success:
-        this._service.success(toast.title, toast.content);
+        notification = this._service.success(toast.title, toast.content);
         break;
       case NotificationToastType.alert:
-        this._service.alert(toast.title, toast.content);
+        notification = this._service.alert(toast.title, toast.content);
         break;
       case NotificationToastType.error:
-        this._service.error(toast.title, toast.content);
+        notification = this._service.error(toast.title, toast.content);
         break;
       case NotificationToastType.info:
-        this._service.info(toast.title, toast.content);
+        notification = this._service.info(toast.title, toast.content);
         break;
     }
+    this.registerClickHandler(notification);
+  }
+
+  registerClickHandler(notification: Notification) {
+    if (notification) {
+      notification.click.subscribe(() => {
+        // TODO: Use navigator.clipboard instead of navigator['clipboard'] once TypeScript will support it.
+        if (navigator['clipboard']) {
+          navigator['clipboard'].writeText(notification.content);
+        } else {
+          // TODO: This fallback should be removed once Clipboard API will be widely adopted:
+          // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard#Browser_compatibility
+          this.copyToClipboard(notification.content);
+        }
+      });
+    }
+  }
+
+  copyToClipboard(text: string){
+    let textarea = document.createElement('textarea');
+    textarea.style.position = 'fixed';
+    textarea.style.left = '0';
+    textarea.style.top = '0';
+    textarea.style.opacity = '0';
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.focus();
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
   }
 
 }
