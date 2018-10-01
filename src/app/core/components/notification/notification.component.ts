606433bb24a41934d6d0a4b132216915c762ca6a
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2018-10-01 13:54:57 +0200

    Notifications fixes (#798)
    * Copy notifications to clipboard on click
    
    * Increase notifications timeout to 10s
    
    * Fix lint issues
    
    * Add simple test
    
    * Add status code to message copied to clipboard
    
    * Put notifications on top of every overlay
    
    * Reuse ngx-clipboard module
    
    * Add close and copy to clipboard buttons for notifications
    
    * Remove /deep/ from css

--- src/app/core/components/notification/notification.component.ts
+++ src/app/core/components/notification/notification.component.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
-import { Component } from '@angular/core';
-import { NotificationsService } from 'angular2-notifications';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ClipboardService } from 'ngx-clipboard';
+import { NotificationsService, Notification, NotificationType } from 'angular2-notifications';
 import { NotificationToast, NotificationToastType } from '../../../shared/interfaces/notification-toast.interface';
 import { select } from '@angular-redux/store';
 
@@ -8,15 +9,18 @@ import { select } from '@angular-redux/store';
   selector: 'kubermatic-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss'],
-  providers: [NotificationsService]
+  providers: [NotificationsService],
+  encapsulation: ViewEncapsulation.None
 })
 export class NotificationComponent {
+  private static readonly closeButtonClass = 'sn-close-button';
+  private static readonly copyButtonClass = 'sn-copy-button';
 
   public options = {
-    timeOut: 5000,
+    timeOut: 10000,
     theClass: 'custom-simple-notification',
     lastOnBottom: true,
-    clickToClose: true,
+    clickToClose: false,
     showProgressBar: true,
     pauseOnHover: true,
     preventDuplicates: false,
@@ -26,7 +30,7 @@ export class NotificationComponent {
 
   @select(['notification', 'toast']) notification$: Observable<NotificationToast>;
 
-  constructor(private _service: NotificationsService) {
+  constructor(private _service: NotificationsService, private _clipboard: ClipboardService) {
     this.notification$.subscribe(toast => {
       if (toast) {
         this.createToast(toast);
@@ -35,20 +39,53 @@ export class NotificationComponent {
   }
 
   createToast(toast: NotificationToast) {
+    let notification: Notification;
+    const plainMessage = `${toast.title}: ${toast.content}`;
+    const htmlMessage = this.createHtmlMessage(toast);
     switch (toast.type) {
       case NotificationToastType.success:
-        this._service.success(toast.title, toast.content);
+        notification = this._service.success(toast.title, htmlMessage);
         break;
       case NotificationToastType.alert:
-        this._service.alert(toast.title, toast.content);
+        notification = this._service.alert(toast.title, htmlMessage);
         break;
       case NotificationToastType.error:
-        this._service.error(toast.title, toast.content);
+        notification = this._service.error(toast.title, htmlMessage);
         break;
       case NotificationToastType.info:
-        this._service.info(toast.title, toast.content);
+        notification = this._service.info(toast.title, htmlMessage);
         break;
+    }
+    this.registerClickHandler(notification, plainMessage);
+  }
+
+  createHtmlMessage(toast: NotificationToast): string {
+    return `${toast.content}<div class="sn-controls"><span class="${NotificationComponent.closeButtonClass}">Close</button>
+    <span class="${NotificationComponent.copyButtonClass}">Copy to clipboard</button></div>`;
+  }
+
+  registerClickHandler(notification: Notification, plainMessage: string) {
+    if (notification) {
+      notification.click.subscribe((e: MouseEvent) => {
+        const targetId = (<HTMLElement>e.target).className;
+        if (targetId.indexOf( NotificationComponent.closeButtonClass) > -1) {
+          this._service.remove(notification.id);
+        }
+        if (targetId.indexOf(NotificationComponent.copyButtonClass) > -1) {
+          this.copyToClipboard(plainMessage);
+        }
+      });
     }
   }
 
+  copyToClipboard(text: string) {
+        // TODO: Use navigator.clipboard instead of navigator['clipboard'] once TypeScript will support it.
+        if (navigator['clipboard']) {
+      navigator['clipboard'].writeText(text);
+        } else {
+          // TODO: This fallback can be removed once Clipboard API will be widely adopted:
+          // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard#Browser_compatibility
+      this._clipboard.copyFromContent(text);
+    }
+  }
 }
