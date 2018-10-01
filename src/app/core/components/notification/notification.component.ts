4d20d73bd2e92db591637fcdc336912a67fcd5ff
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2018-10-01 09:56:48 +0200

    Add close and copy to clipboard buttons for notifications

--- src/app/core/components/notification/notification.component.ts
+++ src/app/core/components/notification/notification.component.ts
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs';
 import { Component } from '@angular/core';
 import { ClipboardService } from 'ngx-clipboard';
-import { NotificationsService, Notification } from 'angular2-notifications';
+import { NotificationsService, Notification, NotificationType } from 'angular2-notifications';
 import { NotificationToast, NotificationToastType } from '../../../shared/interfaces/notification-toast.interface';
 import { select } from '@angular-redux/store';
 
@@ -12,12 +12,14 @@ import { select } from '@angular-redux/store';
   providers: [NotificationsService]
 })
 export class NotificationComponent {
+  private static readonly closeButtonClass = 'sn-close-button';
+  private static readonly copyButtonClass = 'sn-copy-button';
 
   public options = {
     timeOut: 10000,
     theClass: 'custom-simple-notification',
     lastOnBottom: true,
-    clickToClose: true,
+    clickToClose: false,
     showProgressBar: true,
     pauseOnHover: true,
     preventDuplicates: false,
@@ -37,36 +39,52 @@ export class NotificationComponent {
 
   createToast(toast: NotificationToast) {
     let notification: Notification;
+    const plainMessage = `${toast.title}: ${toast.content}`;
+    const htmlMessage = this.createHtmlMessage(toast);
     switch (toast.type) {
       case NotificationToastType.success:
-        notification = this._service.success(toast.title, toast.content);
+        notification = this._service.success(toast.title, htmlMessage);
         break;
       case NotificationToastType.alert:
-        notification = this._service.alert(toast.title, toast.content);
+        notification = this._service.alert(toast.title, htmlMessage);
         break;
       case NotificationToastType.error:
-        notification = this._service.error(toast.title, toast.content);
+        notification = this._service.error(toast.title, htmlMessage);
         break;
       case NotificationToastType.info:
-        notification = this._service.info(toast.title, toast.content);
+        notification = this._service.info(toast.title, htmlMessage);
         break;
     }
-    this.registerClickHandler(notification);
+    this.registerClickHandler(notification, plainMessage);
   }
 
-  registerClickHandler(notification: Notification) {
+  createHtmlMessage(toast: NotificationToast): string {
+    return `${toast.content}<div class="sn-controls"><span class="${NotificationComponent.closeButtonClass}">Close</button>
+    <span class="${NotificationComponent.copyButtonClass}">Copy to clipboard</button></div>`;
+  }
+
+  registerClickHandler(notification: Notification, plainMessage: string) {
     if (notification) {
-      const message = `${notification.title}: ${notification.content}`;
-      notification.click.subscribe(() => {
+      notification.click.subscribe((e: MouseEvent) => {
+        const targetId = (<HTMLElement>e.target).className;
+        if (targetId.indexOf( NotificationComponent.closeButtonClass) > -1) {
+          this._service.remove(notification.id);
+        }
+        if (targetId.indexOf(NotificationComponent.copyButtonClass) > -1) {
+          this.copyToClipboard(plainMessage);
+        }
+      });
+    }
+  }
+
+  copyToClipboard(text: string) {
         // TODO: Use navigator.clipboard instead of navigator['clipboard'] once TypeScript will support it.
         if (navigator['clipboard']) {
-          navigator['clipboard'].writeText(message);
+      navigator['clipboard'].writeText(text);
         } else {
           // TODO: This fallback can be removed once Clipboard API will be widely adopted:
           // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard#Browser_compatibility
-          this._clipboard.copyFromContent(message);
-        }
-      });
+      this._clipboard.copyFromContent(text);
     }
   }
 }
