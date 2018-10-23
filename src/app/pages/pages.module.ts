3dbc9f4d6c33dc8229a99a8d2d82276852c07761
Kristin Groschoff <kgroschoff@gmail.com>
2018-10-23 17:34:40 +0200

    add Terms of Service to UI (#830)
    * add terms of service to ui
    
    * terms of service in card; update breadcrumbs
    
    * fix breadcrumbs

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from '../pages/page-not-found/page-not-found.component';
 import { FrontpageComponent } from '../pages/frontpage/frontpage.component';
+import { TermsOfServiceComponent } from '../pages/terms-of-service/terms-of-service.component';
 
 import { ParticlesModule } from 'angular-particle';
 import { PagesRoutingModule } from './pages-routing.module';
@@ -14,7 +15,8 @@ import { SharedModule } from './../shared/shared.module';
   ],
   declarations: [
     PageNotFoundComponent,
-    FrontpageComponent
+    FrontpageComponent,
+    TermsOfServiceComponent
   ],
   exports: []
 })
---
+++ src/app/pages/terms-of-service/terms-of-service.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kubermatic-terms-of-service',
+  templateUrl: './terms-of-service.component.html',
+  styleUrls: ['./terms-of-service.component.scss']
+})
+export class TermsOfServiceComponent {
+  constructor() { }
+}
