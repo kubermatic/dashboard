a9cee44ef8534f2db914e7c586d0909e8265838f
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2018-11-03 11:50:42 +0100

    Fix e2e tests configuration

---
+++ e2e/src/app.e2e-spec.ts
@@ -0,0 +1,13 @@
+import { AppPage } from './app.po';
+
+describe('workspace-project App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+  });
+});
