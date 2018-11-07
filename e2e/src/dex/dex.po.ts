0f77c43188525177dd20e250c141aa0bcbb3ab24
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2018-11-07 14:05:19 +0100

    Initialize e2e tests (#848)
    * Fix e2e tests configuration
    
    * Fix linting
    
    * Add login page e2e tests
    
    * Fix .gitignore
    
    * Add protractor.conf.js
    
    * Minor changes
    
    * Add e2e login story
    
    * Update xpath
    
    * Update tests
    
    * Minor changes
    
    * Update npm scripts
    
    * Fix lint errors

---
+++ e2e/src/dex/dex.po.ts
@@ -0,0 +1,19 @@
+import { by, element } from 'protractor';
+
+export class DexPage {
+  getStaticLoginButton(): any {
+    return element(by.xpath('//span[contains(text(),"Static Credentials")]'));
+  }
+
+  getStaticLoginInput(): any {
+    return element(by.xpath('//input[@id="login"]'));
+  }
+
+  getStaticPasswordInput(): any {
+    return element(by.xpath('//input[@id="password"]'));
+  }
+
+  getStaticLoginSubmitButton(): any {
+    return element(by.id('submit-login'));
+  }
+}
