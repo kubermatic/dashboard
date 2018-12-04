d58013a928ea0505c36057f2d25991330f2c06f0
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2018-12-04 12:39:12 +0100

    Display node deployment status

--- src/app/cluster/cluster-details/node-deployment-list/node-deployment-list.component.ts
+++ src/app/cluster/cluster-details/node-deployment-list/node-deployment-list.component.ts
@@ -24,7 +24,7 @@ export class NodeDeploymentListComponent implements OnInit {
   @Input() hasInitialNodes: boolean;
   @Output() deleteNodeDeployment = new EventEmitter<NodeDeploymentEntity>();
 
-  displayedColumns: string[] = ['position', 'name', 'replicas', 'ver', 'created'];
+  displayedColumns: string[] = ['position', 'name', 'replicas', 'ver', 'created', 'status'];
   userGroupConfig: UserGroupConfig;
   userGroup: string;
 
@@ -41,5 +41,35 @@ export class NodeDeploymentListComponent implements OnInit {
     const dataSource = new MatTableDataSource<NodeDeploymentEntity>();
     dataSource.data = this.nodeDeployments;
     return dataSource;
+  }
+
+  getHealthStatus(nd: NodeDeploymentEntity, index: number): object {
+    const green = 'fa fa-circle green';
+    const orangeSpinner = 'fa fa-spin fa-circle-o-notch orange';
+    const healthStatus = {};
+
+    if (!!nd.deletionTimestamp) {
+      healthStatus['color'] = orangeSpinner;
+      healthStatus['status'] = 'Deleting';
+      healthStatus['class'] = 'statusDeleting';
+    } else if (nd.status.availableReplicas === nd.spec.replicas) {
+      healthStatus['color'] = green;
+      healthStatus['status'] = 'Running';
+      healthStatus['class'] = 'statusRunning';
+    } else if (nd.status.availableReplicas > nd.spec.replicas) {
+      healthStatus['color'] = orangeSpinner;
+      healthStatus['status'] = 'Updating';
+      healthStatus['class'] = 'statusWaiting';
+    } else {
+      healthStatus['color'] = orangeSpinner;
+      healthStatus['status'] = 'Pending';
+      healthStatus['class'] = 'statusWaiting';
+    }
+
+    if (index % 2 !== 0) {
+      healthStatus['class'] += ' odd';
+    }
+
+    return healthStatus;
   }
 }
