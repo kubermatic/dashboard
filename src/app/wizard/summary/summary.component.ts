ffd9409fce206b02c71960d9f21226ed6979cd7c
kgroschoff <kgroschoff@gmail.com>
2018-12-06 10:21:44 +0100

    add warning to summary page if no ips are left

--- src/app/wizard/summary/summary.component.ts
+++ src/app/wizard/summary/summary.component.ts
@@ -16,10 +16,16 @@ export class SummaryComponent implements OnInit, OnDestroy {
   @Input() cluster: ClusterEntity;
   @Input() providerFormData: ClusterProviderForm;
   @Input() datacenterFormData: ClusterDatacenterForm;
+  noMoreIpsLeft = false;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (!!this.cluster.spec.machineNetworks) {
+      this.noMoreIpsLeft = this.noIpsLeft(this.cluster, this.nodeData.count);
+      console.log('NO IPS LEFT', this.noIpsLeft);
+    }
+  }
 
   ngOnDestroy(): void {}
 
@@ -60,7 +66,41 @@ export class SummaryComponent implements OnInit, OnDestroy {
     }
     return tagsValue;
   }
+
   getDnsServers(dnsServers: string[]): string {
     return dnsServers.join(', ');
+  }
+
+  noIpsLeft(cluster: ClusterEntity, nodeCount: number): boolean {
+    // tslint:disable
+    const ip4ToInt = (ip) => ip.split('.').reduce((int, oct) => (int << 8) + parseInt(oct, 10), 0) >>> 0;
+
+    const isIp4InCidr = (ip, cidr) => {
+      const [range, bits = 32] = cidr.split('/');
+      const mask = ~(2 ** (32 - bits) - 1);
+      return (ip4ToInt(ip) & mask) === (ip4ToInt(range) & mask);
+    };
+    // tslint:enable
+    let ipCount = 0;
+
+    for (const i in cluster.spec.machineNetworks) {
+      if (cluster.spec.machineNetworks.hasOwnProperty(i)) {
+        const isInCidr = isIp4InCidr(cluster.spec.machineNetworks[i].gateway, cluster.spec.machineNetworks[i].cidr);
+        const cidr = +cluster.spec.machineNetworks[i].cidr.split('/')[1];
+        if (isInCidr) {
+          ipCount += (2 ** (32 - cidr)) - 3;
+        } else {
+          ipCount += (2 ** (32 - cidr)) - 2;
+        }
+      }
+    }
+
+    if (!!ipCount && ipCount > 0) {
+      if ((ipCount - nodeCount) >= 0) {
+        return false;
+      } else {
+        return true;
+      }
+    }
   }
 }
