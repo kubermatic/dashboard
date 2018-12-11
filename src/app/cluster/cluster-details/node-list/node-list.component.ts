6e60632246e7164eb13670b8bb1272a76fc8b849
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2018-12-11 14:28:56 +0100

    Enhancements for OpenShift demo (#917)
    * Enhancements for OpenShift demo
    
    * Enhancements for OpenShift demo

--- src/app/cluster/cluster-details/node-list/node-list.component.ts
+++ src/app/cluster/cluster-details/node-list/node-list.component.ts
@@ -37,6 +37,7 @@ export class NodeListComponent implements OnInit {
     disableClose: false,
     hasBackdrop: true,
   };
+  isOnDevServer = false;
 
   constructor(
       public dialog: MatDialog, private appConfigService: AppConfigService, private userService: UserService,
@@ -48,6 +49,9 @@ export class NodeListComponent implements OnInit {
     this.userService.currentUserGroup(this.projectID).subscribe((group) => {
       this.userGroup = group;
     });
+
+    // Enable OpenShift only on dev.kubermatic.io.
+    this.isOnDevServer = window.location.host.includes('dev.kubermatic.io');
   }
 
   deleteNodeDialog(node: NodeEntity): void {
