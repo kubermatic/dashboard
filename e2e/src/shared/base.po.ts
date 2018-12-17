9778f79f8867c8fa9b236b57ae29a0b266d9f561
Sebastian Florek <Sebastian.Florek@loodse.com>
2018-12-17 11:42:59 +0100

    Get rid of browser sleep

--- e2e/src/shared/base.po.ts
+++ e2e/src/shared/base.po.ts
@@ -11,6 +11,13 @@ export class BasePage {
     return browser.wait(ExpectedConditions.stalenessOf(element), 15000);
   }
 
+  /**
+   * @param url - partial url or full expected url after redirect
+   */
+  waitForRedirect(url: string): any {
+    return browser.wait(ExpectedConditions.urlContains(url), 15000);
+  }
+
   waitForNotifications(): any {
     return element.all(this._notificationCloseButton).then((elements: any[]) => {
       elements.forEach((elem) => {
