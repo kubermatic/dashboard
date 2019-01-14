3627dc097b89454c04580751745583a5d2cd5c40
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-01-14 13:14:07 +0100

    Styling fixes and alignments for resource lists (#948)
    * Add API service method to list node deployment nodes
    
    * Refactor node deployment list
    Add expansion panel
    
    * Load nodes on row expand
    
    * Display node list inside node deployment list
    
    * Revert table-in-table demo
    Align status display
    
    * Align status display
    
    * Align status display
    
    * Final adjustments for project list
    
    * Final adjustments for member list
    
    * Final adjustments for cluster details lists
    
    * Final adjustments for SSH key lists
    
    * Fix tests

--- src/app/core/services/api/api.service.ts
+++ src/app/core/services/api/api.service.ts
@@ -70,6 +70,12 @@ export class ApiService {
     return this.http.get<NodeDeploymentEntity[]>(url, {headers: this.headers});
   }
 
+  getNodeDeploymentNodes(nd: NodeDeploymentEntity, cluster: string, dc: string, projectID: string):
+      Observable<NodeEntity[]> {
+    const url = `${this.restRoot}/projects/${projectID}/dc/${dc}/clusters/${cluster}/nodedeployments/${nd.id}/nodes`;
+    return this.http.get<NodeEntity[]>(url, {headers: this.headers});
+  }
+
   patchNodeDeployment(
       nd: NodeDeploymentEntity, patch: NodeDeploymentPatch, clusterId: string, dc: string,
       projectID: string): Observable<NodeDeploymentEntity> {
