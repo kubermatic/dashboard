67b858390e6dc2d046f0ae7c55fcab219cfdfbaa
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-01-14 12:47:52 +0100

    Final adjustments for cluster details lists

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -39,10 +39,9 @@ export class MemberComponent implements OnInit, OnDestroy {
     }));
 
     const timer = interval(5000);
-    this.subscriptions.push(timer.subscribe(
-        (tick) => {
-            // this.refreshMembers();
-        }));
+    this.subscriptions.push(timer.subscribe(() => {
+      this.refreshMembers();
+    }));
     this.refreshMembers();
   }
 
