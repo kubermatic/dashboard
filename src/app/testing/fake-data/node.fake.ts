9882269ef753f5e7ca49348e93b395927afe0cf5
Sebastian Florek <Sebastian.Florek@loodse.com>
2019-01-14 11:35:00 +0100

    Fix tests

--- src/app/testing/fake-data/node.fake.ts
+++ src/app/testing/fake-data/node.fake.ts
@@ -372,6 +372,7 @@ export function nodeDataCentOsFake(): NodeData {
         openstack: {
           flavor: 'm1.small',
           image: '',
+          useFloatingIP: false,
         },
         hetzner: {
           type: 'cx31',
@@ -424,6 +425,7 @@ export function nodeDataContainerLinuxFake(): NodeData {
         openstack: {
           flavor: 'm1.small',
           image: '',
+          useFloatingIP: false,
         },
         hetzner: {
           type: 'cx31',
