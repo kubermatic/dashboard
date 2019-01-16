b2c1eccef829178c56230d78023f30cc48f192ea
Lukasz Szaszkiewicz <lukasz.szaszkiewicz@gmail.com>
2019-01-16 12:04:09 +0100

    updates kubeconfig name on Connect to the Cluster page (#979)
    * updates kubeconfig name on Connect to the Cluster page
    
    * update copy function

--- src/app/cluster/cluster-details/cluster-connect/cluster-connect.component.ts
+++ src/app/cluster/cluster-details/cluster-connect/cluster-connect.component.ts
@@ -22,7 +22,7 @@ export class ClusterConnectComponent {
 
   copy(type: string): string {
     if (type === 'exportKubeconfig') {
-      return 'export KUBECONFIG=$PWD/kubeconfig-' + this.cluster.id;
+      return 'export KUBECONFIG=$PWD/kubeconfig-admin-' + this.cluster.id;
     } else if (type === 'kubectlProxy') {
       return 'kubectl proxy';
     }
