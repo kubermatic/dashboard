aff88ed450681c4d923868aa91b179fe86de0b32
kgroschoff <kgroschoff@gmail.com>
2019-01-18 11:39:22 +0100

    add more tests for add member component

--- src/app/member/add-member/add-member.component.spec.ts
+++ src/app/member/add-member/add-member.component.spec.ts
@@ -1,11 +1,13 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 import {MatDialogRef} from '@angular/material';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {SlimLoadingBarModule} from 'ng2-slim-loading-bar';
+import Spy = jasmine.Spy;
 import {ApiService, ProjectService} from '../../core/services';
 import {SharedModule} from '../../shared/shared.module';
 import {fakeMember} from '../../testing/fake-data/member.fake';
+import {fakeProject} from '../../testing/fake-data/project.fake';
 import {asyncData} from '../../testing/services/api-mock.service';
 import {MatDialogRefMock} from '../../testing/services/mat-dialog-ref-mock';
 import {ProjectMockService} from '../../testing/services/project-mock.service';
@@ -21,10 +23,12 @@ const modules: any[] = [
 describe('AddProjectComponent', () => {
   let fixture: ComponentFixture<AddMemberComponent>;
   let component: AddMemberComponent;
+  let createMembersSpy: Spy;
+  let apiMock;
 
   beforeEach(async(() => {
-    const apiMock = jasmine.createSpyObj('ApiService', ['createMember']);
-    apiMock.createMember.and.returnValue(asyncData(fakeMember()));
+    apiMock = jasmine.createSpyObj('ApiService', ['createMembers']);
+    createMembersSpy = apiMock.createMembers.and.returnValue(asyncData(fakeMember()));
 
     TestBed
         .configureTestingModule({
@@ -46,9 +50,41 @@ describe('AddProjectComponent', () => {
   beforeEach(async(() => {
     fixture = TestBed.createComponent(AddMemberComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
   }));
 
   it('should create the add member component', async(() => {
        expect(component).toBeTruthy();
+     }));
+
+  it('form invalid after creating', () => {
+    expect(component.addMemberForm.valid).toBeFalsy();
+  });
+
+  it('should have required fields', () => {
+    expect(component.addMemberForm.valid).toBeFalsy('form is initially not valid');
+    expect(component.addMemberForm.controls.email.valid).toBeFalsy('email field is initially not valid');
+    expect(component.addMemberForm.controls.email.hasError('required'))
+        .toBeTruthy('email field has initially required error');
+    expect(component.addMemberForm.controls.group.valid).toBeFalsy('group field is initially not valid');
+    expect(component.addMemberForm.controls.group.hasError('required'))
+        .toBeTruthy('group field has initially required error');
+
+    component.addMemberForm.controls.email.patchValue('john@doe.com');
+    expect(component.addMemberForm.controls.email.hasError('required'))
+        .toBeFalsy('email field has no required error after setting email');
+    component.addMemberForm.controls.group.patchValue('editor');
+    expect(component.addMemberForm.controls.group.hasError('required'))
+        .toBeFalsy('group field has no required error after setting group');
+  });
+
+  it('should call addMember method', fakeAsync(() => {
+       component.project = fakeProject();
+       component.addMemberForm.controls.email.patchValue('john@doe.com');
+       component.addMemberForm.controls.group.patchValue('editor');
+       component.addMember();
+       tick();
+
+       expect(createMembersSpy.and.callThrough()).toHaveBeenCalled();
      }));
 });
