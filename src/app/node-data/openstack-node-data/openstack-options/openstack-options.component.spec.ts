5b49c2d01cb7b58cbec7e6950d011f5fa5efc413
kgroschoff <kgroschoff@gmail.com>
2019-01-18 14:26:11 +0100

    add tests for openstack options component

---
+++ src/app/node-data/openstack-node-data/openstack-options/openstack-options.component.spec.ts
@@ -0,0 +1,62 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {DatacenterService} from '../../../core/services/datacenter/datacenter.service';
+import {NodeDataService} from '../../../core/services/node-data/node-data.service';
+import {SharedModule} from '../../../shared/shared.module';
+import {fakeOpenstackCluster} from '../../../testing/fake-data/cluster.fake';
+import {nodeDataFake} from '../../../testing/fake-data/node.fake';
+import {DatacenterMockService} from '../../../testing/services/datacenter-mock.service';
+
+import {OpenstackOptionsComponent} from './openstack-options.component';
+
+const modules: any[] = [
+  BrowserModule,
+  BrowserAnimationsModule,
+  SharedModule,
+  ReactiveFormsModule,
+];
+
+describe('OpenstackOptionsComponent', () => {
+  let fixture: ComponentFixture<OpenstackOptionsComponent>;
+  let component: OpenstackOptionsComponent;
+
+  beforeEach(async(() => {
+    TestBed
+        .configureTestingModule({
+          imports: [
+            ...modules,
+          ],
+          declarations: [
+            OpenstackOptionsComponent,
+          ],
+          providers: [NodeDataService, {provide: DatacenterService, useClass: DatacenterMockService}],
+        })
+        .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OpenstackOptionsComponent);
+    component = fixture.componentInstance;
+    component.cloudSpec = fakeOpenstackCluster().spec.cloud;
+    component.nodeData = nodeDataFake();
+    fixture.detectChanges();
+  });
+
+  it('should create the vshpere options cmp', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have valid form when initializing', () => {
+    expect(component.osOptionsForm.valid).toBeTruthy();
+  });
+
+  it('should call getOsOptionsData method', () => {
+    component.osOptionsForm.controls.image.patchValue('test-image');
+    fixture.detectChanges();
+    expect(component.getOsOptionsData())
+        .toEqual({spec: {openstack: {flavor: 'm1.small', image: 'test-image', useFloatingIP: false}}, valid: true});
+  });
+});
