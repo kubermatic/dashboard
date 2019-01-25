ed755734b3006542927ad5ea916a2d8d2d1b38c6
Sebastian Florek <s.florek91@gmail.com>
2019-01-25 11:49:20 +0100

    Use headless chrome only for CI tests (#1019)
    * Use headless chrome only for CI tests
    
    * Fix e2e tests

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -3,49 +3,63 @@
 
 const {SpecReporter} = require('jasmine-spec-reporter');
 
-exports.config = {
-  allScriptsTimeout: 30000,
+exports.config = createConfig();
 
-  specs: [
-    './src/**/*.e2e-spec.ts'
-  ],
+function createConfig() {
+  const config = {
+    allScriptsTimeout: 30000,
+    specs: [
+      './src/**/*.e2e-spec.ts'
+    ],
 
-  maxSessions: 1,
-  multiCapabilities: [{
-    browserName: 'chrome',
-    chromeOptions: {
-      args: ['--headless', '--no-sandbox', '--disable-gpu', '--window-size=1920,1080']
+    maxSessions: 1,
+    multiCapabilities: [{
+      browserName: 'chrome',
+      chromeOptions: {
+        args: ['--no-sandbox', '--disable-gpu', '--window-size=1920,1080']
+      }
+    }],
+
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+      defaultTimeoutInterval: 60000,
+    },
+
+    directConnect: true,
+    baseUrl: 'http://localhost:8000/',
+
+    onPrepare() {
+      if (process.env.KUBERMATIC_DEX_DEV_E2E_USERNAME === undefined ||
+        process.env.KUBERMATIC_DEX_DEV_E2E_PASSWORD === undefined) {
+        throw new Error(`'KUBERMATIC_DEX_DEV_E2E_USERNAME' and 'KUBERMATIC_DEX_DEV_E2E_PASSWORD' environment variables have to be set.`)
+      }
+
+      browser.params.KUBERMATIC_E2E_USERNAME = process.env.KUBERMATIC_DEX_DEV_E2E_USERNAME;
+      browser.params.KUBERMATIC_E2E_PASSWORD = process.env.KUBERMATIC_DEX_DEV_E2E_PASSWORD;
+
+      require('ts-node').register({
+        project: require('path').join(__dirname, './tsconfig.e2e.json')
+      });
+
+      jasmine.getEnv().addReporter(new SpecReporter({
+        spec: {
+          displayStacktrace: true,
+          displayDuration: true,
+        },
+      }));
+
+      browser.waitForAngularEnabled(false);
     }
-  }],
+  };
 
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    defaultTimeoutInterval: 60000,
-  },
-
-  directConnect: true,
-  baseUrl: 'http://localhost:8000/',
-
-  onPrepare() {
-    if (process.env.KUBERMATIC_DEX_DEV_E2E_USERNAME === undefined ||
-      process.env.KUBERMATIC_DEX_DEV_E2E_PASSWORD === undefined) {
-      throw new Error(`'KUBERMATIC_DEX_DEV_E2E_USERNAME' and 'KUBERMATIC_DEX_DEV_E2E_PASSWORD' environment variables have to be set.`)
-    }
-
-    browser.params.KUBERMATIC_E2E_USERNAME = process.env.KUBERMATIC_DEX_DEV_E2E_USERNAME;
-    browser.params.KUBERMATIC_E2E_PASSWORD = process.env.KUBERMATIC_DEX_DEV_E2E_PASSWORD;
-
-    require('ts-node').register({
-      project: require('path').join(__dirname, './tsconfig.e2e.json')
-    });
-
-    jasmine.getEnv().addReporter(new SpecReporter({
-      spec: {
-        displayStacktrace: true,
-        displayDuration: true,
-      },
-    }));
-
-    browser.waitForAngularEnabled(false);
+  if (!!process.env.JOB_NAME) {
+    config.multiCapabilities = [{
+      browserName: 'chrome',
+      chromeOptions: {
+        args: ['--headless', '--no-sandbox', '--disable-gpu', '--window-size=1920,1080']
+      }
+    }];
   }
-};
+
+  return config;
+}
