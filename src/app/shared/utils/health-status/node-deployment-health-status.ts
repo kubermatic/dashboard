6350bcd394d44c75601f2b5d00167a6340d9ca94
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-01-25 10:48:15 +0100

    Refactor statuses for node deployments

--- src/app/shared/utils/health-status/node-deployment-health-status.ts
+++ src/app/shared/utils/health-status/node-deployment-health-status.ts
@@ -5,10 +5,10 @@ export class NodeDeploymentHealthStatus extends HealthStatus {
   static getHealthStatus(nd: NodeDeploymentEntity): NodeDeploymentHealthStatus {
     if (!!nd.deletionTimestamp) {
       return new NodeDeploymentHealthStatus(HealthStatusMessage.Deleting, HealthStatusColor.Red);
-    } else if (nd.status && nd.status.updatedReplicas !== nd.spec.replicas) {
-      return new NodeDeploymentHealthStatus(HealthStatusMessage.Updating, HealthStatusColor.Orange);
     } else if (nd.status && nd.status.availableReplicas === nd.spec.replicas) {
       return new NodeDeploymentHealthStatus(HealthStatusMessage.Running, HealthStatusColor.Green);
+    } else if (nd.status && nd.status.updatedReplicas !== nd.spec.replicas) {
+      return new NodeDeploymentHealthStatus(HealthStatusMessage.Updating, HealthStatusColor.Orange);
     } else {
       return new NodeDeploymentHealthStatus(HealthStatusMessage.Provisioning, HealthStatusColor.Orange);
     }
