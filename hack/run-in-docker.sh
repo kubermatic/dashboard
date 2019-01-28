dcb101978406a30308e20ed94a467adc688548c2
Henrik Schmidt <mrIncompetent@users.noreply.github.com>
2019-01-28 12:18:21 +0100

    Add docker helper scripts (#999)
    * add docker helper scripts
    
    * fix wrong wording
    
    * use cd for compatibility

---
+++ hack/run-in-docker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+IMAGE_NAME="node:8.15-jessie"
+REL_ROOT_DIR="$(dirname "$0")/../"
+ABS_ROOT_DIR="$(cd ${REL_ROOT_DIR}; pwd)"
+
+CMD="$@"
+docker run \
+  -u "$(id -u):$(id -g)" \
+  -v ${ABS_ROOT_DIR}:/code \
+  -w /code \
+  -p 8000:8000 \
+  -ti \
+  ${IMAGE_NAME} \
+  /bin/bash -c "${CMD}"
