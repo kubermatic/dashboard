2b745a511f981a79e89d6076cc60863216c99201
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-02-01 13:29:20 +0100

    Allow specifying default nodes count (#1032)
    * Allow specifying default nodes count
    
    * Fix tests

--- src/app/testing/fake-data/appConfig.fake.ts
+++ src/app/testing/fake-data/appConfig.fake.ts
@@ -2,6 +2,7 @@ import {Config} from '../../shared/model/Config';
 
 export function fakeAppConfig(): Config {
   return {
+    default_node_count: 3,
     show_demo_info: false,
     show_terms_of_service: false,
     share_kubeconfig: false,
