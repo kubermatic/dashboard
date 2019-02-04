0d40fc573f831a86f07a3013ff3d3554a7b29130
Sebastian Florek <Sebastian.Florek@loodse.com>
2019-02-04 10:07:21 +0100

    Update e2e test config

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -56,7 +56,7 @@ function createConfig() {
     config.multiCapabilities = [{
       browserName: 'chrome',
       chromeOptions: {
-        args: ['--headless', '--no-sandbox', '--disable-gpu', '--window-size=1920,1080']
+        args: ['--headless', '--no-sandbox', '--disable-gpu', '--window-size=1920,1080', '--disable-extensions', '--disable-dev-shm-usage', '--disable-infobars']
       }
     }];
   }
