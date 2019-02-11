03ce173c7935618cec35c303d35d5fe49e4a59c6
Sebastian Florek <Sebastian.Florek@loodse.com>
2019-02-11 15:35:13 +0100

    Make oauth configurable

--- set-env.ts
+++ set-env.ts
@@ -1,4 +1,8 @@
-// The file contents for the current environment will overwrite these during build.
+import {writeFile} from 'fs';
+
+const oauth = process.env.npm_config_oauth ? process.env.npm_config_oauth : 'https://dev.kubermatic.io/dex/auth';
+const targetPath = './src/environments/environment.ts';
+const envConfigFile = `// The file contents for the current environment will overwrite these during build.
 // The build system defaults to the dev environment which uses 'environment.ts', but if you do
 // 'ng build --env=prod' then 'environment.prod.ts' will be used instead.
 // The list of which env maps to which file can be found in 'angular-cli.json'.
@@ -6,7 +10,7 @@
 import {RandomString} from '../app/shared/functions/generate-random-string';
 
 const redirect_uri = window.location.protocol + '//' + window.location.host + '/projects';
-const oauth = 'https://dev.kubermatic.io/dex/auth';
+const oauth = '${oauth}';
 const scope: string[] = ['openid', 'email', 'profile', 'groups'];
 const nonceString = RandomString(32);
 
@@ -21,3 +25,12 @@ export const environment = {
   coreOSdexAuth: oauth + '?response_type=id_token&client_id=kubermatic&redirect_uri=' + redirect_uri +
       '&scope=' + scope.join(' ') + '&nonce=' + nonceString,
 };
+`;
+
+writeFile(targetPath, envConfigFile, (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(`Output generated at ${targetPath}`);
+});
