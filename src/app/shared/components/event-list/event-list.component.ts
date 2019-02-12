d2df0826778c64db99d0a81b30001a5af809fbec
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-02-12 16:48:54 +0100

    Display nodes events in the node deployment details view (#1054)
    * Add events to Node Deployment details
    
    * Initialize event list component
    
    * Style event table
    
    * Add paginator and sort to the table
    
    * Allow to set involved object kind label
    
    * Remove empty file
    
    * Fix tests

---
+++ src/app/shared/components/event-list/event-list.component.ts
@@ -0,0 +1,36 @@
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+
+import {EventEntity} from '../../entity/EventEntity';
+
+@Component({
+  selector: 'km-event-list',
+  templateUrl: './event-list.component.html',
+})
+export class EventListComponent implements OnInit {
+  @Input() events: EventEntity[] = [];
+  @Input() involvedObjectColumnName = 'Involved Object';
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  dataSource = new MatTableDataSource<EventEntity>();
+  displayedColumns: string[] = ['type', 'message', 'involvedObjectName'];
+
+  ngOnInit(): void {
+    this.dataSource.data = this.events;
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  getTypeIcon(event: EventEntity): string {
+    switch (event.type) {
+      case 'Normal':
+        return 'fa fa-info-circle green';
+      case 'Warning':
+        return 'fa fa-exclamation-circle orange';
+      default:
+        return 'fa fa-question-circle';
+    }
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,14 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatDialogModule, MatDividerModule, MatExpansionModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatOptionModule, MatProgressBarModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule} from '@angular/material';
+import {MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatCardModule, MatCheckboxModule, MatDialogModule, MatDividerModule, MatExpansionModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatOptionModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule} from '@angular/material';
 import {SlimLoadingBarModule} from 'ng2-slim-loading-bar';
 import {CustomFormsModule} from 'ng2-validation';
 import {ClipboardModule} from 'ngx-clipboard';
+
 import {AddSshKeyModalComponent} from './components/add-ssh-key-modal/add-ssh-key-modal.component';
 import {ConfirmationDialogComponent} from './components/confirmation-dialog/confirmation-dialog.component';
+import {EventListComponent} from './components/event-list/event-list.component';
 import {PropertyComponent} from './components/property/property.component';
 
 const modules: any[] = [
@@ -48,6 +50,7 @@ const modules: any[] = [
   MatTabsModule,
   MatOptionModule,
   MatFormFieldModule,
+  MatPaginatorModule,
 ];
 
 @NgModule({
@@ -58,12 +61,14 @@ const modules: any[] = [
     AddSshKeyModalComponent,
     ConfirmationDialogComponent,
     PropertyComponent,
+    EventListComponent,
   ],
   exports: [
     ...modules,
     AddSshKeyModalComponent,
     ConfirmationDialogComponent,
     PropertyComponent,
+    EventListComponent,
   ],
   entryComponents: [
     AddSshKeyModalComponent,
