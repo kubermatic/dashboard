cd19b7a9c4691e9b813168cbe851d09aa5ee7009
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-02-12 10:49:36 +0100

    Style event table

--- src/app/shared/components/event-list/event-list.component.ts
+++ src/app/shared/components/event-list/event-list.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input} from '@angular/core';
+import {MatTableDataSource} from '@angular/material';
+
 import {EventEntity} from '../../entity/EventEntity';
 
 @Component({
@@ -8,4 +10,25 @@ import {EventEntity} from '../../entity/EventEntity';
 })
 export class EventListComponent {
   @Input() events: EventEntity[] = [];
+
+  displayedColumns: string[] = ['type', 'message', 'involvedObjectName'];
+
+  // todo sort pagination
+
+  getDataSource(): MatTableDataSource<EventEntity> {
+    const dataSource = new MatTableDataSource<EventEntity>();
+    dataSource.data = this.events;
+    return dataSource;
+  }
+
+  getTypeIcon(event: EventEntity): string {
+    switch (event.type) {
+      case 'Normal':
+        return 'fa fa-info-circle green';
+      case 'Warning':
+        return 'fa fa-exclamation-circle orange';
+      default:
+        return 'fa fa-question-circle';
+    }
+  }
 }
