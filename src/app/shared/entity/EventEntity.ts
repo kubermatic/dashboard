caaec78a8e044817b6dd2dc3abcaae10962a3f54
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-02-13 17:00:56 +0100

    Update Event list with count and last occurrence information (#1062)

--- src/app/shared/entity/EventEntity.ts
+++ src/app/shared/entity/EventEntity.ts
@@ -1,5 +1,11 @@
 export class EventEntity {
-  name: string;
+  creationTimestamp?: Date;
+  deletionTimestamp?: Date;
+  id?: string;
+  name?: string;
   message?: string;
   type?: string;
+  involvedObjectName: string;
+  lastTimestamp: Date;
+  count: number;
 }
