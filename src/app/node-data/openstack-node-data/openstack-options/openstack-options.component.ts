a7e21917d60ae89982e26c8c5c523b22935f7bc9
Kristin Groschoff <kgroschoff@gmail.com>
2019-02-26 10:24:59 +0100

    Use existing image when editing node deployments on openstack (#1076)
    * use existing image
    
    * simplify code
    
    * simplify

--- src/app/node-data/openstack-node-data/openstack-options/openstack-options.component.ts
+++ src/app/node-data/openstack-node-data/openstack-options/openstack-options.component.ts
@@ -46,7 +46,11 @@ export class OpenstackOptionsComponent implements OnInit, OnDestroy {
     }));
 
     this.subscriptions.push(this.addNodeService.nodeOperatingSystemDataChanges$.subscribe((data) => {
-      this.setImage(data);
+      if ((!!this.nodeData.spec.operatingSystem.ubuntu && !data.ubuntu) ||
+          (!!this.nodeData.spec.operatingSystem.centos && !data.centos) ||
+          (!!this.nodeData.spec.operatingSystem.containerLinux && !data.containerLinux)) {
+        this.setImage(data);
+      }
       this.addNodeService.changeNodeProviderData(this.getOsOptionsData());
     }));
 
