df89a5b73407f746ab9a5bcc9aaaf13045162426
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-03-01 16:17:34 +0100

    Add more tests

--- src/app/shared/validators/label-form.validator.spec.ts
+++ src/app/shared/validators/label-form.validator.spec.ts
@@ -1,35 +1,65 @@
 import {FormControl} from '@angular/forms';
 import {LabelFormValidators} from './label-form.validators';
 
-const char8 = 'fG5d3rh8';
-const char64 = 'Zr8bVgxVyLc4VbEa92Gkz97i4wIUCyiECNYKdYejvi1VeCG4axjJaZ232ok69ayk';
-const char256 = 'je85JTLp2AftZ7YtSwdf0m2K4yrwd6l8ynEQWICfgFjAfDgRDw3wNpbqsNEhItM' +
-    'OADHbknuQsE1rP643smxH4ZHRlZqu9zvB8odvkwQC90jlYnzuK20DIgK7K9FctcB6frA1Y2n2KGYv' +
-    '82SHI2C1p9XmmSinNtRAdOYYCiKZcY41jRFw4i7F3UxAtLCUsLKxG3FJbiPf0EJDvAUDnv2Ktr7nI' +
-    'C9Q8hIumOMFzEc61YgQpXiRAeqpg9CDmTQJSeum';
+const validKey = 'env';
+const tooLongKey = 'F4td6JTLp2AftZ7YtSwdf0m2K4yrwd6l8ynEQWICfgFjAfDgRDw3wNpbqsNMuTQg' +
+    'OADHbknuQsE1rP643smxH4ZHRlZqu9zvB8odvkwQC90jlYnzuK20DIgK7K9FctcB6frA1Y2n2KGYvm9' +
+    '82SHI2C1p9XmmSinNtRAdOYYCiKZcYjRFw4i7F3UxAtLCUsLKxG3FJbiPf0EJDvAUDnv2Ktr7nIEhtC' +
+    'C9Q8hIumOMFzEc61YgQpXiRAeqpDmJSegE4';
+const invalidKey = '!@#$%^&*';
+const validPrefix = 'beta.kubernetes.io';
+const tooLongPrefix = 'beta.JTLp2AftZ7YtSwdf0m2K4yrwd6l8ynEQWICfgFjAfDgRDw3wNpbqsNMu' +
+    'OADHbknuQsE1rP643smxH4ZHRlZqu9zvB8odvkwQC90jlYnzuK20DIgK7K9FctcB6frA1Y2n2KGYvth' +
+    '82SHI2C1p9XmmSinNtRAdOYYCiKZcYjRFw4i7F3UxAtLCUsLKxG3FJbiPf0EJDvAUDnv2Ktr7nIEpgD' +
+    'C9Q8hIumOMFzEc61YgQpXiRAeq9CmTQmJSe.io';
+const invalidPrefix = '!@#$%^&*';
 
 describe('LabelFormValidators', () => {
   it('labelKeyNameLength should be valid', () => {
-    const control = {value: char8} as FormControl;
+    const control = {value: validKey} as FormControl;
     const result = LabelFormValidators.labelKeyNameLength(control);
     expect(result).toBe(null);
   });
 
   it('labelKeyNameLength should be invalid', () => {
-    const control = {value: char64} as FormControl;
+    const control = {value: tooLongKey} as FormControl;
     const result = LabelFormValidators.labelKeyNameLength(control);
     expect(result).not.toBe(null);
   });
 
   it('labelKeyPrefixLength should be valid', () => {
-    const control = {value: `${char8}/${char8}`} as FormControl;
+    const control = {value: `${validPrefix}/${validKey}`} as FormControl;
     const result = LabelFormValidators.labelKeyPrefixLength(control);
     expect(result).toBe(null);
   });
 
   it('labelKeyPrefixLength should be invalid', () => {
-    const control = {value: `${char256}/${char8}`} as FormControl;
+    const control = {value: `${tooLongPrefix}/${validKey}`} as FormControl;
     const result = LabelFormValidators.labelKeyPrefixLength(control);
+    expect(result).not.toBe(null);
+  });
+
+  it('labelKeyNamePattern should be valid', () => {
+    const control = {value: `${validKey}`} as FormControl;
+    const result = LabelFormValidators.labelKeyNamePattern(control);
+    expect(result).toBe(null);
+  });
+
+  it('labelKeyNamePattern should be invalid', () => {
+    const control = {value: `${invalidKey}`} as FormControl;
+    const result = LabelFormValidators.labelKeyNamePattern(control);
+    expect(result).not.toBe(null);
+  });
+
+  it('labelKeyPrefixPattern should be valid', () => {
+    const control = {value: `${validPrefix}/${validKey}`} as FormControl;
+    const result = LabelFormValidators.labelKeyPrefixPattern(control);
+    expect(result).toBe(null);
+  });
+
+  it('labelKeyPrefixPattern should be invalid', () => {
+    const control = {value: `${invalidPrefix}/${validKey}`} as FormControl;
+    const result = LabelFormValidators.labelKeyPrefixPattern(control);
     expect(result).not.toBe(null);
   });
 });
