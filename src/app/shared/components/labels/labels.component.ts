9650b67bf752280f1b27ea2e04ce4fd4d8af6aed
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-03-05 12:51:11 +0100

    Add labels for node deployments (#1101)
    * Add label form component
    
    * Initialize labels
    Use object to represent labels
    
    * Add option to toggle visibility
    Add slide animation
    
    * Add label form validators
    
    * Display validation errors
    
    * Update labels object after label deletion
    
    * Nullify removed label values to fix delete issue
    
    * Fix formatting
    
    * Fix wording
    
    * Fix wording
    
    * Fix existing tests
    
    * Add more tests
    
    * Add more tests
    
    * Add more tests
    
    * Add more tests
    
    * Add few more tests
    
    * Change add node modal size
    Adjust label form size
    
    * Add component to display labels
    
    * Allow setting key-only labels
    
    * Filter nullified labels before node deployment creation
    
    * Adjust wizard summary page

---
+++ src/app/shared/components/labels/labels.component.ts
@@ -0,0 +1,30 @@
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+
+@Component({
+  selector: 'km-labels',
+  templateUrl: './labels.component.html',
+})
+export class LabelsComponent implements OnInit, OnChanges {
+  @Input() labels: object = {};
+  labelKeys: string[] = [];
+
+  ngOnInit(): void {
+    this._updateLabelKeys();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this._updateLabelKeys();
+  }
+
+  private _updateLabelKeys(): void {
+    this.labelKeys = [];
+    if (this.labels instanceof Object) {
+      Object.keys(this.labels).forEach(key => {
+        // Do not display nullified (marked for removal) labels.
+        if (this.labels[key] !== null) {
+          this.labelKeys.push(key);
+        }
+      });
+    }
+  }
+}
