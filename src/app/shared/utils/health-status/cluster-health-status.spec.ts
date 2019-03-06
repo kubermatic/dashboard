e4c86f6ace9fc650381cca913d908bc80edfa855
kgroschoff <kgroschoff@gmail.com>
2019-03-06 14:54:01 +0100

    add new cluster health status

---
+++ src/app/shared/utils/health-status/cluster-health-status.spec.ts
@@ -0,0 +1,33 @@
+import {fakeDigitaloceanCluster} from '../../../testing/fake-data/cluster.fake';
+import {fakeHealth, fakeHealthFailed, fakeHealthProvisioning} from '../../../testing/fake-data/health.fake';
+
+import {ClusterHealthStatus, HealthStatusCss} from './cluster-health-status';
+import {HealthStatusColor, HealthStatusMessage} from './health-status';
+
+describe('ClusterHealthStatus', () => {
+  const cluster = fakeDigitaloceanCluster();
+  const health = fakeHealth();
+  const healthProvisioning = fakeHealthProvisioning();
+  const healthFailed = fakeHealthFailed();
+
+  it('should return correct status for clusters', () => {
+    expect(ClusterHealthStatus.getHealthStatus(cluster, health))
+        .toEqual(
+            new ClusterHealthStatus(HealthStatusMessage.Running, HealthStatusColor.Green, HealthStatusCss.Running),
+            'should be correct for running cluster');
+    expect(ClusterHealthStatus.getHealthStatus(cluster, healthProvisioning))
+        .toEqual(
+            new ClusterHealthStatus(
+                HealthStatusMessage.Provisioning, HealthStatusColor.Orange, HealthStatusCss.Provisioning),
+            'should be correct for provisioning cluster');
+    expect(ClusterHealthStatus.getHealthStatus(cluster, healthFailed))
+        .toEqual(
+            new ClusterHealthStatus(HealthStatusMessage.Failed, HealthStatusColor.Red, HealthStatusCss.Failed),
+            'should be correct for failed cluster');
+  });
+
+  it('should return if cluster is running', () => {
+    expect(ClusterHealthStatus.isClusterRunning(cluster, health)).toBeTruthy();
+    expect(ClusterHealthStatus.isClusterRunning(cluster, healthFailed)).toBeFalsy();
+  });
+});
