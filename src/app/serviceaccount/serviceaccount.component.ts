c6af1ddd4fda50a08ccad2c816923e55ca4c20c4
kgroschoff <kgroschoff@gmail.com>
2019-04-03 11:19:38 +0200

    add status icon

--- src/app/serviceaccount/serviceaccount.component.ts
+++ src/app/serviceaccount/serviceaccount.component.ts
@@ -85,7 +85,20 @@ export class ServiceAccountComponent implements OnInit, OnDestroy {
     }
   }
 
-  getGroupDisplayName(group: string) {
+  getStateIconClass(status: string): string {
+    switch (status) {
+      case 'Active':
+        return 'fa fa-circle green';
+      case 'Inactive':
+        return 'fa fa-circle-o red';
+      case 'Terminating':
+        return 'fa fa-spin fa-circle-o-notch orange';
+      default:
+        return 'fa fa-spin fa-circle-o-notch orange';
+    }
+  }
+
+  getGroupDisplayName(group: string): string {
     const prefix = group.split('-')[0];
     switch (prefix) {
       case 'owners':
