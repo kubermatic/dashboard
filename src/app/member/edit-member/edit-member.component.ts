64fabb603871d85c59762b85f7a0f170ba866ccf
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-04-03 13:47:44 +0200

    Refactor members (#1152)
    * Refactor members component
    
    * Refactor members dialogs
    
    * Extract member utils
    Initialize edit with correct value
    
    * Fix tests
    
    * Use timer instead of interval
    
    * Fix imports
    
    * Fix review findings
    
    * Fix review findings
    
    * Fix review findings

--- src/app/member/edit-member/edit-member.component.ts
+++ src/app/member/edit-member/edit-member.component.ts
@@ -5,6 +5,7 @@ import {ApiService} from '../../core/services';
 import {NotificationActions} from '../../redux/actions/notification.actions';
 import {MemberEntity} from '../../shared/entity/MemberEntity';
 import {ProjectEntity} from '../../shared/entity/ProjectEntity';
+import {MemberUtils} from '../../shared/utils/member-utils/member-utils';
 
 @Component({
   selector: 'kubermatic-edit-member',
@@ -15,11 +16,12 @@ export class EditMemberComponent implements OnInit {
   @Input() member: MemberEntity;
   editMemberForm: FormGroup;
 
-  constructor(private api: ApiService, private dialogRef: MatDialogRef<EditMemberComponent>) {}
+  constructor(
+      private readonly _apiService: ApiService, private readonly _matDialogRef: MatDialogRef<EditMemberComponent>) {}
 
   ngOnInit(): void {
     this.editMemberForm = new FormGroup({
-      group: new FormControl('', [Validators.required]),
+      group: new FormControl(MemberUtils.getGroupInProject(this.member, this.project.id), [Validators.required]),
     });
   }
 
@@ -38,8 +40,8 @@ export class EditMemberComponent implements OnInit {
       ],
     };
 
-    this.api.editMembers(this.project.id, editMember).subscribe((res) => {
-      this.dialogRef.close(true);
+    this._apiService.editMembers(this.project.id, editMember).subscribe(() => {
+      this._matDialogRef.close(true);
       NotificationActions.success('Success', `Member ${this.member.name} is edited successfully`);
     });
   }
