bff3e9d33ab0c5411e69661e19bd41c80f3c93e6
kgroschoff <kgroschoff@gmail.com>
2019-04-03 10:07:24 +0200

    fix tests

--- src/app/serviceaccount/edit-serviceaccount/edit-serviceaccount.component.spec.ts
+++ src/app/serviceaccount/edit-serviceaccount/edit-serviceaccount.component.spec.ts
@@ -56,25 +56,8 @@ describe('EditServiceAccountComponent', () => {
        expect(component).toBeTruthy();
      }));
 
-  it('should have invalid form after creating', () => {
-    expect(component.editServiceAccountForm.valid).toBeFalsy();
-  });
-
-  it('should have required fields', () => {
-    expect(component.editServiceAccountForm.valid).toBeFalsy('form is initially not valid');
-    expect(component.editServiceAccountForm.controls.name.valid).toBeFalsy('name field is initially not valid');
-    expect(component.editServiceAccountForm.controls.name.hasError('required'))
-        .toBeTruthy('name field has initially required error');
-    expect(component.editServiceAccountForm.controls.group.valid).toBeFalsy('group field is initially not valid');
-    expect(component.editServiceAccountForm.controls.group.hasError('required'))
-        .toBeTruthy('group field has initially required error');
-
-    component.editServiceAccountForm.controls.name.patchValue('test-service-account');
-    expect(component.editServiceAccountForm.controls.name.hasError('required'))
-        .toBeFalsy('name field has no required error after setting name');
-    component.editServiceAccountForm.controls.group.patchValue('editor');
-    expect(component.editServiceAccountForm.controls.group.hasError('required'))
-        .toBeFalsy('group field has no required error after setting group');
+  it('should have valid form after creating', () => {
+    expect(component.editServiceAccountForm.valid).toBeTruthy();
   });
 
   it('should call editServiceAccount method', fakeAsync(() => {
--- src/app/testing/fake-data/serviceaccount.fake.ts
+++ src/app/testing/fake-data/serviceaccount.fake.ts
@@ -2,13 +2,13 @@ import {ServiceAccountEntity} from '../../shared/entity/ServiceAccountEntity';
 
 export function fakeServiceAccounts(): ServiceAccountEntity[] {
   return [
-    {creationTimestamp: new Date(), id: '987zy6xv5u', name: 'test-service-account', status: 'Active', group: 'owners'},
+    {creationTimestamp: new Date(), id: '987zy6xv5u', name: 'test-service-account', status: 'Active', group: 'editors'},
     {
       creationTimestamp: new Date(),
       id: '765zy4xv3u',
       name: 'test-service-account-2',
       status: 'Active',
-      group: 'editors'
+      group: 'viewers'
     },
   ];
 }
@@ -19,6 +19,6 @@ export function fakeServiceAccount(): ServiceAccountEntity {
     id: '987zy6xv5u',
     name: 'test-service-account',
     status: 'Active',
-    group: 'owners'
+    group: 'editors'
   };
 }
