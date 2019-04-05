ed399b7b8f11d56bd3a79caa9cb5bff053d5577d
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-04-05 09:42:05 +0200

    Use switch map

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {MatDialog, MatDialogConfig, MatSort, MatTableDataSource} from '@angular/material';
 import {merge, Subject, timer} from 'rxjs';
-import {first, takeUntil} from 'rxjs/operators';
+import {first, switchMap, takeUntil} from 'rxjs/operators';
 
 import {ApiService, ProjectService, UserService} from '../core/services';
 import {GoogleAnalyticsService} from '../google-analytics.service';
@@ -40,15 +40,16 @@ export class ProjectComponent implements OnInit, OnDestroy {
     this.sort.active = 'name';
     this.sort.direction = 'asc';
 
-    merge(timer(0, 10000), this._externalProjectsUpdate).pipe(takeUntil(this._unsubscribe)).subscribe(() => {
-      this._apiService.getProjects().pipe(first()).subscribe(projects => {
-        this.projects = projects;
-        this._sortProjectOwners();
-        this._loadClusterCounts();
-        this._loadCurrentUserRoles();
-        this.isInitializing = false;
-      });
-    });
+    merge(timer(0, 10000), this._externalProjectsUpdate)
+        .pipe(takeUntil(this._unsubscribe))
+        .pipe(switchMap(() => this._apiService.getProjects()))
+        .subscribe(projects => {
+          this.projects = projects;
+          this._sortProjectOwners();
+          this._loadClusterCounts();
+          this._loadCurrentUserRoles();
+          this.isInitializing = false;
+        });
   }
 
   ngOnDestroy(): void {
