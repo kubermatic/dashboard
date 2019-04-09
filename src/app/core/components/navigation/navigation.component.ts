9a616db90168c34c61f2d776144cae9a2c4a6f1a
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-04-09 19:29:46 +0200

    Fix imports

--- src/app/core/components/navigation/navigation.component.ts
+++ src/app/core/components/navigation/navigation.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {MatDialog} from '@angular/material';
 import {Router} from '@angular/router';
 import {environment} from '../../../../environments/environment';
 import {AppConstants} from '../../../shared/constants/constants';
