5c6b87317bed1ddd4783794259182d652e2b5870
kgroschoff <kgroschoff@gmail.com>
2019-04-15 18:43:03 +0200

    mock confirmation dialog; test components

--- src/app/cluster/cluster-details/edit-sshkeys/edit-sshkeys-item/edit-sshkeys-item.component.spec.ts
+++ src/app/cluster/cluster-details/edit-sshkeys/edit-sshkeys-item/edit-sshkeys-item.component.spec.ts
@@ -1,36 +1,42 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 import {MatDialog} from '@angular/material';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {SlimLoadingBarModule} from 'ng2-slim-loading-bar';
 import {of} from 'rxjs';
+import Spy = jasmine.Spy;
+
 import {AppConfigService} from '../../../../app-config.service';
 import {ApiService, UserService} from '../../../../core/services';
 import {GoogleAnalyticsService} from '../../../../google-analytics.service';
 import {SharedModule} from '../../../../shared/shared.module';
+import {DialogTestModule, NoopConfirmDialogComponent} from '../../../../testing/components/noop-confirmation-dialog.component';
+import {fakeDigitaloceanCluster} from '../../../../testing/fake-data/cluster.fake';
+import {fakeDigitaloceanDatacenter} from '../../../../testing/fake-data/datacenter.fake';
+import {fakeSSHKeys} from '../../../../testing/fake-data/sshkey.fake';
+import {fakeProject} from '../../../../testing/fake-data/project.fake';
 import {AppConfigMockService} from '../../../../testing/services/app-config-mock.service';
 import {UserMockService} from '../../../../testing/services/user-mock.service';
 import {EditSSHKeysItemComponent} from './edit-sshkeys-item.component';
 
-const modules: any[] = [
-  BrowserModule,
-  BrowserAnimationsModule,
-  SlimLoadingBarModule.forRoot(),
-  SharedModule,
-];
-
 describe('EditSSHKeysItemComponent', () => {
   let fixture: ComponentFixture<EditSSHKeysItemComponent>;
+  let noop: ComponentFixture<NoopConfirmDialogComponent>;
   let component: EditSSHKeysItemComponent;
+  let deleteClusterSSHKeySpy: Spy;
 
   beforeEach(async(() => {
     const apiMock = jasmine.createSpyObj('ApiService', ['deleteClusterSSHKey']);
-    apiMock.deleteClusterSSHKey.and.returnValue(of(null));
+    deleteClusterSSHKeySpy = apiMock.deleteClusterSSHKey.and.returnValue(of(null));
 
     TestBed
         .configureTestingModule({
           imports: [
-            ...modules,
+            BrowserModule,
+            BrowserAnimationsModule,
+            SlimLoadingBarModule.forRoot(),
+            SharedModule,
+            DialogTestModule,
           ],
           declarations: [
             EditSSHKeysItemComponent,
@@ -49,9 +55,38 @@ describe('EditSSHKeysItemComponent', () => {
   beforeEach(async(() => {
     fixture = TestBed.createComponent(EditSSHKeysItemComponent);
     component = fixture.componentInstance;
+    noop = TestBed.createComponent(NoopConfirmDialogComponent);
   }));
 
   it('should create the edit sshkeys item component', async(() => {
        expect(component).toBeTruthy();
+     }));
+
+  it('should open delete cluster ssh key confirmation dialog & call deleteSshKey()', fakeAsync(() => {
+       component.cluster = fakeDigitaloceanCluster();
+       component.datacenter = fakeDigitaloceanDatacenter();
+       component.projectID = fakeProject().id;
+       component.sshKey = fakeSSHKeys()[0];
+       fixture.detectChanges();
+
+       component.deleteSshKey();
+       noop.detectChanges();
+       tick(15000);
+
+       const dialogTitle = document.body.querySelector('.km-dialog-title');
+       const cancelButton = document.body.querySelector('#km-confirmation-dialog-cancel-btn');
+       const deleteButton = document.body.querySelector('#km-confirmation-dialog-confirm-btn') as HTMLInputElement;
+
+       expect(dialogTitle.textContent).toBe('Remove SSH key from cluster');
+       expect(cancelButton.textContent).toBe(' Close ');
+       expect(deleteButton.textContent).toBe(' Delete ');
+
+       deleteButton.click();
+
+       noop.detectChanges();
+       fixture.detectChanges();
+       tick(15000);
+
+       expect(deleteClusterSSHKeySpy.and.callThrough()).toHaveBeenCalled();
      }));
 });
--- src/app/project/project.component.spec.ts
+++ src/app/project/project.component.spec.ts
@@ -1,28 +1,41 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 import {MatDialog} from '@angular/material';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {Router} from '@angular/router';
 import {SlimLoadingBarModule} from 'ng2-slim-loading-bar';
+import {of} from 'rxjs';
+import Spy = jasmine.Spy;
 
 import {AppConfigService} from '../app-config.service';
 import {ApiService, DatacenterService, ProjectService, UserService} from '../core/services';
 import {GoogleAnalyticsService} from '../google-analytics.service';
 import {SharedModule} from '../shared/shared.module';
+import {DialogTestModule, NoopConfirmDialogComponent} from '../testing/components/noop-confirmation-dialog.component';
+import {fakeClusters} from '../testing/fake-data/cluster.fake';
+import {fakeProject, fakeProjects} from '../testing/fake-data/project.fake';
 import {RouterStub, RouterTestingModule} from '../testing/router-stubs';
-import {ApiMockService} from '../testing/services/api-mock.service';
+import {asyncData} from '../testing/services/api-mock.service';
 import {AppConfigMockService} from '../testing/services/app-config-mock.service';
 import {DatacenterMockService} from '../testing/services/datacenter-mock.service';
 import {ProjectMockService} from '../testing/services/project-mock.service';
 import {UserMockService} from '../testing/services/user-mock.service';
-
 import {ProjectComponent} from './project.component';
+
 
 describe('ProjectComponent', () => {
   let fixture: ComponentFixture<ProjectComponent>;
   let component: ProjectComponent;
+  let noop: ComponentFixture<NoopConfirmDialogComponent>;
+  let apiMock;
+  let deleteProjectSpy: Spy;
 
   beforeEach(async(() => {
+    apiMock = jasmine.createSpyObj('ApiService', ['getProjects', 'deleteProject', 'getAllClusters']);
+    deleteProjectSpy = apiMock.deleteProject.and.returnValue(of(null));
+    apiMock.getProjects.and.returnValue(asyncData(fakeProjects()));
+    apiMock.getAllClusters.and.returnValue(asyncData(fakeClusters()));
+
     TestBed
         .configureTestingModule({
           imports: [
@@ -31,11 +44,12 @@ describe('ProjectComponent', () => {
             SlimLoadingBarModule.forRoot(),
             RouterTestingModule,
             SharedModule,
+            DialogTestModule,
           ],
           declarations: [ProjectComponent],
           providers: [
             {provide: Router, useClass: RouterStub},
-            {provide: ApiService, useClass: ApiMockService},
+            {provide: ApiService, useValue: apiMock},
             {provide: ProjectService, useClass: ProjectMockService},
             {provide: UserService, useClass: UserMockService},
             {provide: AppConfigService, useClass: AppConfigMockService},
@@ -50,6 +64,7 @@ describe('ProjectComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProjectComponent);
     component = fixture.componentInstance;
+    noop = TestBed.createComponent(NoopConfirmDialogComponent);
     fixture.detectChanges();
     fixture.debugElement.injector.get(Router);
   });
@@ -57,4 +72,37 @@ describe('ProjectComponent', () => {
   it('should create project cmp', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should open delete project confirmation dialog & call deleteProject()', fakeAsync(() => {
+       const project = fakeProject();
+       const event = new MouseEvent('click');
+
+       component.deleteProject(project, event);
+       noop.detectChanges();
+       fixture.detectChanges();
+       tick(15000);
+
+       const dialogTitle = document.body.querySelector('.km-dialog-title');
+       const cancelButton = document.body.querySelector('#km-confirmation-dialog-cancel-btn');
+       const deleteButton = document.body.querySelector('#km-confirmation-dialog-confirm-btn') as HTMLInputElement;
+       const dialogInput = document.querySelector('#km-confirmation-dialog-input');
+
+       dialogInput.setAttribute('value', project.name);
+       deleteButton.disabled = false;
+
+       noop.detectChanges();
+       fixture.detectChanges();
+
+       expect(dialogTitle.textContent).toBe('Delete Project');
+       expect(cancelButton.textContent).toBe(' Close ');
+       expect(document.querySelector('#km-confirmation-dialog-input').getAttribute('value')).toBe(project.name);
+
+       deleteButton.click();
+
+       noop.detectChanges();
+       fixture.detectChanges();
+       tick(15000);
+
+       expect(deleteProjectSpy.and.callThrough()).toHaveBeenCalled();
+     }));
 });
