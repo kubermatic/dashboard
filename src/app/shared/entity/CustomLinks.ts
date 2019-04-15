8b7bdae1e701d74e70cd322575f1c2490f5763b8
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-04-15 16:49:48 +0200

    Add an option to include custom links into the application (#1131)
    * Add sample config JSON
    
    * Add custom links to the app config service
    
    * Load data during app init
    
    * Display links in the UI
    
    * Add custom links header
    
    * Change custom links panel styling
    
    * Add custom icons
    
    * Add method to determine icon to display
    
    * Allow specifying icon URL
    
    * Add JFrog and Twitter icons
    
    * Remove social icons from the footer
    
    * Fix unit tests
    
    * Move custom links config to config.json
    
    * Highlight custom icons too
    
    * Remove custom links from default config
    
    * Remove unused config

---
+++ src/app/shared/entity/CustomLinks.ts
@@ -0,0 +1,40 @@
+export class CustomLink {
+  label: string;
+  url: string;
+  icon?: CustomLinkIcon|string;
+}
+
+export enum CustomLinkIcon {
+  Default = '/assets/images/icons/custom/default.svg',
+  GitHub = '/assets/images/icons/custom/github.svg',
+  Grafana = '/assets/images/icons/custom/grafana.svg',
+  JFrog = '/assets/images/icons/custom/jfrog.svg',
+  Kibana = '/assets/images/icons/custom/kibana.svg',
+  Prometheus = '/assets/images/icons/custom/prometheus.svg',
+  Slack = '/assets/images/icons/custom/slack.svg',
+  Twitter = '/assets/images/icons/custom/twitter.svg',
+}
+
+export function findMatchingServiceIcon(customLink: CustomLink) {
+  if (isMatching(customLink, 'github')) {
+    return CustomLinkIcon.GitHub;
+  } else if (isMatching(customLink, 'grafana')) {
+    return CustomLinkIcon.Grafana;
+  } else if (isMatching(customLink, 'kibana')) {
+    return CustomLinkIcon.Kibana;
+  } else if (isMatching(customLink, 'prometheus')) {
+    return CustomLinkIcon.Prometheus;
+  } else if (isMatching(customLink, 'slack')) {
+    return CustomLinkIcon.Slack;
+  } else if (isMatching(customLink, 'twitter')) {
+    return CustomLinkIcon.Twitter;
+  } else if (isMatching(customLink, 'jfrog')) {
+    return CustomLinkIcon.JFrog;
+  } else {
+    return CustomLinkIcon.Default;
+  }
+}
+
+function isMatching(customLink: CustomLink, service: string) {
+  return customLink.label.toLowerCase().includes(service) || customLink.url.toLowerCase().includes(service);
+}
