c2daa6328ff175436a830265c0e074ed020a48e0
kgroschoff <kgroschoff@gmail.com>
2019-04-16 15:34:52 +0200

    add tests for cluster secrets component

--- src/app/cluster/cluster-details/change-cluster-version/change-cluster-version.component.spec.ts
+++ src/app/cluster/cluster-details/change-cluster-version/change-cluster-version.component.spec.ts
@@ -4,12 +4,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {Router} from '@angular/router';
 import {SlimLoadingBarModule} from 'ng2-slim-loading-bar';
+import {of} from 'rxjs';
+import Spy = jasmine.Spy;
+
 import {ApiService, ProjectService} from '../../../core/services';
 import {GoogleAnalyticsService} from '../../../google-analytics.service';
 import {SharedModule} from '../../../shared/shared.module';
 import {fakeDigitaloceanCluster} from '../../../testing/fake-data/cluster.fake';
-import Spy = jasmine.Spy;
 import {fakeDigitaloceanDatacenter} from '../../../testing/fake-data/datacenter.fake';
+import {fakeProject} from '../../../testing/fake-data/project.fake';
 import {RouterStub} from '../../../testing/router-stubs';
 import {asyncData} from '../../../testing/services/api-mock.service';
 import {MatDialogRefMock} from '../../../testing/services/mat-dialog-ref-mock';
@@ -27,10 +30,12 @@ describe('ChangeClusterVersionComponent', () => {
   let fixture: ComponentFixture<ChangeClusterVersionComponent>;
   let component: ChangeClusterVersionComponent;
   let patchClusterSpy: Spy;
+  let upgradeClusterNodeDeploymentsSpy: Spy;
 
   beforeEach(async(() => {
-    const apiMock = jasmine.createSpyObj('ApiService', ['patchCluster']);
+    const apiMock = jasmine.createSpyObj('ApiService', ['patchCluster', 'upgradeClusterNodeDeployments']);
     patchClusterSpy = apiMock.patchCluster.and.returnValue(asyncData(fakeDigitaloceanCluster()));
+    upgradeClusterNodeDeploymentsSpy = apiMock.upgradeClusterNodeDeployments.and.returnValue(of(null));
 
     TestBed
         .configureTestingModule({
@@ -72,5 +77,18 @@ describe('ChangeClusterVersionComponent', () => {
        component.changeVersion();
        tick();
        expect(patchClusterSpy.and.callThrough()).toHaveBeenCalledTimes(1);
+     }));
+
+  it('should call upgradeClusterNodeDeployments method', fakeAsync(() => {
+       component.cluster = fakeDigitaloceanCluster();
+       component.datacenter = fakeDigitaloceanDatacenter();
+       component.selectedVersion = 'new-version';
+       component.project = fakeProject();
+
+       fixture.detectChanges();
+       component.upgradeNodeDeployments();
+       tick();
+
+       expect(upgradeClusterNodeDeploymentsSpy.and.callThrough()).toHaveBeenCalled();
      }));
 });
