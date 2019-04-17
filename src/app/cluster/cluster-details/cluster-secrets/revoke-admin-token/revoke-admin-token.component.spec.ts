60ee84c2b36450dbb1b16cae9d677301c14cd982
Kristin Groschoff <kgroschoff@gmail.com>
2019-04-17 16:06:48 +0200

    More unit tests (#1191)
    * add tests for revoke admin token cmp
    
    * tests for add project dialog cmp
    
    * tests for edit provider settings cmp
    
    * fix

--- src/app/cluster/cluster-details/cluster-secrets/revoke-admin-token/revoke-admin-token.component.spec.ts
+++ src/app/cluster/cluster-details/cluster-secrets/revoke-admin-token/revoke-admin-token.component.spec.ts
@@ -1,10 +1,15 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 import {MatDialogRef} from '@angular/material';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import Spy = jasmine.Spy;
+
 import {ApiService} from '../../../../core/services';
 import {SharedModule} from '../../../../shared/shared.module';
-import {ApiMockService} from '../../../../testing/services/api-mock.service';
+import {fakeDigitaloceanCluster, fakeToken} from '../../../../testing/fake-data/cluster.fake';
+import {fakeDigitaloceanDatacenter} from '../../../../testing/fake-data/datacenter.fake';
+import {fakeProject} from '../../../../testing/fake-data/project.fake';
+import {asyncData} from '../../../../testing/services/api-mock.service';
 import {MatDialogRefMock} from '../../../../testing/services/mat-dialog-ref-mock';
 import {RevokeAdminTokenComponent} from './revoke-admin-token.component';
 
@@ -17,8 +22,12 @@ const modules: any[] = [
 describe('RevokeAdminTokenComponent', () => {
   let fixture: ComponentFixture<RevokeAdminTokenComponent>;
   let component: RevokeAdminTokenComponent;
+  let editTokenSpy: Spy;
 
   beforeEach(() => {
+    const apiMock = jasmine.createSpyObj('ApiService', ['editToken']);
+    editTokenSpy = apiMock.editToken.and.returnValue(asyncData(fakeToken()));
+
     TestBed
         .configureTestingModule({
           imports: [
@@ -28,7 +37,7 @@ describe('RevokeAdminTokenComponent', () => {
             RevokeAdminTokenComponent,
           ],
           providers: [
-            {provide: ApiService, useClass: ApiMockService},
+            {provide: ApiService, useValue: apiMock},
             {provide: MatDialogRef, useClass: MatDialogRefMock},
           ],
         })
@@ -43,4 +52,16 @@ describe('RevokeAdminTokenComponent', () => {
   it('should create the revoke admin token cmp', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call editToken method', fakeAsync(() => {
+       component.projectID = fakeProject().id;
+       component.cluster = fakeDigitaloceanCluster();
+       component.datacenter = fakeDigitaloceanDatacenter();
+       component.adminToken = fakeToken();
+
+       fixture.detectChanges();
+       component.revokeAdminToken();
+       tick();
+       expect(editTokenSpy.and.callThrough()).toHaveBeenCalled();
+     }));
 });
