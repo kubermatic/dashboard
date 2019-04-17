94af7b910d415ff6296905605ec3dc106227a516
kgroschoff <kgroschoff@gmail.com>
2019-04-17 09:41:04 +0200

    tests for edit provider settings cmp

--- src/app/cluster/cluster-details/edit-provider-settings/edit-provider-settings.component.spec.ts
+++ src/app/cluster/cluster-details/edit-provider-settings/edit-provider-settings.component.spec.ts
@@ -1,14 +1,19 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 import {MatDialogRef} from '@angular/material';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {Router} from '@angular/router';
+import Spy = jasmine.Spy;
+
 import {ApiService, ClusterService, ProjectService} from '../../../core/services';
 import {GoogleAnalyticsService} from '../../../google-analytics.service';
 import {SharedModule} from '../../../shared/shared.module';
+import {doCloudSpecFake} from '../../../testing/fake-data/cloud-spec.fake';
 import {fakeDigitaloceanCluster} from '../../../testing/fake-data/cluster.fake';
+import {fakeDigitaloceanDatacenter} from '../../../testing/fake-data/datacenter.fake';
+import {fakeProject} from '../../../testing/fake-data/project.fake';
 import {RouterStub} from '../../../testing/router-stubs';
-import {ApiMockService} from '../../../testing/services/api-mock.service';
+import {asyncData} from '../../../testing/services/api-mock.service';
 import {MatDialogRefMock} from '../../../testing/services/mat-dialog-ref-mock';
 import {ProjectMockService} from '../../../testing/services/project-mock.service';
 import {AWSProviderSettingsComponent} from './aws-provider-settings/aws-provider-settings.component';
@@ -28,8 +33,12 @@ const modules: any[] = [
 describe('EditProviderSettingsComponent', () => {
   let fixture: ComponentFixture<EditProviderSettingsComponent>;
   let component: EditProviderSettingsComponent;
+  let patchClusterSpy: Spy;
 
   beforeEach(() => {
+    const apiMock = jasmine.createSpyObj('ApiService', ['patchCluster']);
+    patchClusterSpy = apiMock.patchCluster.and.returnValue(asyncData(fakeDigitaloceanCluster));
+
     TestBed
         .configureTestingModule({
           imports: [
@@ -46,7 +55,7 @@ describe('EditProviderSettingsComponent', () => {
           ],
           providers: [
             ClusterService,
-            {provide: ApiService, useClass: ApiMockService},
+            {provide: ApiService, useValue: apiMock},
             {provide: ProjectService, useClass: ProjectMockService},
             {provide: MatDialogRef, useClass: MatDialogRefMock},
             {provide: Router, useClass: RouterStub},
@@ -66,4 +75,21 @@ describe('EditProviderSettingsComponent', () => {
   it('should create the edit provider settings cmp', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call patchClusterProject method', fakeAsync(() => {
+       component.cluster = fakeDigitaloceanCluster();
+       component.datacenter = fakeDigitaloceanDatacenter();
+       component.project = fakeProject();
+       component.providerSettingsPatch = {
+         isValid: true,
+         cloudSpecPatch: doCloudSpecFake(),
+       };
+
+       fixture.detectChanges();
+
+       component.saveProviderSettings();
+       tick();
+
+       expect(patchClusterSpy.and.callThrough()).toHaveBeenCalled();
+     }));
 });
