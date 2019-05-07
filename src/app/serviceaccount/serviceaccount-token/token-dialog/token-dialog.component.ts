82c158ff6c746830e355621626e5f06661e3c282
kgroschoff <kgroschoff@gmail.com>
2019-05-07 15:07:29 +0200

    add button to download sa token

--- src/app/serviceaccount/serviceaccount-token/token-dialog/token-dialog.component.ts
+++ src/app/serviceaccount/serviceaccount-token/token-dialog/token-dialog.component.ts
@@ -11,4 +11,14 @@ export class TokenDialogComponent {
   @Input() serviceaccountToken: ServiceAccountTokenEntity;
 
   constructor() {}
+
+  downloadToken(): void {
+    const blob = new Blob([this.serviceaccountToken.token], {type: 'text/plain'});
+    const a = window.document.createElement('a');
+    a.href = window.URL.createObjectURL(blob);
+    a.download = window.location.host + '-' + this.serviceaccountToken.id;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }
 }
