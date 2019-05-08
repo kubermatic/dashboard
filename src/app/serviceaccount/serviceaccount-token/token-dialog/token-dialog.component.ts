3eda990b170281b5bcd761eba700b9c87f287e80
Kristin Groschoff <kgroschoff@gmail.com>
2019-05-08 20:36:20 +0200

    Add option to download serviceaccount token (#1234)
    * add button to download sa token
    
    * add project id
    
    * review comment

--- src/app/serviceaccount/serviceaccount-token/token-dialog/token-dialog.component.ts
+++ src/app/serviceaccount/serviceaccount-token/token-dialog/token-dialog.component.ts
@@ -1,4 +1,7 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
+
+import {ProjectService} from '../../../core/services';
 import {ServiceAccountTokenEntity} from '../../../shared/entity/ServiceAccountEntity';
 
 @Component({
@@ -7,8 +10,17 @@ import {ServiceAccountTokenEntity} from '../../../shared/entity/ServiceAccountEn
   styleUrls: ['./token-dialog.component.scss'],
 })
 
-export class TokenDialogComponent {
+export class TokenDialogComponent implements OnInit {
   @Input() serviceaccountToken: ServiceAccountTokenEntity;
+  downloadUrl: SafeUrl;
+  downloadTitle = '';
 
-  constructor() {}
+  constructor(private readonly _projectService: ProjectService, private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    const blob = new Blob([this.serviceaccountToken.token], {type: 'text/plain'});
+    this.downloadUrl = this.sanitizer.bypassSecurityTrustUrl(window.URL.createObjectURL(blob));
+    this.downloadTitle =
+        window.location.host + '-' + this._projectService.getCurrentProjectId() + '-' + this.serviceaccountToken.name;
+  }
 }
