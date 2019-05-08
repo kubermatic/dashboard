1a4e7a388cdb8d3c2a6787735ff0fb112a87a8b1
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-05-08 10:40:23 +0200

    Switch dialog close icon

--- src/app/shared/components/dialog-title/dialog-title.component.ts
+++ src/app/shared/components/dialog-title/dialog-title.component.ts
@@ -3,6 +3,7 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'km-dialog-title',
   templateUrl: './dialog-title.component.html',
+  styleUrls: ['./dialog-title.component.scss'],
 })
 export class DialogTitleComponent {
   @Input() title: string;
