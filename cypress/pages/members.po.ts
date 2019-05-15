bc0a1216015d1f2002461ec84acd70f1ad2e8baa
Sebastian Florek <Sebastian.Florek@loodse.com>
2019-05-15 15:07:19 +0200

    Setup e2e with cypress (#1211)
    * Setup e2e with cypress
    
    * Add docker container for cypress e2e
    
    * Increase timeouts and fix tests
    
    * Formatting fix
    
    * Fix test
    
    * Fail fast when 1 test fails
    
    * Setup cypress for Loodse org
    
    * Increate page load timeout

---
+++ cypress/pages/members.po.ts
@@ -0,0 +1,63 @@
+import {Group} from "../utils/member";
+
+export class MembersPage {
+    static visit() {
+        cy.get('#km-nav-item-members').click();
+    }
+    
+    static addMemberBtn() {
+        return cy.get('#km-add-member-top-btn');
+    }
+    
+    static addMemberDialogEmailInput() {
+        return cy.get('#km-add-member-dialog-email-input');
+    }
+    
+    static addMemberDialogGroupCombobox() {
+        return cy.get('#km-add-member-dialog-group-combobox');
+    }
+    
+    static addMemberDialogSaveBtn() {
+        return cy.get('#km-add-member-dialog-add-btn');
+    }
+    
+    static memberDialogGroup(group: Group) {
+        return cy.get('mat-option').contains('span', group);
+    }
+    
+    static editMemberDialogGroupCombobox() {
+        return cy.get('#km-edit-member-dialog-group-combobox');
+    }
+    
+    static editBtn(email: string) {
+        return MembersPage.tableRow(email).find('button.km-icon-edit');
+    }
+    
+    static deleteBtn(email: string) {
+        return MembersPage.tableRow(email).find('button.km-icon-delete');
+    }
+    
+    static editMemberDialogSaveBtn() {
+        return cy.get('#km-edit-member-dialog-edit-btn');
+    }
+    
+    static table() {
+        return cy.get('tbody');
+    }
+    
+    static tableRow(email: string) {
+        return MembersPage.tableRowEmailColumn(email).parent();
+    }
+    
+    static tableRowEmailColumn(email: string) {
+        return cy.get('td').contains(email);
+    }
+    
+    static tableRowGroupColumn(email: string) {
+        return MembersPage.tableRow(email).find('td.mat-column-group');
+    }
+    
+    static deleteMemberDialogDeleteBtn() {
+        return cy.get('#km-confirmation-dialog-confirm-btn');
+    }
+}
---
+++ cypress/utils/member.ts
@@ -0,0 +1,5 @@
+export enum Group {
+    Owner = 'Owner',
+    Editor = 'Editor',
+    Viewer = 'Viewer',
+}
