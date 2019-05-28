00f2c2083bf03cfce8bc410a199e54843edff4fb
Sebastian Florek <Sebastian.Florek@loodse.com>
2019-05-28 10:47:03 +0200

    Use zap logger (#1267)
    * Use zap logger
    
    * Handle error
    
    * Use sugared logger
    
    * Disable build cache for docker images

--- containers/e2e/build-and-release.sh
+++ containers/e2e/build-and-release.sh
@@ -6,5 +6,5 @@ IMG_REPO="quay.io/kubermatic"
 IMG_NAME="e2e-kind"
 IMG_VERSION="v1.0.7"
 
-docker build -t ${IMG_REPO}/${IMG_NAME}:${IMG_VERSION} .
+docker build --no-cache -t ${IMG_REPO}/${IMG_NAME}:${IMG_VERSION} .
 docker push ${IMG_REPO}/${IMG_NAME}:${IMG_VERSION}
