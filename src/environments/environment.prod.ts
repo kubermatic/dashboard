32b4999efe9391c70d3d34697e1f3b88083a4cd6
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-05-30 09:21:06 +0200

    Move OIDC URL params out of env files (#1273)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,3 @@
-import {RandomString} from '../app/shared/functions/generate-random-string';
-
-const redirect_uri = window.location.protocol + '//' + window.location.host + '/projects';
-const oauth = window.location.protocol + '//' + window.location.host + '/dex/auth';
-const scope: string[] = ['openid', 'email', 'profile', 'groups'];
-const nonceString = RandomString(32);
-
 export const environment = {
   name: 'prod',
   production: true,
@@ -15,7 +8,6 @@ export const environment = {
   restRoot: '/api/v1',
   restRootV3: '/api/v3',
   digitalOceanRestRoot: 'https://api.digitalocean.com/v2',
-  oidcProviderUrl: oauth + '?response_type=id_token&client_id=kubermatic&redirect_uri=' + redirect_uri +
-      '&scope=' + scope.join(' ') + '&nonce=' + nonceString,
+  oidcProviderUrl: window.location.protocol + '//' + window.location.host + '/dex/auth',
   animations: true,
 };
