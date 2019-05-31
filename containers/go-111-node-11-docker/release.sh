cea92134ae8dbce430100813953c5a06863d6220
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-05-31 12:26:11 +0200

    Update Docker images to use Node 11 (#1278)

---
+++ containers/go-111-node-11-docker/release.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+TAG=v0.1
+
+set -euox pipefail
+
+docker build --no-cache --pull -t quay.io/kubermatic/go-111-node-11-docker:${TAG} .
+docker push quay.io/kubermatic/go-111-node-11-docker:${TAG}
