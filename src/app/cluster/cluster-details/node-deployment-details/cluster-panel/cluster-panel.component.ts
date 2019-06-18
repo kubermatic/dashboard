5b52717c9b9e53e2594ed80820a85c916a5796f8
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-06-18 13:54:36 +0200

    Redesign node deployment view (#1320)
    * Add cluster panel
    Change font colors
    
    * Move buttons on node deployment view
    
    * Add expansion panel
    Update icons
    
    * Fix notification side paddings
    
    * Fix unit tests
    
    * Fix go back button
    
    * Fix unit tests

---
+++ src/app/cluster/cluster-details/node-deployment-details/cluster-panel/cluster-panel.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input} from '@angular/core';
+import {Router} from '@angular/router';
+
+import {ClusterEntity} from '../../../../shared/entity/ClusterEntity';
+import {DataCenterEntity} from '../../../../shared/entity/DatacenterEntity';
+import {ClusterUtils} from '../../../../shared/utils/cluster-utils/cluster-utils';
+
+@Component({
+  selector: 'km-cluster-panel',
+  templateUrl: './cluster-panel.component.html',
+  styleUrls: ['./cluster-panel.component.scss'],
+})
+export class ClusterPanelComponent {
+  @Input() cluster: ClusterEntity;
+  @Input() datacenter: DataCenterEntity;
+  @Input() dcName: string;
+  @Input() projectID: string;
+
+  constructor(private readonly _router: Router) {}
+
+  getVersionHeadline(type: string, isKubelet: boolean): string {
+    return ClusterUtils.getVersionHeadline(type, isKubelet);
+  }
+
+  navigate(): void {
+    this._router.navigate(['/projects/' + this.projectID + '/dc/' + this.dcName + '/clusters/' + this.cluster.id]);
+  }
+}
