75cb3209cb8e079684426903190800d4d6a438d1
Christoph Mewes <christoph@loodse.com>
2019-07-03 14:47:32 +0200

    instantiate objects explicitly or the toString() function will be missing

--- src/app/shared/model/NodeProviderConstants.ts
+++ src/app/shared/model/NodeProviderConstants.ts
@@ -40,46 +40,51 @@ export class NodeInstanceFlavor {
 export namespace NodeInstanceFlavors {
   // Keep in sync with https://aws.amazon.com/ec2/instance-types/.
   export const AWS: NodeInstanceFlavor[] = [
-    {id: 't3.small', info: '2 vCPU, 2 GB'},
-    {id: 't3.medium', info: '2 vCPU, 4 GB'},
-    {id: 't3.large', info: '2 vCPU, 8 GB'},
-    {id: 't3.xlarge', info: '4 vCPU, 16 GB'},
-    {id: 't3.2xlarge', info: '8 vCPU, 32 GB'},
-    {id: 'm5.large', info: '2 vCPU, 8 GB'},
-    {id: 'm5d.large', info: '2 vCPU, 8 GB'},
-    {id: 'm5.xlarge', info: '4 vCPU, 16 GB'},
-    {id: 'm5.2xlarge', info: '8 vCPU, 32 GB'},
-    {id: 'm3.medium', info: '1 vCPU, 3.75 GB'},
-    {id: 'c5.large', info: '2 vCPU, 4 GB'},
-    {id: 'c5.xlarge', info: '4 vCPU, 8 GB'},
-    {id: 'c5.2xlarge', info: '8 vCPU, 16 GB'},
+    new NodeInstanceFlavor('t3.small', '2 vCPU, 2 GB'),
+    new NodeInstanceFlavor('t3.medium', '2 vCPU, 4 GB'),
+    new NodeInstanceFlavor('t3.large', '2 vCPU, 8 GB'),
+    new NodeInstanceFlavor('t3.xlarge', '4 vCPU, 16 GB'),
+    new NodeInstanceFlavor('t3.2xlarge', '8 vCPU, 32 GB'),
+    new NodeInstanceFlavor('m5.large', '2 vCPU, 8 GB'),
+    new NodeInstanceFlavor('m5d.large', '2 vCPU, 8 GB'),
+    new NodeInstanceFlavor('m5.xlarge', '4 vCPU, 16 GB'),
+    new NodeInstanceFlavor('m5.2xlarge', '8 vCPU, 32 GB'),
+    new NodeInstanceFlavor('m3.medium', '1 vCPU, 3.75 GB'),
+    new NodeInstanceFlavor('c5.large', '2 vCPU, 4 GB'),
+    new NodeInstanceFlavor('c5.xlarge', '4 vCPU, 8 GB'),
+    new NodeInstanceFlavor('c5.2xlarge', '8 vCPU, 16 GB'),
   ];
 
   // Keep in sync with https://www.packet.com/cloud/servers/.
   export const Packet: NodeInstanceFlavor[] = [
-    {id: 't1.small.x86', info: '4 Cores, 8 GB'},
-    {id: 'c1.small.x86', info: '4 Cores, 32 GB'},
-    {id: 'c2.medium.x86', info: '24 Cores, 64 GB'},
-    {id: 'c1.large.x86', info: '16 Cores, 128 GB'},
-    {id: 'm1.large.x86', info: '24 Cores, 256 GB'},
-    {id: 'm2.large.x86', info: '28 Cores, 384 GB'},
-    {id: 's1.large.x86', info: '16 Cores, 128 GB'},
+    new NodeInstanceFlavor('t1.small.x86', '4 Cores, 8 GB'),
+    new NodeInstanceFlavor('c1.small.x86', '4 Cores, 32 GB'),
+    new NodeInstanceFlavor('c2.medium.x86', '24 Cores, 64 GB'),
+    new NodeInstanceFlavor('c1.large.x86', '16 Cores, 128 GB'),
+    new NodeInstanceFlavor('m1.large.x86', '24 Cores, 256 GB'),
+    new NodeInstanceFlavor('m2.large.x86', '28 Cores, 384 GB'),
+    new NodeInstanceFlavor('s1.large.x86', '16 Cores, 128 GB'),
   ];
 
-  export const Openstack: NodeInstanceFlavor[] =
-      [{id: 'm1.micro'}, {id: 'm1.tiny'}, {id: 'm1.small'}, {id: 'm1.medium'}, {id: 'm1.large'}];
+  export const Openstack: NodeInstanceFlavor[] = [
+    new NodeInstanceFlavor('m1.micro'),
+    new NodeInstanceFlavor('m1.tiny'),
+    new NodeInstanceFlavor('m1.small'),
+    new NodeInstanceFlavor('m1.medium'),
+    new NodeInstanceFlavor('m1.large'),
+  ];
 
   export const Hetzner: NodeInstanceFlavor[] = [
-    {id: 'cx11', info: '1 vCPU, 2 GB'},
-    {id: 'cx21', info: '2 vCPU, 4 GB'},
-    {id: 'cx31', info: '2 vCPU, 8 GB'},
-    {id: 'cx41', info: '4 vCPU, 16 GB'},
-    {id: 'cx51', info: '8 vCPU, 32 GB'},
-    {id: 'cx11-ceph', info: '1 vCPU, 2 GB'},
-    {id: 'cx21-ceph', info: '2 vCPU, 4 GB'},
-    {id: 'cx31-ceph', info: '2 vCPU, 8 GB'},
-    {id: 'cx41-ceph', info: '4 vCPU, 16 GB'},
-    {id: 'cx51-ceph', info: '8 vCPU, 32 GB'},
+    new NodeInstanceFlavor('cx11', '1 vCPU, 2 GB'),
+    new NodeInstanceFlavor('cx21', '2 vCPU, 4 GB'),
+    new NodeInstanceFlavor('cx31', '2 vCPU, 8 GB'),
+    new NodeInstanceFlavor('cx41', '4 vCPU, 16 GB'),
+    new NodeInstanceFlavor('cx51', '8 vCPU, 32 GB'),
+    new NodeInstanceFlavor('cx11-ceph', '1 vCPU, 2 GB'),
+    new NodeInstanceFlavor('cx21-ceph', '2 vCPU, 4 GB'),
+    new NodeInstanceFlavor('cx31-ceph', '2 vCPU, 8 GB'),
+    new NodeInstanceFlavor('cx41-ceph', '4 vCPU, 16 GB'),
+    new NodeInstanceFlavor('cx51-ceph', '8 vCPU, 32 GB'),
   ];
 
   export namespace GCP {
@@ -88,14 +93,14 @@ export namespace NodeInstanceFlavors {
 
     // https://cloud.google.com/compute/docs/machine-types
     export const MachineTypes: NodeInstanceFlavor[] = [
-      {id: 'n1-standard-1', info: '1 vCPU, 3.75 GB'},
-      {id: 'n1-standard-2', info: '2 vCPU, 7.50 GB'},
-      {id: 'n1-standard-4', info: '4 vCPU, 15 GB'},
-      {id: 'n1-standard-8', info: '8 vCPU, 30 GB'},
-      {id: 'n1-standard-16', info: '16 vCPU, 60 GB'},
-      {id: 'n1-standard-32', info: '32 vCPU, 120 GB'},
-      {id: 'n1-standard-64', info: '64 vCPU, 240 GB'},
-      {id: 'n1-standard-96', info: '96 vCPU, 360 GB'},
+      new NodeInstanceFlavor('n1-standard-1', '1 vCPU, 3.75 GB'),
+      new NodeInstanceFlavor('n1-standard-2', '2 vCPU, 7.50 GB'),
+      new NodeInstanceFlavor('n1-standard-4', '4 vCPU, 15 GB'),
+      new NodeInstanceFlavor('n1-standard-8', '8 vCPU, 30 GB'),
+      new NodeInstanceFlavor('n1-standard-16', '16 vCPU, 60 GB'),
+      new NodeInstanceFlavor('n1-standard-32', '32 vCPU, 120 GB'),
+      new NodeInstanceFlavor('n1-standard-64', '64 vCPU, 240 GB'),
+      new NodeInstanceFlavor('n1-standard-96', '96 vCPU, 360 GB'),
     ];
   }
 }
