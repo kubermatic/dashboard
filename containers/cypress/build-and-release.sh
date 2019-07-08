fcacc357445172019c77a6811dd4fc890cf91183
Sebastian Florek <Sebastian.Florek@loodse.com>
2019-07-08 12:36:16 +0200

    Cleanup test containers (#1358)

--- containers/cypress/build-and-release.sh
+++ containers/cypress/build-and-release.sh
@@ -6,5 +6,5 @@ IMG_REPO="quay.io/kubermatic"
 IMG_NAME="e2e-kind-cypress"
 IMG_VERSION="v1.0.3"
 
-docker build --no-cache -t ${IMG_REPO}/${IMG_NAME}:${IMG_VERSION} .
+docker build -t ${IMG_REPO}/${IMG_NAME}:${IMG_VERSION} .
 docker push ${IMG_REPO}/${IMG_NAME}:${IMG_VERSION}
