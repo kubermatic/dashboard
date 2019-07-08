24e83d6c74b0ff8c88aeb9b2c0915fd7ddee8b68
Sebastian Florek <Sebastian.Florek@loodse.com>
2019-07-08 14:54:04 +0200

    Fix vsphere cluster edit

--- src/app/cluster/cluster-details/edit-provider-settings/vsphere-provider-settings/vsphere-provider-settings.component.ts
+++ src/app/cluster/cluster-details/edit-provider-settings/vsphere-provider-settings/vsphere-provider-settings.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {Subscription} from 'rxjs';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 import {ClusterService} from '../../../../core/services';
 import {ProviderSettingsPatch} from '../../../../core/services/cluster/cluster.service';
 import {ClusterEntity} from '../../../../shared/entity/ClusterEntity';
@@ -13,32 +14,40 @@ import {ClusterEntity} from '../../../../shared/entity/ClusterEntity';
 export class VSphereProviderSettingsComponent implements OnInit, OnDestroy {
   @Input() cluster: ClusterEntity;
   vsphereProviderSettingsForm: FormGroup;
-  private subscriptions: Subscription[] = [];
+
+  private _unsubscribe = new Subject<void>();
+
+  private get _infraManagementUsername() {
+    return this.cluster.spec.cloud.vsphere.infraManagementUser ?
+        this.cluster.spec.cloud.vsphere.infraManagementUser.username :
+        '';
+  }
+
+  private get _infraManagementPassword() {
+    return this.cluster.spec.cloud.vsphere.infraManagementUser ?
+        this.cluster.spec.cloud.vsphere.infraManagementUser.password :
+        '';
+  }
 
   constructor(private clusterService: ClusterService) {}
 
   ngOnInit(): void {
     this.vsphereProviderSettingsForm = new FormGroup({
-      infraManagementUsername:
-          new FormControl(this.cluster.spec.cloud.vsphere.infraManagementUser.username, Validators.required),
-      infraManagementPassword:
-          new FormControl(this.cluster.spec.cloud.vsphere.infraManagementUser.password, Validators.required),
+      infraManagementUsername: new FormControl(this._infraManagementUsername, Validators.required),
+      infraManagementPassword: new FormControl(this._infraManagementPassword, Validators.required),
       username: new FormControl(this.cluster.spec.cloud.vsphere.username),
       password: new FormControl(this.cluster.spec.cloud.vsphere.password),
       vmNetName: new FormControl(this.cluster.spec.cloud.vsphere.vmNetName),
     });
 
-    this.subscriptions.push(this.vsphereProviderSettingsForm.valueChanges.subscribe(() => {
+    this.vsphereProviderSettingsForm.valueChanges.pipe(takeUntil(this._unsubscribe)).subscribe(() => {
       this.clusterService.changeProviderSettingsPatch(this.getProviderSettingsPatch());
-    }));
+    });
   }
 
   ngOnDestroy(): void {
-    for (const sub of this.subscriptions) {
-      if (sub) {
-        sub.unsubscribe();
-      }
-    }
+    this._unsubscribe.next();
+    this._unsubscribe.complete();
   }
 
   getProviderSettingsPatch(): ProviderSettingsPatch {
