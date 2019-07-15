24b5c74c695978ac02de1fd9212909b7860a074f
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-07-15 16:48:28 +0200

    Fix unit tests

--- src/app/shared/components/add-ssh-key-dialog/add-ssh-key-modal.component.spec.ts
+++ src/app/shared/components/add-ssh-key-dialog/add-ssh-key-modal.component.spec.ts
@@ -81,7 +81,8 @@ describe('AddSshKeyDialogComponent', () => {
   it('submitting a form calls createSSHKey method and closes dialog', fakeAsync(() => {
        expect(component.addSSHKeyForm.valid).toBeFalsy();
        component.addSSHKeyForm.controls['name'].setValue('testname');
-       component.addSSHKeyForm.controls['key'].setValue('testkey');
+       component.addSSHKeyForm.controls['key'].setValue(
+           'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCyVGaw1PuEl98f4/7Kq3O9ZIvDw2OFOSXAFVqilSFNkHlefm1iMtPeqsIBp2t9cbGUf55xNDULz/bD/4BCV43yZ5lh0cUYuXALg9NI29ui7PEGReXjSpNwUD6ceN/78YOK41KAcecq+SS0bJ4b4amKZIJG3JWmDKljtv1dmSBCrTmEAQaOorxqGGBYmZS7NQumRe4lav5r6wOs8OACMANE1ejkeZsGFzJFNqvr5DuHdDL5FAudW23me3BDmrM9ifUzzjl1Jwku3bnRaCcjaxH8oTumt1a00mWci/1qUlaVFft085yvVq7KZbF2OPPbl+erDW91+EZ2FgEi+v1/CSJ5 your_username@hostname');
        expect(component.addSSHKeyForm.valid).toBeTruthy();
 
        const spyDialogRefClose = spyOn(dialogRef, 'close');
