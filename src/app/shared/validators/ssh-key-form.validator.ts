63c1b8a0db059d9361b563f0bad836d70e1b8482
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-07-16 11:52:12 +0200

    Add initial SSH key validation (#1384)
    * Add initial SSH key validation
    
    * Add one more check
    
    * Minor improvements
    
    * Fix unit tests
    
    * Add new test

---
+++ src/app/shared/validators/ssh-key-form.validator.ts
@@ -0,0 +1,26 @@
+import {FormControl, ValidatorFn} from '@angular/forms';
+
+/**
+ * Validation on the frontend part allows to avoid unnecessary backend calls.
+ * Read more: https://tools.ietf.org/html/rfc4253#section-6.6
+ */
+export function SSHKeyFormValidator(): ValidatorFn {
+  return (control: FormControl): any => {
+    const splitForm = control.value.toString().trim().split(' ');
+
+    if (splitForm.length < 2 || splitForm.length > 3) {
+      return {validSSHKey: true};  // Key type and encoded data are required. Comment part is optional.
+    }
+
+    try {
+      const encoded = window.atob((splitForm[1]));
+      if (!encoded.includes(splitForm[0])) {
+        return {validSSHKey: true};  // Key type should be also included in encoded data.
+      }
+    } catch (err) {
+      return {validSSHKey: true};  // Not able to decode SSH key.
+    }
+
+    return null;
+  };
+}
