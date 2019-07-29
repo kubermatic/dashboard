36a086ca1a6a76de2539ff107bb54c8dc91cf1a1
Bastian Hofmann <b.hofmann@syseleven.de>
2019-07-29 13:19:55 +0200

    Hides cluster type toggle in wizard if only one cluster type is active
    Also the default type is set to Openshift if Kubernetes is not active

--- src/app/wizard/set-cluster-spec/set-cluster-spec.component.spec.ts
+++ src/app/wizard/set-cluster-spec/set-cluster-spec.component.spec.ts
@@ -3,15 +3,19 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule, By} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import {AppConfigService} from '../../app-config.service';
 import {ApiService, WizardService} from '../../core/services';
 import {ClusterNameGenerator} from '../../core/util/name-generator.service';
 import {MachineNetworksModule} from '../../machine-networks/machine-networks.module';
+import {Config} from '../../shared/model/Config';
 import {SharedModule} from '../../shared/shared.module';
+import {ClusterType} from '../../shared/utils/cluster-utils/cluster-utils';
+import {fakeAppConfig} from '../../testing/fake-data/appConfig.fake';
 import {masterVersionsFake} from '../../testing/fake-data/cluster-spec.fake';
 import {asyncData} from '../../testing/services/api-mock.service';
-import {AppConfigMockService} from '../../testing/services/app-config-mock.service';
 import {ClusterNameGeneratorMock} from '../../testing/services/name-generator-mock.service';
+
 import {SetClusterSpecComponent} from './set-cluster-spec.component';
 
 const modules: any[] = [
@@ -27,11 +31,14 @@ describe('SetClusterSpecComponent', () => {
   let fixture: ComponentFixture<SetClusterSpecComponent>;
   let component: SetClusterSpecComponent;
   let nameGenerator: ClusterNameGenerator;
+  let config: Config;
 
   beforeEach(async(() => {
     const apiMock = jasmine.createSpyObj('ApiService', ['getMasterVersions']);
     apiMock.getMasterVersions.and.returnValue(asyncData(masterVersionsFake()));
-
+    const appConfigServiceMock = jasmine.createSpyObj('AppConfigService', ['getConfig']);
+    config = fakeAppConfig() as Config;
+    appConfigServiceMock.getConfig.and.returnValue(config);
     TestBed
         .configureTestingModule({
           imports: [
@@ -44,14 +51,14 @@ describe('SetClusterSpecComponent', () => {
             HttpClient,
             WizardService,
             {provide: ApiService, useValue: apiMock},
-            {provide: AppConfigService, useClass: AppConfigMockService},
+            {provide: AppConfigService, useValue: appConfigServiceMock},
             {provide: ClusterNameGenerator, useClass: ClusterNameGeneratorMock},
           ],
         })
         .compileComponents();
   }));
 
-  beforeEach(() => {
+  const createComponent = () => {
     fixture = TestBed.createComponent(SetClusterSpecComponent);
     component = fixture.componentInstance;
     component.cluster = {
@@ -71,7 +78,9 @@ describe('SetClusterSpecComponent', () => {
     };
     fixture.detectChanges();
     nameGenerator = fixture.debugElement.injector.get(ClusterNameGenerator);
-  });
+  };
+
+  beforeEach(createComponent);
 
   it('should create the set-cluster-spec cmp', () => {
     expect(component).toBeTruthy();
@@ -104,5 +113,31 @@ describe('SetClusterSpecComponent', () => {
     expect(spyGenerateName.and.callThrough()).toHaveBeenCalledTimes(1);
     expect(component.clusterSpecForm.controls['name'].value).toBe(generatedName, 'should patch value');
     expect(nameElement.value).toBe(generatedName, 'should display value in template');
+  });
+
+  it('should set type to kubernetes as default', () => {
+    expect(component.clusterSpecForm.controls['type'].value).toEqual(ClusterType.Kubernetes);
+  });
+
+  it('should set type to openshift as default if kubernetes is hidden', () => {
+    config.hide_kubernetes = true;
+    createComponent();
+    expect(component.clusterSpecForm.controls['type'].value).toEqual(ClusterType.OpenShift);
+  });
+
+  it('should show type toggle by default', () => {
+    expect(component.hasMultipleTypes()).toBeTruthy();
+  });
+
+  it('should not show type toggle if type openshift is hidden', () => {
+    config.hide_openshift = true;
+    createComponent();
+    expect(component.hasMultipleTypes()).toBeFalsy();
+  });
+
+  it('should not show type toggle if type kubernetes is hidden', () => {
+    config.hide_kubernetes = true;
+    createComponent();
+    expect(component.hasMultipleTypes()).toBeFalsy();
   });
 });
