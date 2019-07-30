2e638c38ddc50aa073b9f9fc4c5695ba02948ab5
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-07-30 10:30:27 +0200

    Redesign 'show extended options' switch in the wizard (#1408)
    * Redesign 'show extended options' switch in wizard
    
    * Move the icon to the right

--- src/app/wizard/set-settings/set-settings.component.ts
+++ src/app/wizard/set-settings/set-settings.component.ts
@@ -1,5 +1,4 @@
 import {Component, Input} from '@angular/core';
-import {MatTabChangeEvent} from '@angular/material';
 import {WizardService} from '../../core/services';
 import {ClusterEntity} from '../../shared/entity/ClusterEntity';
 import {SSHKeyEntity} from '../../shared/entity/SSHKeyEntity';
@@ -8,24 +7,18 @@ import {NodeData} from '../../shared/model/NodeSpecChange';
 @Component({
   selector: 'kubermatic-set-settings',
   templateUrl: './set-settings.component.html',
+  styleUrls: ['./set-settings.component.scss'],
 })
 export class SetSettingsComponent {
   @Input() cluster: ClusterEntity;
   @Input() clusterSSHKeys: SSHKeyEntity[] = [];
   @Input() nodeData: NodeData;
+  isExtended = false;
 
   constructor(private wizardService: WizardService) {}
 
-  changeView(event: MatTabChangeEvent): void {
-    switch (event.tab.textLabel) {
-      case 'Simple':
-        this.wizardService.changeSettingsFormView({hideOptional: true});
-        break;
-      case 'Extended':
-        this.wizardService.changeSettingsFormView({hideOptional: false});
-        break;
-      default:
-        this.wizardService.changeSettingsFormView({hideOptional: true});
-    }
+  extend(): void {
+    this.isExtended = true;
+    this.wizardService.changeSettingsFormView({hideOptional: !this.isExtended});
   }
 }
