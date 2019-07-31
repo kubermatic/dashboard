6eb318c27f3f914f1dc5a0ed01bdfd33b1722296
kgroschoff <kgroschoff@gmail.com>
2019-07-31 12:12:45 +0200

    fix test

--- src/app/wizard/set-settings/provider-settings/provider-settings.component.spec.ts
+++ src/app/wizard/set-settings/provider-settings/provider-settings.component.spec.ts
@@ -3,11 +3,18 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ActivatedRoute, Router} from '@angular/router';
 
-import {WizardService} from '../../../core/services';
+import {ApiService, ProjectService, UserService, WizardService} from '../../../core/services';
 import {SharedModule} from '../../../shared/shared.module';
 import {fakeDigitaloceanCluster} from '../../../testing/fake-data/cluster.fake';
+import {RouterStub} from '../../../testing/router-stubs';
+import {ActivatedRouteMock} from '../../../testing/services/activate-route-mock';
+import {ApiMockService} from '../../../testing/services/api-mock.service';
+import {ProjectMockService} from '../../../testing/services/project-mock.service';
+import {UserMockService} from '../../../testing/services/user-mock.service';
 import {CustomPresetsSettingsComponent} from '../custom-credentials/custom-presets.component';
+import {ClusterSSHKeysComponent} from '../ssh-keys/cluster-ssh-keys.component';
 
 import {AWSClusterSettingsComponent} from './aws/aws.component';
 import {AzureClusterSettingsComponent} from './azure/azure.component';
@@ -37,6 +44,7 @@ describe('ClusterProviderSettingsComponent', () => {
           declarations: [
             CustomPresetsSettingsComponent,
             ClusterProviderSettingsComponent,
+            ClusterSSHKeysComponent,
             DigitaloceanClusterSettingsComponent,
             AWSClusterSettingsComponent,
             OpenstackClusterSettingsComponent,
@@ -49,6 +57,11 @@ describe('ClusterProviderSettingsComponent', () => {
           ],
           providers: [
             WizardService,
+            {provide: ActivatedRoute, useCass: ActivatedRouteMock},
+            {provide: ApiService, useClass: ApiMockService},
+            {provide: ProjectService, useClass: ProjectMockService},
+            {provide: UserService, useClass: UserMockService},
+            {provide: Router, useClass: RouterStub},
           ],
         })
         .compileComponents();
@@ -58,6 +71,7 @@ describe('ClusterProviderSettingsComponent', () => {
     fixture = TestBed.createComponent(ClusterProviderSettingsComponent);
     component = fixture.componentInstance;
     component.cluster = fakeDigitaloceanCluster();
+    component.clusterSSHKeys = [];
     fixture.detectChanges();
   });
 
