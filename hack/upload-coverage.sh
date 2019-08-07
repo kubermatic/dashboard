c02acd335e1e936df16c7b24bddac1ed0d6fe07e
Sebastian Florek <s.florek91@gmail.com>
2019-08-07 12:53:28 +0200

    Fix e2e kubermatic deployment (#1459)
    * Fix e2e kubermatic deployment
    
    * Do not fail on codecov upload fail

--- hack/upload-coverage.sh
+++ hack/upload-coverage.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 COMMIT_SHA=${PULL_PULL_SHA:-$PULL_BASE_SHA}
 
-bash <(curl -s https://codecov.io/bash) -c -K -C ${COMMIT_SHA} -b ${BUILD_ID}
+bash <(curl -s https://codecov.io/bash) -c -K -C ${COMMIT_SHA} -b ${BUILD_ID} || echo "Codecov upload failed"
