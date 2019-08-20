d72a3e65395b7697a7d1a122d141f2cc29a17ec7
kgroschoff <kgroschoff@gmail.com>
2019-08-20 08:49:27 +0200

    fix unit test

--- src/app/testing/fake-data/vsphere.fake.ts
+++ src/app/testing/fake-data/vsphere.fake.ts
@@ -2,11 +2,11 @@ import {VSphereNetwork} from '../../shared/entity/provider/vsphere/VSphereEntity
 
 export function fakeVSphereNetworks(): VSphereNetwork[] {
   return [
+    {'name': 'fake-network', 'absolutePath': '/dc-1/network/fake-network', 'type': 'DistributedVirtualPortgroup'},
     {
-      name: 'fake vsphere network',
-    },
-    {
-      name: 'another fake vsphere network',
+      'name': 'another-fake-network',
+      'absolutePath': '/dc-1/network/another-fake-network',
+      'type': 'DistributedVirtualPortgroup'
     },
   ];
 }
