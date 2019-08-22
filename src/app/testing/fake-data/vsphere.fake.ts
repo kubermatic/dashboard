0f170411b2511083597f94685878832855cfd944
Kristin Groschoff <kgroschoff@gmail.com>
2019-08-22 13:13:25 +0200

    Adjust vsphere networks (#1480)
    * adjust vsphere networks
    
    * fix unit test
    
    * adjust dropdown
    
    * fix styling
    
    * add review comments

--- src/app/testing/fake-data/vsphere.fake.ts
+++ src/app/testing/fake-data/vsphere.fake.ts
@@ -3,10 +3,16 @@ import {VSphereNetwork} from '../../shared/entity/provider/vsphere/VSphereEntity
 export function fakeVSphereNetworks(): VSphereNetwork[] {
   return [
     {
-      name: 'fake vsphere network',
+      'name': 'fake-network',
+      'absolutePath': '/dc-1/network/fake-network',
+      'relativePath': 'fake-network',
+      'type': 'DistributedVirtualPortgroup'
     },
     {
-      name: 'another fake vsphere network',
+      'name': 'another-fake-network',
+      'absolutePath': '/dc-1/network/another-fake-network',
+      'relativePath': 'another-fake-network',
+      'type': 'DistributedVirtualPortgroup'
     },
   ];
 }
