18a3931ff591e46b73b39b1c8ef384c8ca48471c
Sebastian Florek <Sebastian.Florek@loodse.com>
2019-09-06 14:03:20 +0200

    Preload kind image (#1531)

--- containers/e2e/build-and-release.sh
+++ containers/e2e/build-and-release.sh
@@ -6,5 +6,13 @@ IMG_REPO="quay.io/kubermatic"
 IMG_NAME="e2e-kind"
 IMG_VERSION="v1.0.10"
 
+# Preloaded images
+IMG_KIND="kindest/node:v1.13.4"
+IMG_KIND_NAME="kindest.tar"
+
+docker pull ${IMG_KIND}
+docker save -o ${IMG_KIND_NAME} ${IMG_KIND}
 docker build -t ${IMG_REPO}/${IMG_NAME}:${IMG_VERSION} .
 docker push ${IMG_REPO}/${IMG_NAME}:${IMG_VERSION}
+
+rm ${IMG_KIND_NAME}
