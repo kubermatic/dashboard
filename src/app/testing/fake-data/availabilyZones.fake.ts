4c0a8f2254f66eeda2ccc14d2e81a150f9ec2e98
Kristin Groschoff <kgroschoff@gmail.com>
2019-09-12 10:12:22 +0200

    Remove AWS Availability Zone endpoint (#1553)
    * remove aws availability zone
    
    * really delete all az endpoint parts

--- src/app/testing/fake-data/availabilyZones.fake.ts
+++
@@ -1,5 +0,0 @@
-import {AWSAvailabilityZone} from '../../shared/entity/provider/aws/AWS';
-
-export function fakeAwsZones(): AWSAvailabilityZone[] {
-  return [{'name': 'eu-central-1a'}, {'name': 'eu-central-1b'}, {'name': 'eu-central-1c'}];
-}
