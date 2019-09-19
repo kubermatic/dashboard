9de80e217d256433acfe07bca09921b425d9fb82
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-09-19 12:38:14 +0200

    Remove workaround from Cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,14 +2,4 @@ const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
 
 module.exports = on => {
   on('file:preprocessor', cypressTypeScriptPreprocessor);
-
-  // ref: https://docs.cypress.io/api/plugins/browser-launch-api.html#Usage
-  on('before:browser:launch', (browser = {}, args) => {
-    if (browser.name === 'chrome') {
-      args.push('--disable-dev-shm-usage');
-      return args;
-    }
-
-    return args;
-  });
 };
