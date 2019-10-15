c681f194da071dda2b69388e367fda8b7abf41d7
Sebastian Florek <Sebastian.Florek@loodse.com>
2019-10-15 12:18:29 +0200

    Silence 403 error for cluster GET (#1676)

--- src/app/core/interceptors/error-notifications/error-notifications.service.ts
+++ src/app/core/interceptors/error-notifications/error-notifications.service.ts
@@ -8,14 +8,28 @@ import {NotificationActions} from '../../../redux/actions/notification.actions';
 export class ErrorNotificationsInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(tap(() => {}, (errorInstance) => {
-      if (errorInstance) {
-        if (!!errorInstance.error && !!errorInstance.error.error) {
-          NotificationActions.error(`Error ${errorInstance.status}: ${
-              errorInstance.error.error.message || errorInstance.message || errorInstance.statusText}`);
-        } else {
-          NotificationActions.error(`${errorInstance.status}: ${errorInstance.statusText}`);
+      if (!errorInstance) {
+        return;
+      }
+
+      if (!!errorInstance.error && !!errorInstance.error.error) {
+        // Silence the 403 error for GET cluster until API gets fixed
+        if (errorInstance.error.error.code === 403 && this.isGetClusterEndpoint(errorInstance.url)) {
+          return;
         }
+
+        NotificationActions.error(`Error ${errorInstance.status}: ${
+            errorInstance.error.error.message || errorInstance.message || errorInstance.statusText}`);
+      } else {
+        NotificationActions.error(`${errorInstance.status}: ${errorInstance.statusText}`);
       }
     }));
+  }
+
+  // TODO(#1677): remove this after fixing API
+  isGetClusterEndpoint(url: string): boolean {
+    // Simple regex to check if it was an endpoint to GET cluster
+    const regex = new RegExp('^http[s]?:\\/\\/.*\\/api\\/v1\\/projects\\/.*\\/dc\\/.*\\/clusters\\/[a-zA-Z0-9]+$');
+    return regex.test(url);
   }
 }
