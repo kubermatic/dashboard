f91abced2f496e0123e5cd64d18eab84e186641a
Sebastian Florek <Sebastian.Florek@loodse.com>
2019-10-23 08:50:20 +0200

    Fail fast when 1 spec fails (#1707)

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,3 +1,6 @@
+import Context = Mocha.Context;
+import Done = Mocha.Done;
+
 before(() => {
   cy.clearCookies();
 });
@@ -5,4 +8,12 @@ before(() => {
 beforeEach(() => {
   cy.server();
   Cypress.Cookies.preserveOnce('token', 'nonce');
+});
+
+afterEach(function (this: Context, done: Done): void {
+  if (this.currentTest && this.currentTest.state === 'failed') {
+    (Cypress as any).runner.stop();
+  }
+
+  done();
 });
