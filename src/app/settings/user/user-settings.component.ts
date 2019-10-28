3d32a9771d7cadeb3d6aafcf4b7746038702f17d
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-10-28 15:48:12 +0100

    Add settings service

--- src/app/settings/user/user-settings.component.ts
+++ src/app/settings/user/user-settings.component.ts
@@ -1,22 +1,35 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import * as _ from 'lodash';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+
+import {SettingsService} from '../../core/services/settings/settings.service';
+import {UserSettings} from '../../shared/entity/MemberEntity';
+
 
 @Component({
   selector: 'kubermatic-user-settings',
   templateUrl: 'user-settings.component.html',
   styleUrls: ['user-settings.component.scss'],
 })
-export class UserSettingsComponent implements OnInit {
-  form: FormGroup;
-  themes = ['default', 'dark', 'comic-sans'];
-  itemsPerPage = 10;
+export class UserSettingsComponent implements OnInit, OnDestroy {
+  settings: UserSettings;       // Settings loaded from the API.
+  localSettings: UserSettings;  // Local copy of settings, may be edited by the user.
+  private _unsubscribe = new Subject<void>();
 
-  constructor(private readonly _builder: FormBuilder) {}
+  constructor(private readonly _settingsService: SettingsService) {}
 
   ngOnInit(): void {
-    this.form =
-        this._builder.group({themes: this._builder.control('default'), itemsPerPage: this._builder.control(10)});
+    this._settingsService.userSettings.pipe(takeUntil(this._unsubscribe)).subscribe(settings => {
+      if (!_.isEqual(settings, this.settings)) {  // if something was changed elsewhere
+        this.settings = settings;
+        this.localSettings = settings;  // todo refresh only specific fields
+      }
+    });
   }
 
-  changeTheme(): void {}
+  ngOnDestroy(): void {
+    this._unsubscribe.next();
+    this._unsubscribe.complete();
+  }
 }
