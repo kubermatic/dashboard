028c525a4eb70b02ab8479ad8c104546e541ae26
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-11-04 15:47:22 +0100

    Add history service

---
+++ src/app/core/services/history/history.service.ts
@@ -0,0 +1,31 @@
+import {Injectable, Injector} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {filter, pairwise} from 'rxjs/operators';
+
+@Injectable()
+export class HistoryService {
+  private _router: Router;
+  private _previousStateUrl: string;
+  private _currentStateUrl: string;
+
+  constructor(private readonly _injector: Injector) {}
+
+  init(): void {
+    this._router = this._injector.get(Router);
+
+    this._router.events.pipe(filter(e => e instanceof NavigationEnd))
+        .pipe(pairwise())
+        .subscribe((e: [NavigationEnd, NavigationEnd]) => {
+          this._previousStateUrl = e[0].url;
+          this._currentStateUrl = e[1].url;
+        });
+  }
+
+  goToPreviousState(defaultState: string): Promise<boolean> {
+    if (this._previousStateUrl && this._previousStateUrl !== this._currentStateUrl) {
+      return this._router.navigateByUrl(this._previousStateUrl);
+    }
+
+    return this._router.navigate([defaultState], {queryParamsHandling: 'preserve'});
+  }
+}
