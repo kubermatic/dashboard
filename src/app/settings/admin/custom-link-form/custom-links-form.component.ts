a75107c8531fd81c24b49f708f5e5a7488f64695
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-11-20 16:04:06 +0100

    Initialize custom links form

---
+++ src/app/settings/admin/custom-link-form/custom-links-form.component.ts
@@ -0,0 +1,87 @@
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {AbstractControl, FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Subject} from 'rxjs';
+
+import {CustomLink} from '../../../shared/utils/custom-link-utils/custom-link';
+
+@Component({
+  selector: 'km-custom-links-form',
+  templateUrl: './custom-links-form.component.html',
+  styleUrls: ['./custom-links-form.component.scss'],
+})
+export class CustomLinksFormComponent implements OnInit, OnDestroy {
+  @Input() customLinks: CustomLink[] = [];
+  @Output() customLinksChange = new EventEmitter<object>();
+  initialcustomLinks: CustomLink[];
+  form: FormGroup;
+  private _unsubscribe = new Subject<void>();
+
+  constructor(private readonly _formBuilder: FormBuilder) {}
+
+  get customLinksArray(): FormArray {
+    return this.form.get('customLinks') as FormArray;
+  }
+
+  ngOnInit(): void {
+    this.initialcustomLinks = this.customLinks;
+    this.form = this._formBuilder.group({customLinks: this._formBuilder.array([])});
+    this.customLinks.forEach(customLink => this._addCustomLink(customLink.label, customLink.url));
+    this._addCustomLink();
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribe.next();
+    this._unsubscribe.complete();
+  }
+
+  isRemovable(index: number): boolean {
+    return index < this.customLinksArray.length - 1;
+  }
+
+  deleteLabel(index: number): void {
+    this.customLinksArray.removeAt(index);
+    this._updateLabelsObject();
+  }
+
+  check(): void {
+    this._addLabelIfNeeded();
+    this._updateLabelsObject();
+  }
+
+  private static _isFilled(customLink: AbstractControl): boolean {
+    return customLink.get('label').value.length !== 0 && customLink.get('url').value.length !== 0;
+  }
+
+  private _addLabelIfNeeded(): void {
+    const lastLabel = this.customLinksArray.at(this.customLinksArray.length - 1);
+    if (CustomLinksFormComponent._isFilled(lastLabel)) {
+      this._addCustomLink();
+    }
+  }
+
+  private _addCustomLink(label = '', url = ''): void {
+    this.customLinksArray.push(this._formBuilder.group({
+      label: [{value: label}, Validators.required],
+      url: [{value: url}, Validators.required],
+    }));
+  }
+
+  private _updateLabelsObject(): void {
+    const customLinks = [];
+    this.customLinksArray.getRawValue().forEach(raw => {
+      if (raw.label.length !== 0 && raw.url.length !== 0) {
+        customLinks.push({label: raw.label, url: raw.url} as CustomLink);
+      }
+    });
+
+    // Nullify initial labels data (it is needed to make edit work as it uses JSON Merge Patch).
+    // Object.keys(this.initialcustomLinks).forEach(initialKey => {
+    //   if (!labelsObject.hasOwnProperty(initialKey)) {
+    //     labelsObject[initialKey] = null;
+    //   }
+    // });
+
+    this.customLinks = customLinks;
+    this.customLinksChange.emit(this.customLinks);
+  }
+}
