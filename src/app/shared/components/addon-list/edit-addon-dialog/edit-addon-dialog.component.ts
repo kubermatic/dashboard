1c5e868d637e1517d687c5e754cb5295ef9b71cd
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-12-04 16:02:05 +0100

    Add edit view for addons (#1853)
    * Add edit view for addons
    Reload addons on any change
    
    * Use common variable

--- src/app/shared/components/addon-list/edit-addon-dialog/edit-addon-dialog.component.ts
+++ src/app/shared/components/addon-list/edit-addon-dialog/edit-addon-dialog.component.ts
@@ -1,37 +1,29 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
 import {MatDialogRef} from '@angular/material/dialog';
 import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
-import {AddonConfigEntity, AddonEntity, AddonFormSpec} from '../../../entity/AddonEntity';
+import {AddonConfigEntity, AddonEntity} from '../../../entity/AddonEntity';
+import {InstallAddonDialogComponent} from '../install-addon-dialog/install-addon-dialog.component';
 
 @Component({
-  selector: 'km-install-addon-dialog',
-  templateUrl: './install-addon-dialog.component.html',
-  styleUrls: ['./install-addon-dialog.component.scss'],
+  selector: 'km-edit-addon-dialog',
+  templateUrl: './edit-addon-dialog.component.html',
+  styleUrls: ['./edit-addon-dialog.component.scss'],
 })
-export class InstallAddonDialogComponent implements OnInit {
-  @Input() addonName: string;
+export class EditAddonDialogComponent implements OnInit {
+  @Input() addon: AddonEntity;
   @Input() addonConfig: AddonConfigEntity;
   form: FormGroup;
 
-  static getControlValidators(control: AddonFormSpec): ValidatorFn[] {
-    return control.required ? [Validators.required] : [];
-  }
-
-  static getFormState(control: AddonFormSpec): string|number {
-    return control.type === 'number' ? 0 : '';
-  }
-
-  constructor(
-      public dialogRef: MatDialogRef<InstallAddonDialogComponent>, private readonly _domSanitizer: DomSanitizer) {}
+  constructor(public dialogRef: MatDialogRef<EditAddonDialogComponent>, private readonly _domSanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
     const group = {};
     if (this.addonConfig.spec.formSpec) {
       this.addonConfig.spec.formSpec.forEach(control => {
         group[control.internalName] = new FormControl(
-            InstallAddonDialogComponent.getFormState(control),
+            this.addon.spec.variables[control.internalName],
             InstallAddonDialogComponent.getControlValidators(control),
         );
       });
@@ -49,17 +41,17 @@ export class InstallAddonDialogComponent implements OnInit {
         `data:image/${this.addonConfig.spec.logoFormat};base64,${this.addonConfig.spec.logo}`);
   }
 
-  private _getAddonEntity(): AddonEntity {
+  private _getAddonPatch(): AddonEntity {
     const variables = {};
 
     Object.keys(this.form.controls).forEach(key => {
       variables[key] = this.form.controls[key].value;
     });
 
-    return {name: this.addonName, spec: {variables}};
+    return {name: this.addon.name, spec: {variables}};
   }
 
-  install(): void {
-    this.dialogRef.close(this._getAddonEntity());
+  edit(): void {
+    this.dialogRef.close(this._getAddonPatch());
   }
 }
