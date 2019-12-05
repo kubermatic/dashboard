a6c3e89f29065a60564254b9e90bf8aaabd9e5eb
kgroschoff <kgroschoff@gmail.com>
2019-12-05 14:22:28 +0100

    fix tests

--- src/app/node-data/openstack-node-data/openstack-node-data.component.spec.ts
+++ src/app/node-data/openstack-node-data/openstack-node-data.component.spec.ts
@@ -68,13 +68,13 @@ describe('OpenstackNodeDataComponent', () => {
     spyOn(component, 'isInWizard').and.returnValue(false);
 
     fixture.detectChanges();
-    const tooltipEl = fixture.debugElement.query(By.css('.km-icon-info'));
+    const tooltipEl = fixture.debugElement.query(By.css('.km-floating-ip-checkbox .km-icon-info'));
     expect(tooltipEl).not.toBeNull();
     expect(component.form.controls.useFloatingIP.disabled).toBeTruthy();
   });
 
   it('should enable floating ip checkbox when not enforced by datacenter', () => {
-    const tooltipEl = fixture.debugElement.query(By.css('.km-icon-info'));
+    const tooltipEl = fixture.debugElement.query(By.css('.km-floating-ip-checkbox .km-icon-info'));
     spyOn(component, 'isInWizard').and.returnValue(false);
 
     fixture.detectChanges();
