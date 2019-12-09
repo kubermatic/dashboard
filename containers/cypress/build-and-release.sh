719c523bc33d276fba02712d9fe5863a147c7021
Alvaro Aleman <alv2412@googlemail.com>
2019-12-09 11:13:43 +0100

    Use current kind version

--- containers/cypress/build-and-release.sh
+++ containers/cypress/build-and-release.sh
@@ -4,15 +4,13 @@ set -e
 
 IMG_REPO="quay.io/kubermatic"
 IMG_NAME="e2e-kind-cypress"
-IMG_VERSION="v1.0.7"
+IMG_VERSION="v1.1.0"
 
 # Preloaded images
-IMG_KIND="kindest/node:v1.13.4"
+IMG_KIND="kindest/node:v1.15.6"
 IMG_KIND_NAME="kindest.tar"
 
-docker pull ${IMG_KIND}
-docker save -o ${IMG_KIND_NAME} ${IMG_KIND}
+#docker pull ${IMG_KIND}
+#docker save -o ${IMG_KIND_NAME} ${IMG_KIND}
 docker build -t ${IMG_REPO}/${IMG_NAME}:${IMG_VERSION} .
 docker push ${IMG_REPO}/${IMG_NAME}:${IMG_VERSION}
-
-rm ${IMG_KIND_NAME}
