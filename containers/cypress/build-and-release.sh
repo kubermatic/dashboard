00c6493717c99f74a0eca0e7bf1000a434d583bc
Alvaro Aleman <alv2412@googlemail.com>
2019-12-09 18:17:29 +0100

    Restore docker pull and save

--- containers/cypress/build-and-release.sh
+++ containers/cypress/build-and-release.sh
@@ -10,7 +10,9 @@ IMG_VERSION="v1.1.1"
 IMG_KIND="kindest/node:v1.15.6"
 IMG_KIND_NAME="kindest.tar"
 
-#docker pull ${IMG_KIND}
-#docker save -o ${IMG_KIND_NAME} ${IMG_KIND}
+docker pull ${IMG_KIND}
+docker save -o ${IMG_KIND_NAME} ${IMG_KIND}
 docker build -t ${IMG_REPO}/${IMG_NAME}:${IMG_VERSION} .
 docker push ${IMG_REPO}/${IMG_NAME}:${IMG_VERSION}
+
+rm ${IMG_KIND_NAME}
