4322993290e996c6ab0d825f738de12e73aec59b
Alvaro Aleman <alv2412@googlemail.com>
2019-12-09 14:58:56 +0100

    Use envsubst instead of eval

--- hack/e2e/ci-e2e.sh
+++ hack/e2e/ci-e2e.sh
@@ -12,8 +12,11 @@ export CYPRESS_RECORD_KEY=7859bcb8-1d2a-4d56-b7f5-ca70b93f944c
 export KUBERMATIC_SKIP_BUILDING=true
 export KUBERMATIC_VERSION=latest
 
+
+apt install -y gettext bash-completion
+
 # Need to pipe it through echo so the vars get replaced
-export ADDITIONAL_HELM_ARGS="--set=kubermatic.presets=$(eval \"echo \"$(cat $(dirname $0)/fixtures/presets.yaml)\"\"|base64 -w0)"
+export ADDITIONAL_HELM_ARGS="--set=kubermatic.presets=$(cat $(dirname $0)/fixtures/presets.yaml|envsubst|base64 -w0)"
 export CACHE_VERSION="$(git -C ../kubermatic rev-parse HEAD)"
 source ${GOPATH}/src/github.com/kubermatic/kubermatic/api/hack/ci/ci-setup-kubermatic-in-kind.sh
 
