18f735b5d9443c49706506ab74a24d53495dbb57
Alvaro Aleman <alv2412@googlemail.com>
2019-12-09 13:50:56 +0100

    Add presets and users

--- hack/e2e/ci-e2e.sh
+++ hack/e2e/ci-e2e.sh
@@ -12,10 +12,13 @@ export CYPRESS_RECORD_KEY=7859bcb8-1d2a-4d56-b7f5-ca70b93f944c
 export KUBERMATIC_SKIP_BUILDING=true
 export KUBERMATIC_VERSION=latest
 
+# Need to pipe it through echo so the vars get replaced
+export ADDITIONAL_HELM_ARGS="--set=kubermatic.presets=$(echo $(cat $(dirname $0)/fixtures/presets.yaml)|base64 -w0)"
 export CACHE_VERSION="$(git -C ../kubermatic rev-parse HEAD)"
 source ${GOPATH}/src/github.com/kubermatic/kubermatic/api/hack/ci/ci-setup-kubermatic-in-kind.sh
 
+echodate "Applying user"
+retry 2 kubectl apply -f $(dirname $0)/fixtures/user.yaml
 
-cd ${GOPATH}/src/github.com/kubermatic/dashboard-v2
 npm run versioninfo
 WAIT_ON_TIMEOUT=600000 npm run e2e:local
