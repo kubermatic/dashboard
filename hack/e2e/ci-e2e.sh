cbce86ffda0af95536708df00ba96c74a2617eb3
Sebastian Florek <sebastian.florek@loodse.com>
2020-01-10 11:51:53 +0100

    Fix presets

--- hack/e2e/ci-e2e.sh
+++ hack/e2e/ci-e2e.sh
@@ -16,8 +16,64 @@ apt install -y gettext bash-completion
 export CACHE_VERSION="$(git -C ../kubermatic rev-parse HEAD)"
 source ${GOPATH}/src/github.com/kubermatic/kubermatic/api/hack/ci/ci-setup-kubermatic-in-kind.sh
 
-echodate "Applying presets..."
-retry 2 kubectl apply -f $(dirname $0)/fixtures/presets.yaml
+
+echodate "Creating UI Azure preset..."
+cat <<EOF > preset-azure.yaml
+apiVersion: kubermatic.k8s.io/v1
+kind: Preset
+metadata:
+  name: e2e-azure
+  namespace: kubermatic
+spec:
+  azure:
+    tenantId: ${AZURE_E2E_TESTS_TENANT_ID}
+    subscriptionId: ${AZURE_E2E_TESTS_SUBSCRIPTION_ID}
+    clientId: ${AZURE_E2E_TESTS_CLIENT_ID}
+    clientSecret: ${AZURE_E2E_TESTS_CLIENT_SECRET}
+EOF
+retry 2 kubectl apply -f preset-azure.yaml
+
+echodate "Creating UI DigitalOcean preset..."
+cat <<EOF > preset-digitalocean.yaml
+apiVersion: kubermatic.k8s.io/v1
+kind: Preset
+metadata:
+  name: e2e-digitalocean
+  namespace: kubermatic
+spec:
+  digitalocean:
+    token: ${DO_E2E_TESTS_TOKEN}
+EOF
+retry 2 kubectl apply -f preset-digitalocean.yaml
+
+echodate "Creating UI GCP preset..."
+cat <<EOF > preset-gcp.yaml
+apiVersion: kubermatic.k8s.io/v1
+kind: Preset
+metadata:
+  name: e2e-gcp
+  namespace: kubermatic
+spec:
+  gcp:
+    serviceAccount: ${GOOGLE_SERVICE_ACCOUNT}
+EOF
+retry 2 kubectl apply -f preset-gcp.yaml
+
+echodate "Creating UI OpenStack preset..."
+cat <<EOF > preset-openstack.yaml
+apiVersion: kubermatic.k8s.io/v1
+kind: Preset
+metadata:
+  name: e2e-openstack
+  namespace: kubermatic
+spec:
+  openstack:
+    username: ${OS_USERNAME}
+    password: ${OS_PASSWORD}
+    tenant: ${OS_TENANT_NAME}
+    domain: ${OS_DOMAIN}
+EOF
+retry 2 kubectl apply -f preset-openstack.yaml
 
 echodate "Applying user..."
 retry 2 kubectl apply -f $(dirname $0)/fixtures/user.yaml
