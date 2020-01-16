978d0d74a2c11128ccf1bf490740a084cf65f4ca
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-01-16 09:35:33 +0100

    Fix merge issue

--- src/app/shared/entity/NodeEntity.ts
+++ src/app/shared/entity/NodeEntity.ts
@@ -104,16 +104,6 @@ export class NodeSystemInfo {
   containerRuntimeVersion: string;
 }
 
-export class NodeMetrics {
-  name: string;
-  cpuAvailableMillicores: number;
-  cpuTotalMillicores: number;
-  cpuUsedPercentage: number;
-  memoryAvailableBytes: number;
-  memoryTotalBytes: number;
-  memoryUsedPercentage: number;
-}
-
 export function getEmptyNodeProviderSpec(provider: string): object {
   switch (provider) {
     case NodeProvider.AWS:
