84cfb4ab1015f2977938dc4bbfdc300facfcc1cf
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-02-14 14:06:53 +0100

    Update env files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,8 +11,6 @@ export const environment = {
   customCSS: '../../assets/custom/style.css',
   refreshTimeBase: 1000,  // Unit: ms
   restRoot: 'api/v1',
-  restRootV3: 'api/v3',
-  digitalOceanRestRoot: 'https://api.digitalocean.com/v2',
   oidcProviderUrl: 'https://dev.kubermatic.io/dex/auth',
   oidcConnectorId: null,
   animations: true,
