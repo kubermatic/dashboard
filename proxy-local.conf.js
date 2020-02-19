c401e694212507bb4cc774087bff99fe51727848
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-02-19 11:58:46 +0100

    Revert local proxy config

--- proxy-local.conf.js
+++ proxy-local.conf.js
@@ -7,12 +7,11 @@
 const PROXY_CONFIG = [
   {
     context: [
-      "/",
+      "/api/**",
     ],
     target: "http://localhost:8080",
     changeOrigin: true,
     secure: false,
-    ws: true,
   }
 ];
 
