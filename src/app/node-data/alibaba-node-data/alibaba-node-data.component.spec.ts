26454a3bb13560482e4b4fd718cf7c6c4ccb3d99
kgroschoff <kgroschoff@gmail.com>
2020-03-02 16:36:57 +0100

    fetch instance types from api

--- src/app/node-data/alibaba-node-data/alibaba-node-data.component.spec.ts
+++ src/app/node-data/alibaba-node-data/alibaba-node-data.component.spec.ts
@@ -4,11 +4,15 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import {WizardService} from '../../core/services';
+import {ApiService, DatacenterService, WizardService} from '../../core/services';
 import {NodeDataService} from '../../core/services/node-data/node-data.service';
 import {SharedModule} from '../../shared/shared.module';
+import {fakeAlibabaInstanceTypes} from '../../testing/fake-data/alibaba.fake';
 import {fakeAlibabaCluster} from '../../testing/fake-data/cluster.fake';
+import {fakeAlibabaDatacenter} from '../../testing/fake-data/datacenter.fake';
 import {nodeDataFake} from '../../testing/fake-data/node.fake';
+import {asyncData} from '../../testing/services/api-mock.service';
+
 import {AlibabaNodeDataComponent} from './alibaba-node-data.component';
 
 const modules: any[] = [
@@ -22,8 +26,15 @@ const modules: any[] = [
 describe('AlibabaNodeDataComponent', () => {
   let fixture: ComponentFixture<AlibabaNodeDataComponent>;
   let component: AlibabaNodeDataComponent;
+  let datacenterMock;
+  let apiMock;
 
   beforeEach(async(() => {
+    apiMock = {'getAlibabaInstanceTypes': jest.fn()};
+    apiMock.getAlibabaInstanceTypes.mockReturnValue(asyncData(fakeAlibabaInstanceTypes()));
+    datacenterMock = {'getDataCenter': jest.fn()};
+    datacenterMock.getDataCenter.mockReturnValue(asyncData(fakeAlibabaDatacenter()));
+
     TestBed
         .configureTestingModule({
           imports: [
@@ -35,6 +46,8 @@ describe('AlibabaNodeDataComponent', () => {
           providers: [
             NodeDataService,
             WizardService,
+            {provide: ApiService, useValue: apiMock},
+            {provide: DatacenterService, useValue: datacenterMock},
           ],
         })
         .compileComponents();
--- src/app/shared/entity/provider/alibaba/Alibaba.ts
+++ src/app/shared/entity/provider/alibaba/Alibaba.ts
@@ -1,7 +1,5 @@
 export class AlibabaInstanceType {
   id: string;
-  memorySize: number;
-  cpuCoreCount: number;
 }
 
 export class ZoneID {
