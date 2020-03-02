60366ef6f217b45382879105eb19879496965213
kgroschoff <kgroschoff@gmail.com>
2020-03-02 16:53:08 +0100

    update fake instance types

--- src/app/testing/fake-data/alibaba.fake.ts
+++ src/app/testing/fake-data/alibaba.fake.ts
@@ -2,10 +2,16 @@ import {AlibabaInstanceType} from '../../shared/entity/provider/alibaba/Alibaba'
 
 export function fakeAlibabaInstanceTypes(): AlibabaInstanceType[] {
   return [
-    {'id': 'ecs.s2.small', 'cpuCoreCount': 2, 'memorySize': 2},
-    {'id': 'ecs.s3.medium', 'cpuCoreCount': 4, 'memorySize': 4},
-    {'id': 'ecs.c1.small', 'cpuCoreCount': 8, 'memorySize': 8},
-    {'id': 'ecs.c2.medium', 'cpuCoreCount': 16, 'memorySize': 16},
-    {'id': 'ecs.s1.small', 'cpuCoreCount': 1, 'memorySize': 2},
+    {'id': 'ecs.c4.2xlarge'},
+    {'id': 'ecs.c4.3xlarge'},
+    {'id': 'ecs.c4.xlarge'},
+    {'id': 'ecs.c5.2xlarge'},
+    {'id': 'ecs.c5.3xlarge'},
+    {'id': 'ecs.c5.large'},
+    {'id': 'ecs.c5.xlarge'},
+    {'id': 'ecs.c6.2xlarge'},
+    {'id': 'ecs.c6.3xlarge'},
+    {'id': 'ecs.c6.large'},
+    {'id': 'ecs.c6.xlarge'},
   ];
 }
