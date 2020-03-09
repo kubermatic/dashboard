f66be1052c84d2eb2b1ebfb5778368d2b9a7bc1e
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-03-09 15:29:16 +0100

    Refactor dialog title component Add update available badge

--- src/app/shared/components/dialog-title/dialog-title.component.ts
+++ src/app/shared/components/dialog-title/dialog-title.component.ts
@@ -1,10 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'km-dialog-title',
   templateUrl: './dialog-title.component.html',
   styleUrls: ['./dialog-title.component.scss'],
 })
-export class DialogTitleComponent {
-  @Input() title: string;
-}
+export class DialogTitleComponent {}
