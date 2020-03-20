079a029965386f2ddc018a6892567980dcac9612
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-03-20 14:31:56 +0100

    Format files

--- src/app/node-data-new/module.ts
+++ src/app/node-data-new/module.ts
@@ -26,6 +26,7 @@ const components = [
   AWSExtendedNodeDataComponent,
   DigitalOceanBasicNodeDataComponent,
   DigitalOceanExtendedNodeDataComponent,
+  HetznerBasicNodeDataComponent,
   NodeDataComponent,
   ExtendedNodeDataComponent,
 ];
@@ -48,7 +49,7 @@ const services = [
     MatAutocompleteModule,
     MatTooltipModule,
   ],
-  declarations: [...components, HetznerBasicNodeDataComponent],
+  declarations: [...components],
   providers: [...services],
   exports: [...components],
 })
