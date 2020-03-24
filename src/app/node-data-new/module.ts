5ea9ecc50ada3725aee9ae0364099b79c22ffd9b
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-03-24 12:11:08 +0100

    Add KubeVirt to the new wizard (#2114)
    * Enable provider
    
    * Add basic settings
    
    * Add node data
    
    * Format files
    
    * Fix review findings
    
    * Fix review findings

--- src/app/node-data-new/module.ts
+++ src/app/node-data-new/module.ts
@@ -14,6 +14,7 @@ import {SharedModule} from '../shared/shared.module';
 
 import {AWSBasicNodeDataComponent} from './basic/provider/aws/component';
 import {DigitalOceanBasicNodeDataComponent} from './basic/provider/digitalocean/component';
+import {KubeVirtBasicNodeDataComponent} from './basic/provider/kubevirt/component';
 import {NodeDataComponent} from './component';
 import {ExtendedNodeDataComponent} from './extended/component';
 import {AWSExtendedNodeDataComponent} from './extended/provider/aws/component';
@@ -25,6 +26,7 @@ const components = [
   AWSExtendedNodeDataComponent,
   DigitalOceanBasicNodeDataComponent,
   DigitalOceanExtendedNodeDataComponent,
+  KubeVirtBasicNodeDataComponent,
   NodeDataComponent,
   ExtendedNodeDataComponent,
 ];
