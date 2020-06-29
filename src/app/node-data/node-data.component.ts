51abc458151afe2160d22d8d5091aa86a87ecc4c
Kristin Groschoff <kgroschoff@gmail.com>
2020-06-29 16:58:01 +0200

    enable flatcar for openstack (#2430)

--- src/app/node-data/node-data.component.ts
+++ src/app/node-data/node-data.component.ts
@@ -287,6 +287,7 @@ export class NodeDataComponent implements OnInit, OnDestroy {
     return (
       !!this.cluster.spec.cloud.aws ||
       !!this.cluster.spec.cloud.azure ||
+      !!this.cluster.spec.cloud.openstack ||
       (!!this.cluster.spec.cloud.vsphere && this.isImageAvailableForVsphere(OperatingSystem.Flatcar))
     );
   }
