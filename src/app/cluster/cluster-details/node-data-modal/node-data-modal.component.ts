3399a6bb36f002b771899fed98ce08bcd01bc9bd
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-07-03 11:02:53 +0200

    Add license header to the sources (#2454)
    * Add license header to the sources
    
    * Revert some of the changes
    
    * Revert some of the changes
    
    * Revert some of the changes
    
    * Regenerate package.lock

--- src/app/cluster/cluster-details/node-data-modal/node-data-modal.component.ts
+++ src/app/cluster/cluster-details/node-data-modal/node-data-modal.component.ts
@@ -1,3 +1,14 @@
+// Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import {Component, EventEmitter, Inject, OnDestroy, OnInit, Output} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import * as _ from 'lodash';
