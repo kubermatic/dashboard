29bc847a7353d807ccdf23b0e9513221db894331
Sebastian Florek <sebastian.florek@kubermatic.com>
2020-08-03 15:20:20 +0200

    Fix e2e

--- cypress/utils/wizard.ts
+++ cypress/utils/wizard.ts
@@ -13,7 +13,7 @@ export enum WizardStep {
   Provider = 'provider',
   Cluster = 'cluster',
   ProviderSettings = 'settings',
-  NodeSettings = 'initial nodes',
-  MachineNetwork = 'machine network',
+  NodeSettings = 'initial-nodes',
+  MachineNetwork = 'machine-network',
   Summary = 'summary',
 }
