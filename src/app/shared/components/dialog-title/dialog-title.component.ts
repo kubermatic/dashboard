d5645429a6fb3c2fc1ed419d44e31f13dbea2b61
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-08-17 16:27:17 +0200

    Other adjustments

--- src/app/shared/components/dialog-title/dialog-title.component.ts
+++ src/app/shared/components/dialog-title/dialog-title.component.ts
@@ -14,6 +14,5 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'km-dialog-title',
   templateUrl: './dialog-title.component.html',
-  styleUrls: ['./dialog-title.component.scss'],
 })
 export class DialogTitleComponent {}
