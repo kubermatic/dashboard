075959c94fff87b164c4acde6c70be1b8b69f6a6
Sebastian Florek <s.florek91@gmail.com>
2020-08-18 14:59:44 +0200

    Skip ND test case (#2565)

--- cypress/integration/stories/machine-deployments.spec.ts
+++ cypress/integration/stories/machine-deployments.spec.ts
@@ -23,7 +23,8 @@ import {prefixedString} from '../../utils/random';
 import {View} from '../../utils/view';
 import {WizardStep} from '../../utils/wizard';
 
-describe('Machine Deployments Story', () => {
+// TODO: remove skip once networking issues are fixed
+describe.skip('Machine Deployments Story', () => {
   const email = Cypress.env('KUBERMATIC_DEX_DEV_E2E_USERNAME');
   const password = Cypress.env('KUBERMATIC_DEX_DEV_E2E_PASSWORD');
   const projectName = prefixedString('e2e-test-project');
