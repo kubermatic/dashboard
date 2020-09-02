1373f43b46efa380b32b55b5fe27a42145f7b8f7
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-09-02 11:40:46 +0200

    Remove redundant classes

--- src/app/cluster/cluster-list/cluster-list.component.spec.ts
+++ src/app/cluster/cluster-list/cluster-list.component.spec.ts
@@ -91,7 +91,7 @@ describe('ClusterListComponent', () => {
     component.isInitialized = true;
     fixture.detectChanges();
 
-    const de = fixture.debugElement.query(By.css('.km-with-table-header'));
+    const de = fixture.debugElement.query(By.css('.mat-card'));
 
     expect(de).not.toBeNull();
     discardPeriodicTasks();
