63a2fc45ee4cdcdb983866a58df619ea4e0357b1
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-09-14 15:23:58 +0200

    Remove redundant import

--- src/app/shared/components/external-cluster-data-dialog/component.ts
+++ src/app/shared/components/external-cluster-data-dialog/component.ts
@@ -13,7 +13,6 @@ import {Component, Input, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {MatDialogRef} from '@angular/material/dialog';
 import {ExternalClusterModel} from '../../model/ExternalClusterModel';
-import {Router} from '@angular/router';
 
 export enum Controls {
   Name = 'name',
@@ -30,10 +29,7 @@ export class ExternalClusterDataDialogComponent implements OnInit {
   form: FormGroup;
   kubeconfig = '';
 
-  constructor(
-    private readonly _matDialogRef: MatDialogRef<ExternalClusterDataDialogComponent>,
-    private readonly _router: Router
-  ) {}
+  constructor(private readonly _matDialogRef: MatDialogRef<ExternalClusterDataDialogComponent>) {}
 
   ngOnInit(): void {
     this.form = new FormGroup({
