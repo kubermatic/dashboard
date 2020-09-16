9b270c70fffffaafd3c859e164cda65ee904d3b5
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-09-16 15:10:09 +0200

    Debug

--- hack/e2e/ci-e2e.sh
+++ hack/e2e/ci-e2e.sh
@@ -10,7 +10,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eo pipefail
+# set -eo pipefail
 
 shopt -s nocasematch
 if [[ ${KUBERMATIC_EDITION} = ce ]]; then
