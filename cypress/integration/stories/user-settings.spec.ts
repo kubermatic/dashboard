326a8b3c6c741c2b5609b361d54b6ae1a9a025fb
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2021-03-29 17:11:55 +0200

    Add user settings story

---
+++ cypress/integration/stories/user-settings.spec.ts
@@ -0,0 +1,44 @@
+// Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {login, logout} from '../../utils/auth';
+import {Condition} from '../../utils/condition';
+import {View} from '../../utils/view';
+import {UserSettingsPage} from "../../pages/user-settings.po";
+
+describe('User Settings Story', () => {
+  const email = Cypress.env('KUBERMATIC_DEX_DEV_E2E_USERNAME');
+  const password = Cypress.env('KUBERMATIC_DEX_DEV_E2E_PASSWORD');
+  const kubermaticEdition = Cypress.env('KUBERMATIC_EDITION');
+  const isEnterpriseEdition = kubermaticEdition === 'ee';
+  const themePickerAvailability = isEnterpriseEdition ? 'available' : 'not available';
+
+  it('should login', () => {
+    login(email, password);
+    cy.url().should(Condition.Include, View.Projects);
+  });
+
+  it('should go to the user settings', () => {
+    UserSettingsPage.visit();
+  });
+
+  it(`should check if theme picker is ${themePickerAvailability}`, () => {
+    UserSettingsPage.getThemePicker().should(isEnterpriseEdition ? Condition.Exist : Condition.NotExist);
+  });
+
+  // TODO: Check field availability.
+  // TODO: Modify some of the values
+  // TODO: Check if the modification were applied in the app.
+
+  it('should logout', () => {
+    logout();
+  });
+});
