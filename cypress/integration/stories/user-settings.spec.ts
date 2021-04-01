4b96f7226b1f9ea1a9bae0b660dae502f0bb93c3
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2021-04-01 10:27:01 +0200

    Remove username check

--- cypress/integration/stories/user-settings.spec.ts
+++ cypress/integration/stories/user-settings.spec.ts
@@ -17,7 +17,6 @@ import {ProjectsPage} from '../../pages/projects.po';
 import {prefixedString} from '../../utils/random';
 
 describe('User Settings Story', () => {
-  const username = 'roxy';
   const email = Cypress.env('KUBERMATIC_DEX_DEV_E2E_USERNAME');
   const password = Cypress.env('KUBERMATIC_DEX_DEV_E2E_PASSWORD');
   const projectName = prefixedString('e2e-test-project');
@@ -38,10 +37,6 @@ describe('User Settings Story', () => {
 
   it('should go to the user settings', () => {
     UserSettingsPage.visit();
-  });
-
-  it('should check if user name is correct', () => {
-    UserSettingsPage.getUserName().should(Condition.Contain, username);
   });
 
   it('should check if user email is correct', () => {
