4006543c8ee899a84ad6686ba0d3cfbb9ce22312
kgroschoff <kgroschoff@gmail.com>
2021-04-19 15:26:55 +0200

    use anexia vlan endpoints

---
+++ src/app/core/services/wizard/provider/anexia.ts
@@ -0,0 +1,57 @@
+// Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {HttpClient} from '@angular/common/http';
+import {EMPTY, Observable} from 'rxjs';
+
+import {NodeProvider} from '../../../../shared/model/NodeProviderConstants';
+
+import {Provider} from './provider';
+import {AnexiaVlan} from '../../../../shared/entity/provider/anexia';
+
+export class Anexia extends Provider {
+  constructor(http: HttpClient, provider: NodeProvider) {
+    super(http, provider);
+
+    this._setRequiredHeaders(Anexia.Header.Token);
+  }
+
+  credential(credential: string): Anexia {
+    super._credential(credential);
+    return this;
+  }
+
+  token(token: string): Anexia {
+    if (token) {
+      this._headers = this._headers.set(Anexia.Header.Token, token);
+    }
+    return this;
+  }
+
+  vlans(onLoadingCb: () => void = null): Observable<AnexiaVlan[]> {
+    if (!this._hasRequiredHeaders()) {
+      return EMPTY;
+    }
+
+    if (onLoadingCb) {
+      onLoadingCb();
+    }
+
+    const url = `${this._restRoot}/providers/${this._provider}/vlans`;
+    return this._http.get<AnexiaVlan[]>(url, {headers: this._headers});
+  }
+}
+
+export namespace Anexia {
+  export enum Header {
+    Token = 'Token',
+  }
+}
---
+++ src/app/node-data/service/provider/anexia.ts
@@ -0,0 +1,74 @@
+// Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {ApiService} from '@core/services/api/service';
+import {ProjectService} from '@core/services/project/service';
+import {PresetsService} from '@core/services/wizard/presets.service';
+import {AnexiaVlan} from '@shared/entity/provider/anexia';
+import {NodeProvider} from '@shared/model/NodeProviderConstants';
+import {ClusterService} from '@shared/services/cluster.service';
+import {Observable, of, onErrorResumeNext} from 'rxjs';
+import {catchError, filter, take, switchMap, tap} from 'rxjs/operators';
+import {NodeDataMode} from '../../config';
+import {NodeDataService} from '../service';
+
+export class NodeDataAnexiaProvider {
+  constructor(
+    private readonly _nodeDataService: NodeDataService,
+    private readonly _clusterService: ClusterService,
+    private readonly _presetService: PresetsService,
+    private readonly _apiService: ApiService,
+    private readonly _projectService: ProjectService
+  ) {}
+
+  vlans(onError: () => void = undefined, onLoadingCb: () => void = null): Observable<AnexiaVlan[]> {
+    switch (this._nodeDataService.mode) {
+      case NodeDataMode.Wizard:
+        return this._clusterService.clusterChanges
+          .pipe(filter(_ => this._clusterService.provider === NodeProvider.ANEXIA))
+          .pipe(
+            switchMap(cluster =>
+              this._presetService
+                .provider(NodeProvider.ANEXIA)
+                .token(cluster.spec.cloud.anexia.token)
+                .credential(this._presetService.preset)
+                .vlans(onLoadingCb)
+                .pipe(
+                  catchError(_ => {
+                    if (onError) {
+                      onError();
+                    }
+
+                    return onErrorResumeNext(of([]));
+                  })
+                )
+            )
+          );
+      case NodeDataMode.Dialog: {
+        let selectedProject: string;
+        return this._projectService.selectedProject
+          .pipe(tap(project => (selectedProject = project.id)))
+          .pipe(tap(_ => (onLoadingCb ? onLoadingCb() : null)))
+          .pipe(switchMap(_ => this._apiService.getAnexiaVlans(selectedProject, this._clusterService.cluster.id)))
+          .pipe(
+            catchError(_ => {
+              if (onError) {
+                onError();
+              }
+
+              return onErrorResumeNext(of([]));
+            })
+          )
+          .pipe(take(1));
+      }
+    }
+  }
+}
