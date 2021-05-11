69a87112c86cc30b69c4437493e17ffb67388a9c
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2021-05-11 19:18:06 +0200

    Retry

--- cypress/utils/auth.ts
+++ cypress/utils/auth.ts
@@ -35,6 +35,6 @@ export function login(email: string, password: string): void {
 
 export function logout(): void {
   UserPanel.logout();
-  DexPage.getLoginBtn().should(Condition.Exist);
+  LoginPage.getLoginBtn().should(Condition.Exist);
   cy.reload();
 }
