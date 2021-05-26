7c1875b811f914dbcb1c84c42c7c389c9f8514c6
Sebastian Florek <sebastian.florek@kubermatic.com>
2021-05-26 10:22:46 +0200

    Increase retry limit for administrators e2e

--- cypress/integration/stories/admin-settings/administrators.spec.ts
+++ cypress/integration/stories/admin-settings/administrators.spec.ts
@@ -20,7 +20,7 @@ describe('Admin Settings - Administrators Story', () => {
   const userEmail = Cypress.env('KUBERMATIC_DEX_DEV_E2E_USERNAME');
   const adminEmail = Cypress.env('KUBERMATIC_DEX_DEV_E2E_USERNAME_2');
   const password = Cypress.env('KUBERMATIC_DEX_DEV_E2E_PASSWORD');
-  const retries = 5;
+  const retries = 15;
   let adminsCount = 1;
 
   it('should login as admin', () => {
