2b604375a2e9088d5a5d4f9d77024811f0645786
Sebastian Florek <sebastian.florek@kubermatic.com>
2021-05-26 14:11:09 +0200

    Disable administrators e2e test

--- cypress/integration/stories/admin-settings/administrators.spec.ts
+++ cypress/integration/stories/admin-settings/administrators.spec.ts
@@ -16,7 +16,7 @@ import {AdminSettingsPage} from '../../../pages/admin-settings.po';
 import {RequestType, Response, ResponseType, TrafficMonitor} from '../../../utils/monitor';
 import {Endpoint} from '../../../utils/endpoint';
 
-describe('Admin Settings - Administrators Story', () => {
+xdescribe('Admin Settings - Administrators Story', () => {
   const userEmail = Cypress.env('KUBERMATIC_DEX_DEV_E2E_USERNAME');
   const adminEmail = Cypress.env('KUBERMATIC_DEX_DEV_E2E_USERNAME_2');
   const password = Cypress.env('KUBERMATIC_DEX_DEV_E2E_PASSWORD');
