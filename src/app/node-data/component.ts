b1e290f839151b95769641b94e1387a367a079c4
kgroschoff <kgroschoff@gmail.com>
2021-06-07 13:14:38 +0200

    remove unused

--- src/app/node-data/component.ts
+++ src/app/node-data/component.ts
@@ -175,13 +175,10 @@ export class NodeDataComponent extends BaseFormValidator implements OnInit, OnDe
       .pipe(takeUntil(this._unsubscribe))
       .subscribe(_ => (this._nodeDataService.operatingSystemSpec = this._getOperatingSystemSpec()));
 
-    this._settingsService.adminSettings
-      .pipe(take(1))
-      .pipe(takeUntil(this._unsubscribe))
-      .subscribe(settings => {
-        const replicas = this.dialogEditMode ? this._nodeDataService.nodeData.count : settings.defaultNodeCount;
-        this.form.get(Controls.Count).setValue(replicas);
-      });
+    this._settingsService.adminSettings.pipe(take(1)).subscribe(settings => {
+      const replicas = this.dialogEditMode ? this._nodeDataService.nodeData.count : settings.defaultNodeCount;
+      this.form.get(Controls.Count).setValue(replicas);
+    });
   }
 
   ngOnDestroy(): void {
