f4f80dc05cdaefe81d5aee2a40864ba4cba9b038
Sebastian Florek <sebastian.florek@kubermatic.com>
2021-07-01 10:55:53 +0200

    Improve go back button logic

--- src/app/settings/admin/routing.ts
+++ src/app/settings/admin/routing.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: 'administrators',
+        redirectTo: 'defaults',
         pathMatch: 'full',
       },
       {
