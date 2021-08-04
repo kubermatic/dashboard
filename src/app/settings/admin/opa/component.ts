173fa4804d2cbe8e99b942483f4404c5bd7d4dfc
kgroschoff <kgroschoff@gmail.com>
2021-08-04 16:09:04 +0200

    add OnDestroy

--- src/app/settings/admin/opa/component.ts
+++ src/app/settings/admin/opa/component.ts
@@ -9,7 +9,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnDestroy} from '@angular/core';
 import {Context} from '@shared/components/tab-card/component';
 import {DynamicTabComponent} from '@shared/components/tab-card/dynamic-tab/component';
 import {DynamicTab} from '@shared/model/dynamic-tab';
@@ -21,7 +21,7 @@ import {takeUntil} from 'rxjs/operators';
   templateUrl: './template.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdminSettingsOPAComponent {
+export class AdminSettingsOPAComponent implements OnDestroy {
   private readonly _unsubscribe = new Subject<void>();
   readonly Context = Context;
   private _dynamicTabs = new Set<DynamicTabComponent>();
@@ -32,5 +32,10 @@ export class AdminSettingsOPAComponent {
 
   onActivate(component: DynamicTab): void {
     component.onTabReady.pipe(takeUntil(this._unsubscribe)).subscribe(tab => this._dynamicTabs.add(tab));
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribe.next();
+    this._unsubscribe.complete();
   }
 }
