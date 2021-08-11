97aa08ed80a9a5dbb7275b90397117544a815fea
Marcin Maciaszczyk <marcin9yk@icloud.com>
2021-08-11 11:34:57 +0200

    Improve build config

---
+++ src/app/dynamic/module-registry.ts
@@ -0,0 +1,19 @@
+// Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This is the dynamic module registry file that will be used by default in enterprise edition builds.
+// In the community edition builds it will be replaced by the module-registry.ce.ts file.
+// The configuration can be found in the angular.json and package.json files.
+// IMPORTANT: Keep in sync with module-registry.ce.ts file.
+
+export function loadThemingModule() {
+  return import('./enterprise/theming/module').then(module => module.ThemingModule);
+}
--- src/app/settings/user/routing.ts
+++ src/app/settings/user/routing.ts
@@ -11,7 +11,7 @@
 
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {getEditionDirName} from '@app/dynamic/common';
+import {loadThemingModule} from '@app/dynamic/module-registry';
 import {AuthGuard} from '@core/services/auth/guard';
 import {UserSettingsComponent} from './component';
 
@@ -23,8 +23,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        loadChildren: () =>
-          import(`../../dynamic/${getEditionDirName()}/theming/module`).then(module => module.ThemingModule),
+        loadChildren: () => loadThemingModule(),
         data: {preload: true},
       },
     ],
