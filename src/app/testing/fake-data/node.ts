b7542e89a852e194d4b97722b154014d1ad5a0d3
Marcin Maciaszczyk <marcin9yk@icloud.com>
2021-08-16 23:33:48 +0200

    Add Flatcar support for KubeVirt (#3561)

--- src/app/testing/fake-data/node.ts
+++ src/app/testing/fake-data/node.ts
@@ -59,7 +59,7 @@ export function nodeFake(): Node {
         },
       ],
       nodeInfo: {
-        kernelVersion: '4.14.11-coreos',
+        kernelVersion: '4.14.11-aws',
         kubeletVersion: 'v1.8.5',
         operatingSystem: 'linux',
         architecture: 'amd64',
@@ -232,7 +232,7 @@ export function nodesFake(): Node[] {
           },
         ],
         nodeInfo: {
-          kernelVersion: '4.14.11-coreos',
+          kernelVersion: '4.14.11-aws',
           kubeletVersion: 'v1.8.5',
           operatingSystem: 'linux',
           architecture: 'amd64',
@@ -286,7 +286,7 @@ export function nodesFake(): Node[] {
           },
         ],
         nodeInfo: {
-          kernelVersion: '4.14.11-coreos',
+          kernelVersion: '4.14.11-aws',
           kubeletVersion: null,
           operatingSystem: 'linux',
           architecture: 'amd64',
@@ -357,115 +357,6 @@ export function nodeDataCentOsFake(): NodeData {
       operatingSystem: {
         centos: {
           distUpgradeOnBoot: false,
-        },
-      },
-      versions: {
-        kubelet: null,
-      },
-    },
-    count: 3,
-    valid: true,
-  };
-}
-
-export function nodeDataSLESFake(): NodeData {
-  return {
-    spec: {
-      cloud: {
-        digitalocean: {
-          size: 's-1vcpu-1gb',
-          backups: false,
-          ipv6: false,
-          monitoring: false,
-          tags: [],
-        },
-        aws: {
-          instanceType: 't3.small',
-          diskSize: 25,
-          volumeType: 'standard',
-          ami: '',
-          tags: {},
-          subnetID: 'subnet-f3427db9',
-          availabilityZone: 'eu-central-1c',
-        },
-        openstack: {
-          flavor: 'm1.small',
-          image: '',
-          useFloatingIP: false,
-          tags: {},
-          instanceReadyCheckPeriod: '10',
-          instanceReadyCheckTimeout: '120',
-        },
-        hetzner: {
-          type: 'cx31',
-        },
-        vsphere: {
-          cpus: 1,
-          memory: 512,
-          template: '',
-          diskSizeGB: 256,
-        },
-        azure: {
-          size: 'cx31',
-          assignPublicIP: false,
-          zones: ['1'],
-          tags: {},
-          dataDiskSize: 0,
-          osDiskSize: 0,
-        },
-        gcp: {
-          diskSize: 25,
-          diskType: 'pd-ssd',
-          machineType: '',
-          zone: '',
-          preemptible: false,
-          tags: [],
-          labels: {},
-        },
-      },
-      operatingSystem: {
-        sles: {
-          distUpgradeOnBoot: false,
-        },
-      },
-      versions: {
-        kubelet: null,
-      },
-    },
-    count: 3,
-    valid: true,
-  };
-}
-
-export function nodeDataRHELFake(): NodeData {
-  return {
-    spec: {
-      cloud: {
-        aws: {
-          instanceType: 't3.small',
-          diskSize: 25,
-          volumeType: 'standard',
-          ami: '',
-          tags: {},
-          subnetID: 'subnet-f3427db9',
-          availabilityZone: 'eu-central-1c',
-        },
-        gcp: {
-          diskSize: 25,
-          diskType: 'pd-ssd',
-          machineType: '',
-          zone: '',
-          preemptible: false,
-          tags: [],
-          labels: {},
-        },
-      },
-      operatingSystem: {
-        rhel: {
-          distUpgradeOnBoot: false,
-          rhelSubscriptionManagerUser: 'user',
-          rhelSubscriptionManagerPassword: 'password',
-          rhsmOfflineToken: 'dasdsaeiq3jjrojiq3wnjrfoqw3r',
         },
       },
       versions: {
