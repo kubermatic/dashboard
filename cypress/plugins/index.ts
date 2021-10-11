005a8fc4e70c00dcf058ddb800ca7d9f66efcf02
Marcin Maciaszczyk <marcin9yk@icloud.com>
2021-10-11 16:16:45 +0200

    Fix ignore

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -20,12 +20,12 @@ export default async (on, config) => {
   if (useMocks) {
     // Skip everything except already mocked tests.
     // TODO: Remove all ignores once all tests have their mocks configured.
-    config.ignoreTestFiles = ['**/integration/stories/*.spec.ts'];
+    config.ignoreTestFiles = ['**/integration/stories/**.spec.ts'];
   } else {
     // Skip flaky and already mocked tests.
     // TODO: Remove all ignores after fixing flaky tests and making the full tests optional.
     config.ignoreTestFiles = [
-      '**/integration/providers/*.spec.ts',
+      '**/integration/providers/**.spec.ts',
       '**/integration/stories/machine-deployment.spec.ts',
       '**/integration/stories/opa.spec.ts',
       '**/integration/stories/admin-settings/administrators.spec.ts',
