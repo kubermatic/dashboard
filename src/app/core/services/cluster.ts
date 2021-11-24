2d009f810fd906a59938f2b8a6c2127e6d154b86
Marcin Maciaszczyk <marcin9yk@icloud.com>
2021-11-24 10:39:27 +0100

    Remove logging

--- src/app/core/services/cluster.ts
+++ src/app/core/services/cluster.ts
@@ -115,9 +115,6 @@ export class ClusterService {
   }
 
   addExternalCluster(projectID: string, model: ExternalClusterModel): Observable<Cluster> {
-    console.log(projectID);
-    console.log(model);
-
     const url = `${this._newRestRoot}/projects/${projectID}/kubernetes/clusters`;
     return this._http.post<Cluster>(url, model);
   }
