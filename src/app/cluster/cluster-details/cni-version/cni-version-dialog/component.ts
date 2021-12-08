23a9dc0c17fb834b732d52f14c795b1df77821aa
kgroschoff <kgroschoff@gmail.com>
2021-12-08 09:44:49 +0100

    fix review remark

--- src/app/cluster/cluster-details/cni-version/cni-version-dialog/component.ts
+++ src/app/cluster/cluster-details/cni-version/cni-version-dialog/component.ts
@@ -46,9 +46,7 @@ export class CNIVersionDialog implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    this.selectedCNIVersion = this.data.cluster?.spec?.cniPlugin?.version
-      ? this.data.cluster.spec.cniPlugin.version
-      : '';
+    this.selectedCNIVersion = this.data.cluster?.spec?.cniPlugin?.version || '';
 
     this._projectService.selectedProject
       .pipe(takeUntil(this._unsubscribe))
