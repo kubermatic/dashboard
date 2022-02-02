cac74209de4b16f1ff1f72132971ccd1664cb129
Marcin Maciaszczyk <marcin9yk@icloud.com>
2022-02-02 11:11:02 +0100

    Add basic provider step

---
+++ src/app/wizard/step/provider-settings/provider/basic/nutanix/component.ts
@@ -0,0 +1,118 @@
+// Copyright 2020 The Kubermatic Kubernetes Platform contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {Component, forwardRef, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validators} from '@angular/forms';
+import {ClusterSpecService} from '@core/services/cluster-spec';
+import {PresetsService} from '@core/services/wizard/presets';
+import {CloudSpec, Cluster, ClusterSpec, NutanixCloudSpec} from '@shared/entity/cluster';
+import {NodeProvider} from '@shared/model/NodeProviderConstants';
+import {BaseFormValidator} from '@shared/validators/base-form.validator';
+import {merge} from 'rxjs';
+import {distinctUntilChanged, filter, takeUntil} from 'rxjs/operators';
+
+export enum Controls {
+  Username = 'username',
+  Password = 'password',
+  ProxyURL = 'proxyURL',
+}
+
+@Component({
+  selector: 'km-wizard-nutanix-provider-basic',
+  templateUrl: './template.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => NutanixProviderBasicComponent),
+      multi: true,
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => NutanixProviderBasicComponent),
+      multi: true,
+    },
+  ],
+})
+export class NutanixProviderBasicComponent extends BaseFormValidator implements OnInit, OnDestroy {
+  readonly Controls = Controls;
+
+  constructor(
+    private readonly _builder: FormBuilder,
+    private readonly _presets: PresetsService,
+    private readonly _clusterSpecService: ClusterSpecService
+  ) {
+    super('Nutanix Provider Basic');
+  }
+
+  ngOnInit(): void {
+    this.form = this._builder.group({
+      [Controls.Username]: this._builder.control('', [Validators.required]),
+      [Controls.Password]: this._builder.control('', [Validators.required]),
+      [Controls.ProxyURL]: this._builder.control('', [Validators.required]),
+    });
+
+    this.form.valueChanges
+      .pipe(filter(_ => this._clusterSpecService.provider === NodeProvider.HETZNER))
+      .pipe(takeUntil(this._unsubscribe))
+      .subscribe(_ =>
+        this._presets.enablePresets(Object.values(Controls).every(control => !this.form.get(control).value))
+      );
+
+    this._presets.presetChanges
+      .pipe(takeUntil(this._unsubscribe))
+      .subscribe(preset => Object.values(Controls).forEach(control => this._enable(!preset, control)));
+
+    merge(
+      this.form.get(Controls.Username).valueChanges,
+      this.form.get(Controls.Password).valueChanges,
+      this.form.get(Controls.ProxyURL).valueChanges
+    )
+      .pipe(distinctUntilChanged())
+      .pipe(takeUntil(this._unsubscribe))
+      .subscribe(_ => (this._clusterSpecService.cluster = this._getClusterEntity()));
+
+    merge(this._clusterSpecService.providerChanges, this._clusterSpecService.datacenterChanges)
+      .pipe(takeUntil(this._unsubscribe))
+      .subscribe(_ => this.form.reset());
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribe.next();
+    this._unsubscribe.complete();
+  }
+
+  private _enable(enable: boolean, name: string): void {
+    if (enable && this.form.get(name).disabled) {
+      this.form.get(name).enable();
+    }
+
+    if (!enable && this.form.get(name).enabled) {
+      this.form.get(name).disable();
+    }
+  }
+
+  private _getClusterEntity(): Cluster {
+    return {
+      spec: {
+        cloud: {
+          nutanix: {
+            username: this.form.get(Controls.Username).value,
+            password: this.form.get(Controls.Password).value,
+            proxyURL: this.form.get(Controls.ProxyURL).value,
+          } as NutanixCloudSpec,
+        } as CloudSpec,
+      } as ClusterSpec,
+    } as Cluster;
+  }
+}
