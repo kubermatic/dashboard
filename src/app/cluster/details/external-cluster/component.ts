18947ed1d87d953e50ab84441a54a5734da79aa2
Ahmad Hamzh <ahmadhamzh@ymail.com>
2022-06-07 19:00:44 +0300

    revert go back method in external cluster

--- src/app/cluster/details/external-cluster/component.ts
+++ src/app/cluster/details/external-cluster/component.ts
@@ -14,7 +14,7 @@
 
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {AppConfigService} from '@app/config.service';
 import {ClusterService} from '@core/services/cluster';
 import {PathParam} from '@core/services/params';
@@ -31,6 +31,7 @@ import {forkJoin, of, Subject, timer} from 'rxjs';
 import {switchMap, take, takeUntil, tap} from 'rxjs/operators';
 import {ExternalMachineDeployment} from '@shared/entity/external-machine-deployment';
 import {MasterVersion} from '@shared/entity/cluster';
+import {ClusterListTab} from '@app/cluster/list/component';
 
 @Component({
   selector: 'km-external-cluster-details',
@@ -61,6 +62,7 @@ export class ExternalClusterDetailsComponent implements OnInit, OnDestroy {
 
   constructor(
     private readonly _activatedRoute: ActivatedRoute,
+    private readonly _router: Router,
     private readonly _matDialog: MatDialog,
     private readonly _clusterService: ClusterService,
     private readonly _userService: UserService,
@@ -158,6 +160,12 @@ export class ExternalClusterDetailsComponent implements OnInit, OnDestroy {
     const dialog = this._matDialog.open(EditClusterConnectionDialogComponent);
     dialog.componentInstance.projectId = this.projectID;
     dialog.componentInstance.name = this.cluster.name;
+  }
+
+  goBack(): void {
+    this._router.navigate(['/projects/' + this.projectID + '/clusters'], {
+      fragment: `${ClusterListTab.ExternalCluster}`,
+    });
   }
 
   canDisconnect(): boolean {
