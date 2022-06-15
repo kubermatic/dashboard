d3a5fd56763ecb58409f2cb103e0f22baadeac5c
Ahmad Hamzh <85109141+ahmadhamzh@users.noreply.github.com>
2022-06-15 17:35:03 +0300

    add view name in header (#4503)
    * add view name in header
    
    * adjust styling
    
    * solve the comments
    
    * using router events inside ngOnit
    
    * typo
    
    * use classname instead of the element name
    
    * adjusting styling and naming convention
    
    * add new class for logo border
    
    * adjust class for header logo
    
    * Handle viewname for tos, rest-api routes otherwise handle empty view for non catered routes
    
    Co-authored-by: KhizerRehan <khizerrehan92@gmail.com>

--- src/app/core/components/navigation/component.ts
+++ src/app/core/components/navigation/component.ts
@@ -13,10 +13,27 @@
 // limitations under the License.
 
 import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
 import {Auth} from '@core/services/auth/service';
 import {UserService} from '@core/services/user';
 import {merge, of, Subject} from 'rxjs';
-import {switchMapTo, take, takeUntil} from 'rxjs/operators';
+import {switchMapTo, take, takeUntil, filter} from 'rxjs/operators';
+
+const enum ViewName {
+  Projects = 'Projects',
+  UserSettings = 'User Settings',
+  AdminSettings = 'Admin Settings',
+  TermsOfServices = 'Terms of Service',
+  RestApi = 'API Docs',
+}
+
+const enum RoutePath {
+  Projects = 'projects',
+  Account = 'account',
+  Settings = 'settings',
+  TermsOfServices = 'terms-of-service',
+  RestApi = 'rest-api',
+}
 
 @Component({
   selector: 'km-navigation',
@@ -28,14 +45,49 @@ export class NavigationComponent implements OnInit, OnDestroy {
   private readonly _onSettingsChange = new Subject<void>();
   @Input() showMenuSwitchAndProjectSelector: boolean;
   showSidenav = true;
+  currentView: string;
 
-  constructor(private readonly _auth: Auth, private readonly _userService: UserService) {}
+  constructor(
+    private readonly _auth: Auth,
+    private readonly _userService: UserService,
+    private readonly _router: Router
+  ) {}
 
   ngOnInit(): void {
     merge(of(true), this._onSettingsChange)
       .pipe(switchMapTo(this._userService.currentUserSettings))
       .pipe(takeUntil(this._unsubscribe))
       .subscribe(settings => (this.showSidenav = !settings.collapseSidenav));
+
+    this._router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        takeUntil(this._unsubscribe)
+      )
+      .subscribe({
+        next: (event: NavigationEnd) => {
+          const currentRoute = event.url.split('/')[1];
+          switch (currentRoute) {
+            case RoutePath.Projects:
+              this.currentView = ViewName.Projects;
+              break;
+            case RoutePath.Account:
+              this.currentView = ViewName.UserSettings;
+              break;
+            case RoutePath.Settings:
+              this.currentView = ViewName.AdminSettings;
+              break;
+            case RoutePath.TermsOfServices:
+              this.currentView = ViewName.TermsOfServices;
+              break;
+            case RoutePath.RestApi:
+              this.currentView = ViewName.RestApi;
+              break;
+            default:
+              this.currentView = '';
+          }
+        },
+      });
   }
 
   ngOnDestroy(): void {
