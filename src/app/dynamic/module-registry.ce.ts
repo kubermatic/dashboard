Talha-Jamil-TJ/kubermatic-dashboard

add project quotes page to admin settings
Signed-off-by: Talha Jamil TJ <talha.jamil@kubermatic.com>

src/app/dynamic/module-registry.ce.ts
@@ -20,4 +20,5 @@ export namespace DynamicModule {
     module => module.AllowedRegistriesModule
   );
   export const Metering = import('./community/metering/module').then(module => module.MeteringModule);
+  export const Quotas = import('./enterprise/quotas/module').then(module => module.QuotasModule);
 }

kubermatic/dashboard

src/app/dynamic/module-registry.ce.ts
@@ -20,4 +20,5 @@ export namespace DynamicModule {
     module => module.AllowedRegistriesModule
   );
   export const Metering = import('./community/metering/module').then(module => module.MeteringModule);
+  export const Quotas = import('./community/quotas/module').then(module => module.QuotasModule);
 }