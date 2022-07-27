a651d91e533d70a59c153c86a5f27c91b6dfa4a7
Ahmad Hamzh <ahmadhamzh@ymail.com>
2022-07-27 15:13:52 +0300

    revert styling changes

--- src/app/core/services/external-cluster.ts
+++ src/app/core/services/external-cluster.ts
@@ -245,11 +245,6 @@ export class ExternalClusterService {
     return this._http.get<string[]>(url, {headers}).pipe(catchError(() => of<[]>()));
   }
 
-  getEKSSubnetsForCluster(vpcId: string, projectID: string, clusterID: string): Observable<string[]> {
-    const url = `${this._newRestRoot}/projects/${projectID}/kubernetes/clusters/${clusterID}/providers/eks/subnets`;
-    return this._http.get<string[]>(url, {headers: {VpcId: vpcId}}).pipe(catchError(() => of<[]>()));
-  }
-
   getEKSSubnetsForCreateMachineDeployment(projectID: string, clusterID: string, vpcId: string): Observable<string[]> {
     const url = `${this._newRestRoot}/projects/${projectID}/kubernetes/clusters/${clusterID}/providers/eks/subnets`;
     return this._http.get<string[]>(url, {headers: {VpcId: vpcId}}).pipe(catchError(() => of<[]>()));
