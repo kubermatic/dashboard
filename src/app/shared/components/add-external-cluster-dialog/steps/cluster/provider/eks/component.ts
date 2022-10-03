68d2d280587aaff13a34e27b69a574303ff2b52b
Ahmad Hamzh <ahmadhamzh@ymail.com>
2022-10-03 16:42:40 +0300

    add back button

--- src/app/shared/components/add-external-cluster-dialog/steps/cluster/provider/eks/component.ts
+++ src/app/shared/components/add-external-cluster-dialog/steps/cluster/provider/eks/component.ts
@@ -61,6 +61,7 @@ export class EKSClusterSelectComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this._unsubscribe.next();
     this._unsubscribe.complete();
+    this._externalClusterService.clusterStepValidity = false;
   }
 
   get isEmpty(): boolean {
