ed2c88e78b6dbd029812cf3d8f7d7a1547dc672f
pprzekwas <patryk@kubermatic.com>
2022-11-10 14:16:14 +0100

    Add OpenIDAuthPlugin feature gate to API (#5245)
    * Add OpenIDAuthPlugin feature gate to API
    
    * Add OpenIDAuthPlugin feature gate to API

--- pkg/provider/kubernetes/featuregates.go
+++ pkg/provider/kubernetes/featuregates.go
@@ -39,6 +39,9 @@ func (fg featureGatesProvider) GetFeatureGates() (apiv2.FeatureGates, error) {
 	if v, ok := fg[features.OIDCKubeCfgEndpoint]; ok {
 		f.OIDCKubeCfgEndpoint = &v
 	}
+	if v, ok := fg[features.OpenIDAuthPlugin]; ok {
+		f.OpenIDAuthPlugin = &v
+	}
 
 	return f, nil
 }
