2f1d5acaf59eff48df275960f55c3055c8a96a94
khizerrehan <khizerrehan92@gmail.com>
2022-11-22 12:53:38 +0500

    Gloabl fix of yaml dialog height

--- modules/web/src/app/shared/components/editor/component.ts
+++ modules/web/src/app/shared/components/editor/component.ts
@@ -33,7 +33,7 @@ export enum EditorHeaderClass {
 export class EditorComponent implements OnInit {
   @Input() header = '';
   @Input() headerClass: EditorHeaderClass = EditorHeaderClass.Dialog;
-  @Input() height = '200px';
+  @Input() height = '300px';
   @Input() language = 'yaml';
   @Input() model: string;
   @Output() modelChange = new EventEmitter<string>();
