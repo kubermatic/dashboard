51fb0d937127d4c85addee8870f5fbfa541f35c2
Waseem Abbas <waseemabbas8261@gmail.com>
2022-12-08 15:13:33 +0500

    Cherrypick autoscaling nodes config PRs #5369 and #5398

--- src/app/core/services/node.ts
+++ src/app/core/services/node.ts
@@ -36,6 +36,8 @@ export class NodeService {
         template: nodeData.spec,
         replicas: nodeData.count,
         dynamicConfig: nodeData.dynamicConfig,
+        minReplicas: nodeData.minReplicas,
+        maxReplicas: nodeData.maxReplicas,
       },
     };
     if (nodeData.operatingSystemProfile) {
@@ -52,6 +54,8 @@ export class NodeService {
         replicas: data.nodeData.count,
         template: data.nodeData.spec,
         dynamicConfig: data.nodeData.dynamicConfig,
+        minReplicas: data.nodeData.minReplicas,
+        maxReplicas: data.nodeData.maxReplicas,
       },
     };
 
@@ -110,6 +114,8 @@ export class NodeService {
           name: md.name,
           spec: md.spec.template,
           dynamicConfig: md.spec.dynamicConfig,
+          minReplicas: md.spec.minReplicas,
+          maxReplicas: md.spec.maxReplicas,
         } as NodeData,
       },
     });
