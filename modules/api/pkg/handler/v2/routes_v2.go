24aaa7d078d492810b8b00f57eb332e2b3c53567
Marvin Beckers <marvin@kubermatic.com>
2022-12-09 08:48:54 +0100

    Add project-scoped API endpoint for Equinix Metal and add no credentials endpoint for equinix
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- modules/api/pkg/handler/v2/routes_v2.go
+++ modules/api/pkg/handler/v2/routes_v2.go
@@ -866,11 +866,12 @@ func (r Routing) RegisterV2(mux *mux.Router, oidcKubeConfEndpoint bool, oidcCfg 
 				Path("/projects/{project_id}/providers/openstack/availabilityzones").
 				Handler(r.listProjectOpenstackAvailabilityZones())
 
-			// Equinix Metal (Packet) endpoints
-			mux.Methods(http.MethodGet).
-				Path("/projects/{project_id}/providers/packet/sizes").
-				Handler(r.listProjectPacketSizes())
 		*/
+
+	// Equinix Metal (Packet) endpoints
+	mux.Methods(http.MethodGet).
+		Path("/projects/{project_id}/providers/equinixmetal/sizes").
+		Handler(r.listProjectEquinixMetalSizes())
 
 	// Hetzner endpoints
 	mux.Methods(http.MethodGet).
@@ -1005,6 +1006,10 @@ func (r Routing) RegisterV2(mux *mux.Router, oidcKubeConfEndpoint bool, oidcCfg 
 	mux.Methods(http.MethodGet).
 		Path("/projects/{project_id}/clusters/{cluster_id}/providers/packet/sizes").
 		Handler(r.listPacketSizesNoCredentials())
+
+	mux.Methods(http.MethodGet).
+		Path("/projects/{project_id}/clusters/{cluster_id}/providers/equinixmetal/sizes").
+		Handler(r.listEquinixMetalSizesNoCredentials())
 
 	mux.Methods(http.MethodGet).
 		Path("/projects/{project_id}/clusters/{cluster_id}/providers/anexia/vlans").
@@ -4384,7 +4389,7 @@ func (r Routing) listAlibabaVSwitchesNoCredentials() http.Handler {
 
 // swagger:route GET /api/v2/projects/{project_id}/clusters/{cluster_id}/providers/packet/sizes packet listPacketSizesNoCredentialsV2
 //
-// Lists sizes from packet
+// Lists sizes from packet (use Equinix Metal API endpoints instead).
 //
 //	Produces:
 //	- application/json
@@ -4399,8 +4404,32 @@ func (r Routing) listPacketSizesNoCredentials() http.Handler {
 			middleware.UserSaver(r.userProvider),
 			middleware.SetClusterProvider(r.clusterProviderGetter, r.seedsGetter),
 			middleware.SetPrivilegedClusterProvider(r.clusterProviderGetter, r.seedsGetter),
-		)(provider.PacketSizesWithClusterCredentialsEndpoint(r.projectProvider, r.privilegedProjectProvider, r.seedsGetter, r.userInfoGetter, r.settingsProvider)),
-		provider.DecodePacketSizesNoCredentialsReq,
+		)(provider.EquinixMetalSizesWithClusterCredentialsEndpoint(r.projectProvider, r.privilegedProjectProvider, r.seedsGetter, r.userInfoGetter, r.settingsProvider)),
+		provider.DecodeEquinixMetalSizesNoCredentialsReq,
+		handler.EncodeJSON,
+		r.defaultServerOptions()...,
+	)
+}
+
+// swagger:route GET /api/v2/projects/{project_id}/clusters/{cluster_id}/providers/equinixmetal/sizes equinixmetal listEquinixMetalSizesNoCredentialsV2
+//
+// Lists sizes from Equinix Metal.
+//
+//	Produces:
+//	- application/json
+//
+//	Responses:
+//	  default: errorResponse
+//	  200: []PacketSizeList
+func (r Routing) listEquinixMetalSizesNoCredentials() http.Handler {
+	return httptransport.NewServer(
+		endpoint.Chain(
+			middleware.TokenVerifier(r.tokenVerifiers, r.userProvider),
+			middleware.UserSaver(r.userProvider),
+			middleware.SetClusterProvider(r.clusterProviderGetter, r.seedsGetter),
+			middleware.SetPrivilegedClusterProvider(r.clusterProviderGetter, r.seedsGetter),
+		)(provider.EquinixMetalSizesWithClusterCredentialsEndpoint(r.projectProvider, r.privilegedProjectProvider, r.seedsGetter, r.userInfoGetter, r.settingsProvider)),
+		provider.DecodeEquinixMetalSizesNoCredentialsReq,
 		handler.EncodeJSON,
 		r.defaultServerOptions()...,
 	)
@@ -6035,6 +6064,28 @@ func (r Routing) listProjectNutanixCategoryValues() http.Handler {
 			middleware.UserSaver(r.userProvider),
 		)(provider.NutanixCategoryValuesEndpoint(r.presetProvider, r.seedsGetter, r.userInfoGetter, true)),
 		provider.DecodeNutanixProjectCategoryValueReq,
+		handler.EncodeJSON,
+		r.defaultServerOptions()...,
+	)
+}
+
+// swagger:route GET /api/v2/projects/{project_id}/providers/equinixmetal/sizes equinixmetal listProjectEquinixMetalSizes
+//
+// Lists sizes from Equinix Metal.
+//
+//	Produces:
+//	- application/json
+//
+//	Responses:
+//	  default: errorResponse
+//	  200: []PacketSizeList
+func (r Routing) listProjectEquinixMetalSizes() http.Handler {
+	return httptransport.NewServer(
+		endpoint.Chain(
+			middleware.TokenVerifier(r.tokenVerifiers, r.userProvider),
+			middleware.UserSaver(r.userProvider),
+		)(provider.EquinixMetalProjectSizesEndpoint(r.presetProvider, r.userInfoGetter, r.seedsGetter, r.settingsProvider)),
+		provider.DecodeEquinixMetalProjectSizesReq,
 		handler.EncodeJSON,
 		r.defaultServerOptions()...,
 	)
