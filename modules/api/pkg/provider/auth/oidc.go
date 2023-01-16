fc4b15662b161dda823cd70b0dfe4280d0fd664c
Patryk Przekwas <patryk@kubermatic.com>
2023-01-16 17:42:02 +0100

    Update oidc provider factory

--- modules/api/pkg/provider/auth/oidc.go
+++ modules/api/pkg/provider/auth/oidc.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"github.com/gorilla/securecookie"
+
 	"k8c.io/dashboard/v2/pkg/handler/auth"
 	"k8c.io/dashboard/v2/pkg/provider"
 	authtypes "k8c.io/dashboard/v2/pkg/provider/auth/types"
@@ -19,14 +21,35 @@ func OIDCIssuerVerifierProviderFactory(
 			return defaultOIDCIssuerVerifier, nil
 		}
 
+		secureCookie := defaultOIDCIssuerVerifier.OIDCConfig().SecureCookie
+		cookieSecureMode := defaultOIDCIssuerVerifier.OIDCConfig().CookieSecureMode
+		offlineAccessAsScope := defaultOIDCIssuerVerifier.OIDCConfig().OfflineAccessAsScope
+		skipTLSVerify := defaultOIDCIssuerVerifier.OIDCConfig().SkipTLSVerify
+
+		// Overwriting default vaules from Seed configuration.
 		oidc := seed.Spec.OIDCProviderConfiguration
+		if oidc.CookieHashKey != nil {
+			secureCookie = securecookie.New([]byte(*oidc.CookieHashKey), nil)
+		}
+		if oidc.CookieSecureMode != nil {
+			cookieSecureMode = *oidc.CookieSecureMode
+		}
+		if oidc.OfflineAccessAsScope != nil {
+			offlineAccessAsScope = *oidc.OfflineAccessAsScope
+		}
+		if oidc.SkipTLSVerify != nil {
+			skipTLSVerify = *oidc.SkipTLSVerify
+		}
+
 		return auth.NewOpenIDClient(
 			&authtypes.OIDCConfiguration{
-				URL:          oidc.IssuerURL,
-				ClientID:     oidc.IssuerClientID,
-				ClientSecret: oidc.IssuerClientSecret,
-				// TODO add it to the Seed fields and take from there
-				SkipTLSVerify: oidcSkipTLSVerify,
+				URL:                  oidc.IssuerURL,
+				ClientID:             oidc.IssuerClientID,
+				ClientSecret:         oidc.IssuerClientSecret,
+				SecureCookie:         secureCookie,
+				CookieSecureMode:     cookieSecureMode,
+				OfflineAccessAsScope: offlineAccessAsScope,
+				SkipTLSVerify:        skipTLSVerify,
 			},
 			oidcIssuerRedirectURI,
 			auth.NewCombinedExtractor(
