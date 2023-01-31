535fe508b8595d5049bedd741c781cec298e9ce3
Marvin Beckers <marvin@kubermatic.com>
2023-01-31 09:54:29 +0100

    Move containerRuntime defaulting
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- modules/api/pkg/handler/common/cluster.go
+++ modules/api/pkg/handler/common/cluster.go
@@ -218,10 +218,6 @@ func GenerateCluster(
 		return nil, common.KubernetesErrorToHTTPError(err)
 	}
 
-	// Default container runtime if it is empty and run the validation.
-	if spec.ContainerRuntime == "" {
-		spec.ContainerRuntime = "containerd"
-	}
 	if err = validation.ValidateContainerRuntime(spec); err != nil {
 		return nil, common.KubernetesErrorToHTTPError(err)
 	}
