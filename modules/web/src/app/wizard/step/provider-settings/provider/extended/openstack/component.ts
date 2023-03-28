249e3dd91fbeca02cc82001a07011aea4a26630e
Waseem Abbas <waseemabbas8261@gmail.com>
2023-03-28 22:13:46 +0500

    Add functionality to configure `enableIngressHostname` and `ingressHostnameSuffix` for OpenStack provider

--- modules/web/src/app/wizard/step/provider-settings/provider/extended/openstack/component.ts
+++ modules/web/src/app/wizard/step/provider-settings/provider/extended/openstack/component.ts
@@ -18,10 +18,16 @@ import {
   CredentialsType,
   OpenstackCredentialsTypeService,
 } from '@app/wizard/step/provider-settings/provider/extended/openstack/service';
+import {ClusterSpecService} from '@core/services/cluster-spec';
+import {CloudSpec, Cluster, ClusterSpec, OpenstackCloudSpec} from '@shared/entity/cluster';
 import {BaseFormValidator} from '@shared/validators/base-form.validator';
+import {merge} from 'rxjs';
+import {distinctUntilChanged, takeUntil} from 'rxjs/operators';
 
 enum Controls {
   Credentials = 'credentials',
+  EnableIngressHostname = 'enableIngressHostname',
+  IngressHostnameSuffix = 'ingressHostnameSuffix',
 }
 
 @Component({
@@ -50,6 +56,7 @@ export class OpenstackProviderExtendedComponent extends BaseFormValidator implem
 
   constructor(
     private readonly _builder: FormBuilder,
+    private readonly _clusterSpecService: ClusterSpecService,
     private readonly _credentialsTypeService: OpenstackCredentialsTypeService
   ) {
     super('Openstack Provider Extended');
@@ -58,11 +65,52 @@ export class OpenstackProviderExtendedComponent extends BaseFormValidator implem
   ngOnInit(): void {
     this.form = this._builder.group({
       [Controls.Credentials]: this._builder.control(''),
+      [Controls.EnableIngressHostname]: this._builder.control(false),
+      [Controls.IngressHostnameSuffix]: this._builder.control({value: '', disabled: true}),
     });
+
+    this.form
+      .get(Controls.EnableIngressHostname)
+      .valueChanges.pipe(takeUntil(this._unsubscribe))
+      .subscribe(value => {
+        this._enable(value, Controls.IngressHostnameSuffix);
+      });
+
+    merge(
+      this.form.get(Controls.EnableIngressHostname).valueChanges,
+      this.form.get(Controls.IngressHostnameSuffix).valueChanges
+    )
+      .pipe(distinctUntilChanged())
+      .pipe(takeUntil(this._unsubscribe))
+      .subscribe(_ => (this._clusterSpecService.cluster = this._getClusterEntity()));
   }
 
   ngOnDestroy(): void {
     this._unsubscribe.next();
     this._unsubscribe.complete();
+  }
+
+  private _getClusterEntity(): Cluster {
+    return {
+      spec: {
+        cloud: {
+          openstack: {
+            enableIngressHostname: this.form.get(Controls.EnableIngressHostname).value || null,
+            ingressHostnameSuffix: this.form.get(Controls.IngressHostnameSuffix).value || null,
+          } as OpenstackCloudSpec,
+        } as CloudSpec,
+      } as ClusterSpec,
+    } as Cluster;
+  }
+
+  private _enable(enable: boolean, name: string): void {
+    if (enable && this.form.get(name).disabled) {
+      this.form.get(name).enable();
+    }
+
+    if (!enable && this.form.get(name).enabled) {
+      this.form.get(name).reset();
+      this.form.get(name).disable();
+    }
   }
 }
