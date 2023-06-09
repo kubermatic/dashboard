cf5010ec20ce8ffd75ec371ae5f178acd2c77ffa
pprzekwas <patryk@kubermatic.com>
2023-06-09 18:45:19 +0200

    Update Golang veresion to 1.20.5 (#6025)
    * Update go veresion - part 1
    
    * Update kubermatic/build in .prow folder
    
    * Update kubectl & helm in webtermial image
    
    * Update chrome-headless
    
    * Fix image tag
    
    * Update kubectl inside web-terminal container to 1.26.5
    
    * Fix image tag

--- modules/api/hack/update-swagger.sh
+++ modules/api/hack/update-swagger.sh
@@ -21,7 +21,7 @@ source hack/lib.sh
 
 API=modules/api
 
-CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.20-node-18-4 containerize ./$API/hack/update-swagger.sh
+CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.20-node-18-7 containerize ./$API/hack/update-swagger.sh
 
 echodate "Generating swagger spec"
 cd $API/cmd/kubermatic-api/
