863ee8e3a1a387bcd5e312a78d148db91c5de7c3
Waseem Abbas <waseemabbas8261@gmail.com>
2023-08-02 19:16:51 +0500

    Migrate material slide-toggle component from legacy to latest version (#6142)

--- modules/web/src/app/shared/module.ts
+++ modules/web/src/app/shared/module.ts
@@ -40,8 +40,7 @@ import {
 } from '@angular/material/legacy-paginator';
 import {MatLegacyRadioModule as MatRadioModule} from '@angular/material/legacy-radio';
 import {MatLegacySelectModule as MatSelectModule} from '@angular/material/legacy-select';
-import {MatLegacySlideToggleModule as MatSlideToggleModule} from '@angular/material/legacy-slide-toggle';
-import {MatLegacySliderModule as MatSliderModule} from '@angular/material/legacy-slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatLegacyTableModule as MatTableModule} from '@angular/material/legacy-table';
 import {MatLegacyTabsModule as MatTabsModule} from '@angular/material/legacy-tabs';
 import {MatLegacyTooltipModule as MatTooltipModule} from '@angular/material/legacy-tooltip';
@@ -164,7 +163,6 @@ const modules = [
   MatChipsModule,
   MatCardModule,
   MatDialogModule,
-  MatSliderModule,
   MatSlideToggleModule,
   MatProgressBarModule,
   MatExpansionModule,
