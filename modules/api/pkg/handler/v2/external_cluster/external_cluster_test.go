65b5fb3f46124a25fae1def4de3e0e9e85198826
Marvin Beckers <marvin@kubermatic.com>
2023-09-15 13:10:47 +0200

    [release/v2.23] Bump to k8c.io/kubermatic/v2@4a8e342d8697
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- modules/api/pkg/handler/v2/external_cluster/external_cluster_test.go
+++ modules/api/pkg/handler/v2/external_cluster/external_cluster_test.go
@@ -306,7 +306,7 @@ func TestListClusters(t *testing.T) {
 						ID:   "clusterAbcID",
 					},
 					Spec: apiv1.ClusterSpec{
-						Version: "v1.26.6",
+						Version: "v1.26.9",
 					},
 					Labels: map[string]string{kubermaticv1.ProjectIDLabelKey: test.GenDefaultProject().Name},
 				},
@@ -316,7 +316,7 @@ func TestListClusters(t *testing.T) {
 						ID:   "clusterDefID",
 					},
 					Spec: apiv1.ClusterSpec{
-						Version: "v1.26.6",
+						Version: "v1.26.9",
 					},
 					Labels: map[string]string{kubermaticv1.ProjectIDLabelKey: test.GenDefaultProject().Name},
 				},
@@ -339,7 +339,7 @@ func TestListClusters(t *testing.T) {
 						ID:   "clusterAbcID",
 					},
 					Spec: apiv1.ClusterSpec{
-						Version: "v1.26.6",
+						Version: "v1.26.9",
 					},
 					Labels: map[string]string{kubermaticv1.ProjectIDLabelKey: test.GenDefaultProject().Name},
 				},
@@ -349,7 +349,7 @@ func TestListClusters(t *testing.T) {
 						ID:   "clusterDefID",
 					},
 					Spec: apiv1.ClusterSpec{
-						Version: "v1.26.6",
+						Version: "v1.26.9",
 					},
 					Labels: map[string]string{kubermaticv1.ProjectIDLabelKey: test.GenDefaultProject().Name},
 				},
