46b216f94090047b88f70885ba7857cf0e5c34cd
Marvin Beckers <marvin@kubermatic.com>
2023-10-11 11:45:44 +0200

    Update jobs and scripts to Go 1.21.3
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- modules/api/hack/update-swagger.sh
+++ modules/api/hack/update-swagger.sh
@@ -21,7 +21,7 @@ source hack/lib.sh
 
 API=modules/api
 
-CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.21-node-18-1 containerize ./$API/hack/update-swagger.sh
+CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.21-node-18-6 containerize ./$API/hack/update-swagger.sh
 
 echodate "Generating swagger spec"
 cd $API/cmd/kubermatic-api/
