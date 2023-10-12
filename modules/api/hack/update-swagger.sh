b8a58ef09ca2a58280e37fe161b05391a41fb3f4
Marvin Beckers <marvin@kubermatic.com>
2023-10-12 15:09:04 +0200

    [release/v2.23] Update jobs and scripts to Go 1.20.10 (#6271)
    * [release/v2.23] Update jobs and scripts to Go 1.20.10
    
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>
    
    * Update golang.org/x/net to v0.17.0
    
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>
    
    * go mod tidy
    
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>
    
    * Update Go image used for gen-api-client.sh
    
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>
    
    ---------
    
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>

--- modules/api/hack/update-swagger.sh
+++ modules/api/hack/update-swagger.sh
@@ -21,7 +21,7 @@ source hack/lib.sh
 
 API=modules/api
 
-CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.20-node-18-8 containerize ./$API/hack/update-swagger.sh
+CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.20-node-18-11 containerize ./$API/hack/update-swagger.sh
 
 echodate "Generating swagger spec"
 cd $API/cmd/kubermatic-api/
