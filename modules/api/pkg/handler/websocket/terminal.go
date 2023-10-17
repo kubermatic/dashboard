941b00539e0d6fb801ad941b39fcb3a5095b2572
pprzekwas <patryk@kubermatic.com>
2023-10-17 09:28:58 +0200

    [release/v2.22] Update dependencies in web-termial docker image and bump util version (#6288)
    * Update dependencies in webtermial docker image and bump util version
    
    * fix image version
    
    * Bump webtermial image version

--- modules/api/pkg/handler/websocket/terminal.go
+++ modules/api/pkg/handler/websocket/terminal.go
@@ -65,7 +65,7 @@ const (
 	pingMessage                       = "PING"
 	pongMessage                       = "PONG"
 
-	webTerminalImage                   = resources.RegistryQuay + "/kubermatic/web-terminal:0.6.0"
+	webTerminalImage                   = resources.RegistryQuay + "/kubermatic/web-terminal:0.6.3"
 	webTerminalContainerKubeconfigPath = "/etc/kubernetes/kubeconfig/kubeconfig"
 )
 
