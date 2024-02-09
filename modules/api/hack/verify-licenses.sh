182170f45b4793bbc0024004bcbd2cbf4e2746bb
Christoph Mewes <christoph@kubermatic.com>
2024-02-09 07:56:01 +0100

    update to Go 1.22 (#6505)

--- modules/api/hack/verify-licenses.sh
+++ modules/api/hack/verify-licenses.sh
@@ -21,7 +21,7 @@ source hack/lib.sh
 
 API=modules/api
 
-CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.21-node-18-9 containerize ./$API/hack/verify-licenses.sh
+CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.22-node-18-2 containerize ./$API/hack/verify-licenses.sh
 
 cd $API
 go mod vendor
