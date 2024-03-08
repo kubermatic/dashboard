7a8e391195b5dc5ec94033ced4aaf56c231b999c
Christoph Mewes <christoph@kubermatic.com>
2024-03-08 10:33:38 +0100

    [release/v2.24] Bump to Go 1.21.8

--- modules/api/hack/update-swagger.sh
+++ modules/api/hack/update-swagger.sh
@@ -21,7 +21,7 @@ source hack/lib.sh
 
 API=modules/api
 
-CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.21-node-18-9 containerize ./$API/hack/update-swagger.sh
+CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.21-node-18-12 containerize ./$API/hack/update-swagger.sh
 
 echodate "Generating swagger spec"
 cd $API/cmd/kubermatic-api/
