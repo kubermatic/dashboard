d964268bcf37351c208ec21cddd85abb51382660
Waleed Malik <ahmedwaleedmalik@gmail.com>
2024-03-29 19:25:22 +0500

    Use build image with node 20.12 (#6641)
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- modules/api/hack/verify-licenses.sh
+++ modules/api/hack/verify-licenses.sh
@@ -21,7 +21,7 @@ source hack/lib.sh
 
 API=modules/api
 
-CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.22-node-18-5 containerize ./$API/hack/verify-licenses.sh
+CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.22-node-20-2 containerize ./$API/hack/verify-licenses.sh
 
 cd $API
 go mod vendor
