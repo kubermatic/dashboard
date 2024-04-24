a5673da8fca69d8e2d30e2dd44dbc16fec49e3e9
Waleed Malik <ahmedwaleedmalik@gmail.com>
2024-04-24 19:16:12 +0500

    Update tests
    Signed-off-by: Waleed Malik <ahmedwaleedmalik@gmail.com>

--- modules/api/pkg/handler/v2/external_cluster/external_cluster_test.go
+++ modules/api/pkg/handler/v2/external_cluster/external_cluster_test.go
@@ -306,7 +306,7 @@ func TestListClusters(t *testing.T) {
 						ID:   "clusterAbcID",
 					},
 					Spec: apiv1.ClusterSpec{
-						Version: "v1.28.7",
+						Version: "v1.29.4",
 					},
 					Labels: map[string]string{kubermaticv1.ProjectIDLabelKey: test.GenDefaultProject().Name},
 				},
@@ -316,7 +316,7 @@ func TestListClusters(t *testing.T) {
 						ID:   "clusterDefID",
 					},
 					Spec: apiv1.ClusterSpec{
-						Version: "v1.28.7",
+						Version: "v1.29.4",
 					},
 					Labels: map[string]string{kubermaticv1.ProjectIDLabelKey: test.GenDefaultProject().Name},
 				},
@@ -339,7 +339,7 @@ func TestListClusters(t *testing.T) {
 						ID:   "clusterAbcID",
 					},
 					Spec: apiv1.ClusterSpec{
-						Version: "v1.28.7",
+						Version: "v1.29.4",
 					},
 					Labels: map[string]string{kubermaticv1.ProjectIDLabelKey: test.GenDefaultProject().Name},
 				},
@@ -349,7 +349,7 @@ func TestListClusters(t *testing.T) {
 						ID:   "clusterDefID",
 					},
 					Spec: apiv1.ClusterSpec{
-						Version: "v1.28.7",
+						Version: "v1.29.4",
 					},
 					Labels: map[string]string{kubermaticv1.ProjectIDLabelKey: test.GenDefaultProject().Name},
 				},
