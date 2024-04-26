7122a9fa5be009b9cb28a6a7d53a6b1cc7d51f49
Waseem Abbas <waseemabbas8261@gmail.com>
2024-04-26 14:41:44 +0500

    Import hotfix-mat-select directive in shared module

--- modules/web/src/app/core/module.ts
+++ modules/web/src/app/core/module.ts
@@ -81,7 +81,6 @@ import {NutanixService} from '@core/services/provider/nutanix';
 import {VMwareCloudDirectorService} from '@core/services/provider/vmware-cloud-director';
 import {DialogModeService} from '@core/services/dialog-mode';
 import {ClusterBackupService} from './services/cluster-backup';
-import {HotfixMatSelectDirective} from '@app/hotfix-mat-select';
 
 const components = [
   SidenavComponent,
@@ -96,7 +95,6 @@ const components = [
   UserPanelComponent,
   ChangelogDialog,
   HelpPanelComponent,
-  HotfixMatSelectDirective,
 ];
 
 const services = [
