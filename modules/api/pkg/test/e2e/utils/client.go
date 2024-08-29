ad176c87461351e1828663bd0c94c308da032d55
Mohamed Rafraf <81432497+mohamed-rafraf@users.noreply.github.com>
2024-08-29 17:29:21 +0200

    change hetzner machine size (#6786)

--- modules/api/pkg/test/e2e/utils/client.go
+++ modules/api/pkg/test/e2e/utils/client.go
@@ -599,7 +599,7 @@ func (r *TestClient) CreateHetznerCluster(projectID, dc, name, credential, versi
 	}
 
 	if replicas > 0 {
-		instanceSize := "cx21"
+		instanceSize := "cx22"
 
 		clusterSpec.NodeDeployment = &models.NodeDeployment{
 			Spec: &models.NodeDeploymentSpec{
