4f07e4a6d48c0f495ec60a22ba7b3dd55cbeb554
archups <archupsawant@gmail.com>
2024-11-06 15:11:21 +0530

    Bump the version programmatically
    Signed-off-by: archups <archupsawant@gmail.com>

--- modules/api/cmd/kubermatic-api/main.go
+++ modules/api/cmd/kubermatic-api/main.go
@@ -19,7 +19,7 @@ limitations under the License.
 // This OpenAPI 2.0 specification describes the REST APIs used by the Kubermatic Kubernetes Platform Dashboard.
 //
 //	Schemes: https
-//	Version: 2.23
+//	Version: 2.27
 //
 //	Consumes:
 //	- application/json
