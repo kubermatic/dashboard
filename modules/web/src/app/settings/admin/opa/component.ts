8949be942decbfabda0237eeb2487d66caf2becd
ahmadhamzh <ahmadhamzh@ymail.com>
2024-11-15 12:06:11 +0300

    add OPA deprevated warning

--- modules/web/src/app/settings/admin/opa/component.ts
+++ modules/web/src/app/settings/admin/opa/component.ts
@@ -22,6 +22,7 @@ import {takeUntil} from 'rxjs/operators';
 @Component({
   selector: 'km-admin-settings-opa',
   templateUrl: './template.html',
+  styleUrls: ['style.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false,
 })
