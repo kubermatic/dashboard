07ba48b73f2f2224815808ea36dcd8792a375955
Kai Fink <kai.fink@yahoo.de>
2025-04-07 22:03:18 +0200

    New linting requires capital URL in method name
    Signed-off-by: Kai Fink <kai.fink@yahoo.de>

--- modules/api/cmd/kubermatic-api/main.go
+++ modules/api/cmd/kubermatic-api/main.go
@@ -698,7 +698,7 @@ func clusterProviderFactory(mapper meta.RESTMapper, seedKubeconfigGetter provide
 			return nil, fmt.Errorf("failed to create dynamic seed client: %w", err)
 		}
 
-		userClusterConnectionProvider, err := client.NewExternalWithProxy(seedCtrlruntimeClient, seed.GetManagementProxyUrl())
+		userClusterConnectionProvider, err := client.NewExternalWithProxy(seedCtrlruntimeClient, seed.GetManagementProxyURL())
 		if err != nil {
 			return nil, fmt.Errorf("failed to get userClusterConnectionProvider: %w", err)
 		}
