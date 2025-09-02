199376616701ed8c6c9b4cabc2563ac02b872f39
archups <archupsawant@gmail.com>
2025-09-03 02:47:59 +0530

    Bump Go build image version to 1.25
    Signed-off-by: archups <archupsawant@gmail.com>

--- modules/api/hack/update-swagger.sh
+++ modules/api/hack/update-swagger.sh
@@ -21,7 +21,7 @@ source hack/lib.sh
 
 API=modules/api
 
-CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.24-node-20-6 containerize ./$API/hack/update-swagger.sh
+CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.25-node-24-1 containerize ./$API/hack/update-swagger.sh
 
 echodate "Generating swagger spec"
 cd $API/cmd/kubermatic-api/
