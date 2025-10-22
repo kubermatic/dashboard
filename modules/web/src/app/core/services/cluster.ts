0059eb4925084ccda63fcf166a98e62eb34e5efc
ahmadhamzh <ahmadhamzh@ymail.com>
2025-10-22 13:02:03 +0300

    minor fixes

--- modules/web/src/app/core/services/cluster.ts
+++ modules/web/src/app/core/services/cluster.ts
@@ -254,7 +254,8 @@ export class ClusterService {
   }
 
   getShareKubeconfigURL(projectID: string, clusterID: string, userID: string, oidcKubeLogin = false): string {
-    return `${this._location}${this._restRoot}/kubeconfig?project_id=${projectID}&cluster_id=${clusterID}&user_id=${userID}&oidc_kubelogin_enabled=${oidcKubeLogin}`;
+    const location = this._location.endsWith('/') ? this._location : `${this._location}/`;
+    return `${location}${this._restRoot}/kubeconfig?project_id=${projectID}&cluster_id=${clusterID}&user_id=${userID}&oidc_kubelogin_enabled=${oidcKubeLogin}`;
   }
 
   externalClusterEvents(projectID: string, clusterID: string): Observable<Event[]> {
