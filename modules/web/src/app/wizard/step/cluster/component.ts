8a076702047c451c0f86a71ffab1d17d2e97848b
Khizer Rehan <khizerrehan92@gmail.com>
2026-02-23 00:43:03 +0500

    UX Improvement for Kubernetes/OPA Deprecation Warning  (#7868)
    * Add deprecation warning icon for Kubernetes Dashboard in UI
    
    * fix icon warning styles
    
    * Add OPA deprecation message and update related components
    
    * Remove inline margin style from Kubernetes Dashboard deprecation warning icon
    
    * Add OPA deprecation warning icon and update tab component for label templates

--- modules/web/src/app/wizard/step/cluster/component.ts
+++ modules/web/src/app/wizard/step/cluster/component.ts
@@ -75,7 +75,7 @@ import {
   generateEncryptionKey,
 } from '@shared/utils/cluster';
 import {getEditionVersion} from '@shared/utils/common';
-import {KUBERNETES_DASHBOARD_DEPRECATED_MESSAGE} from '@app/shared/constants/common';
+import {KUBERNETES_DASHBOARD_DEPRECATED_MESSAGE, OPA_DEPRECATED_MESSAGE} from '@app/shared/constants/common';
 import {AsyncValidators} from '@shared/validators/async.validators';
 import {KmValidators} from '@shared/validators/validators';
 import * as y from 'js-yaml';
@@ -208,6 +208,7 @@ export class ClusterStepComponent extends StepBase implements OnInit, ControlVal
   readonly IPFamily = IPFamily;
   readonly NodeProvider = NodeProvider;
   readonly KUBERNETES_DASHBOARD_DEPRECATED_MESSAGE = KUBERNETES_DASHBOARD_DEPRECATED_MESSAGE;
+  readonly OPA_DEPRECATED_MESSAGE = OPA_DEPRECATED_MESSAGE;
   private _datacenterSpec: Datacenter;
   private _seedSettings: SeedSettings;
   private _settings: AdminSettings;
