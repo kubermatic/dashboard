5609f87c2ed2fbaf04bb124c0a7913fee78c1215
KhizerRehan <khizerrehan92@gmail.com>
2026-08-11 03:41:38 +0500

    fix: hide cluster backup features in CE builds
    Cluster backup is an enterprise-only feature, but the cluster wizard,
    edit cluster dialog, cluster summary and admin default settings rendered
    its controls in community edition builds as well. Gate all of them on
    DynamicModule.isEnterpriseEdition, and skip the backup storage location
    lookup, the related form control wiring and the spec.backupConfig
    payload in community edition.
    
    (cherry picked from commit 29011b799a05ea8563c6d0a3b820bd8ef28a69cc)
    Signed-off-by: KhizerRehan <khizerrehan92@gmail.com>

--- modules/web/src/app/wizard/step/cluster/component.ts
+++ modules/web/src/app/wizard/step/cluster/component.ts
@@ -215,7 +215,7 @@ export class ClusterStepComponent extends StepBase implements OnInit, ControlVal
   }
 
   get isclusterBackupEnabled(): boolean {
-    return this._settings.enableClusterBackups;
+    return this.isEnterpriseEdition && !!this._settings?.enableClusterBackups;
   }
 
   constructor(
@@ -272,9 +272,11 @@ export class ClusterStepComponent extends StepBase implements OnInit, ControlVal
       this.form.updateValueAndValidity();
     });
 
-    this._projectService.selectedProject
-      .pipe(takeUntil(this._unsubscribe))
-      .subscribe(project => this._getCBSL(project.id));
+    if (this.isEnterpriseEdition) {
+      this._projectService.selectedProject
+        .pipe(takeUntil(this._unsubscribe))
+        .subscribe(project => this._getCBSL(project.id));
+    }
 
     this._fetchCNIPlugins();
 
@@ -400,16 +402,18 @@ export class ClusterStepComponent extends StepBase implements OnInit, ControlVal
         }
       });
 
-    this.control(Controls.ClusterBackup)
-      .valueChanges.pipe(takeUntil(this._unsubscribe))
-      .subscribe((value: boolean) => {
-        if (value) {
-          this.form.addControl(Controls.BackupStorageLocation, this._builder.control('', Validators.required));
-          this._handleClusterBackupChange();
-        } else {
-          this.form.removeControl(Controls.BackupStorageLocation);
-        }
-      });
+    if (this.isEnterpriseEdition) {
+      this.control(Controls.ClusterBackup)
+        .valueChanges.pipe(takeUntil(this._unsubscribe))
+        .subscribe((value: boolean) => {
+          if (value) {
+            this.form.addControl(Controls.BackupStorageLocation, this._builder.control('', Validators.required));
+            this._handleClusterBackupChange();
+          } else {
+            this.form.removeControl(Controls.BackupStorageLocation);
+          }
+        });
+    }
 
     this.control(Controls.AuditLogging)
       .valueChanges.pipe(takeUntil(this._unsubscribe))
@@ -685,7 +689,9 @@ export class ClusterStepComponent extends StepBase implements OnInit, ControlVal
       [Controls.UserSSHKeyAgent]: this._builder.control(
         this.isUserSshKeyEnabled ? (clusterSpec?.enableUserSSHKeyAgent ?? true) : false
       ),
-      [Controls.ClusterBackup]: this._builder.control(clusterSpec?.backupConfig || false),
+      [Controls.ClusterBackup]: this._builder.control(
+        this.isEnterpriseEdition ? clusterSpec?.backupConfig || false : false
+      ),
       [Controls.OPAIntegration]: this._builder.control(clusterSpec?.opaIntegration?.enabled ?? false),
       [Controls.KyvernoIntegration]: this._builder.control(clusterSpec?.kyverno?.enabled ?? false),
       [Controls.Konnectivity]: this._builder.control(clusterSpec?.clusterNetwork?.konnectivityEnabled ?? true),
@@ -749,7 +755,7 @@ export class ClusterStepComponent extends StepBase implements OnInit, ControlVal
 
     this.control(Controls.Konnectivity).disable();
 
-    if (this.control(Controls.ClusterBackup).value) {
+    if (this.isEnterpriseEdition && this.control(Controls.ClusterBackup).value) {
       this.form.addControl(
         Controls.BackupStorageLocation,
         this._builder.control(clusterSpec?.backupConfig?.backupStorageLocation?.name, Validators.required)
@@ -791,7 +797,7 @@ export class ClusterStepComponent extends StepBase implements OnInit, ControlVal
             this.isUserSshKeyEnabled && clusterSpec?.enableUserSSHKeyAgent
               ? true
               : this.controlValue(Controls.UserSSHKeyAgent),
-          [Controls.ClusterBackup]: clusterSpec.backupConfig || false,
+          [Controls.ClusterBackup]: this.isEnterpriseEdition ? clusterSpec.backupConfig || false : false,
           [Controls.OPAIntegration]: clusterSpec?.opaIntegration?.enabled ?? this.controlValue(Controls.OPAIntegration),
           [Controls.KyvernoIntegration]:
             clusterSpec?.kyverno?.enabled ?? this.controlValue(Controls.KyvernoIntegration),
@@ -1211,14 +1217,16 @@ export class ClusterStepComponent extends StepBase implements OnInit, ControlVal
       } as ClusterSpec,
     } as Cluster;
 
-    if (this.controlValue(Controls.ClusterBackup)) {
-      clusterObject.spec.backupConfig = {
-        backupStorageLocation: {
-          name: this.controlValue(Controls.BackupStorageLocation),
-        },
-      };
-    } else {
-      clusterObject.spec.backupConfig = null;
+    if (this.isEnterpriseEdition) {
+      if (this.controlValue(Controls.ClusterBackup)) {
+        clusterObject.spec.backupConfig = {
+          backupStorageLocation: {
+            name: this.controlValue(Controls.BackupStorageLocation),
+          },
+        };
+      } else {
+        clusterObject.spec.backupConfig = null;
+      }
     }
     return clusterObject;
   }
