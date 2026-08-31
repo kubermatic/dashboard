b51e0e4dd0b8ca22474c10321097fd709882dbd4
KhizerRehan <khizerrehan92@gmail.com>
2026-08-31 11:44:48 +0500

    fix: update container images to go-1.26-node-24

--- modules/api/hack/verify-licenses.sh
+++ modules/api/hack/verify-licenses.sh
@@ -21,7 +21,7 @@ source hack/lib.sh
 
 API=modules/api
 
-CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.26-node-22-13 containerize ./$API/hack/verify-licenses.sh
+CONTAINERIZE_IMAGE=quay.io/kubermatic/build:go-1.26-node-24-14 containerize ./$API/hack/verify-licenses.sh
 
 cd $API
 go mod vendor
